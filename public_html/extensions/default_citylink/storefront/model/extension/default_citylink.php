f1455e96f25f4018da22fdc6fdabb5f16b0235f6
abantecart <projkov@abantecart.com>
2012-06-21 13:34:22 -0400

    Citylink and Weight based shipping for product level shipping settings

--- public_html/extensions/default_citylink/storefront/model/extension/default_citylink.php
+++ public_html/extensions/default_citylink/storefront/model/extension/default_citylink.php
@@ -44,28 +44,62 @@ class ModelExtensionDefaultCitylink extends Model {
 	
 		if ($status) {
 			$cost = 0;
-			$weight = $this->cart->getWeight();
 			
 			$rates = explode(',', $this->config->get('default_citylink_rates'));
-			
-			foreach ($rates as $rate) {
-  				$data = explode(':', $rate);
-  					
-				if ($data[0] >= $weight) {
-					if (isset($data[1])) {
-    					$cost = $data[1];
-					}
-					
-   					break;
-  				}
+
+			//Process all products shipped together with not special shipping settings on a product level
+			$b_products = $this->cart->basicShippingProducts(); 
+			if ( count($b_products) > 0 ) {
+			    $prod_ids = array();
+			    foreach ($b_products as $prd) {
+			    	$prod_ids[] = $prd['product_id'];
+			    }
+
+			    $weight = $this->cart->getWeight( $prod_ids );
+				
+				foreach ($rates as $rate) {
+	  				$data = explode(':', $rate);  					
+					if ($data[0] >= $weight) {
+						if (isset($data[1])) {
+	    					$cost = $data[1];
+						}
+	   					break;
+	  				}
+				}
 			}
 			
+			//Process products that have special shipping settings
+			$special_ship_products = $this->cart->specialShippingProducts();
+			foreach ($special_ship_products as $product) {
+			    
+			    $weight = $this->cart->getWeight( array($product['product_id']) );
+			    if ($product['free_shipping']) {
+			    	continue;
+			    } else if($product['shipping_price'] > 0) {
+			    	$fixed_cost = $product['shipping_price'];
+			    	//If ship individually count every quintaty 
+			     	if ($product['ship_individually']) {
+			     		$cost = $fixed_cost * $product['quantity'];	
+			     	}
+			    } else {
+					foreach ($rates as $rate) {
+		  				$data = explode(':', $rate);  					
+						if ($data[0] >= $weight) {
+							if (isset($data[1])) {
+		    					$cost = $cost + $data[1];
+							}
+		   					break;
+		  				}
+					}
+			    }
+			}
+				
 			$quote_data = array();
 			
 			if ((float)$cost) {
 				$quote_data['default_citylink'] = array(
         			'id'           => 'default_citylink.default_citylink',
-        			'title'        => $this->language->get('text_title') . '  (' . $this->language->get('text_weight') . ' ' . $this->weight->format($weight, $this->config->get('config_weight_class')) . ')',
+        			'title'        => $this->language->get('text_title') . '  (' . $this->language->get('text_weight') . ' ' . $this->weight->format($this->cart->getWeight(), $this->config->get('config_weight_class')) . ')',
         			'cost'         => $cost,
         			'tax_class_id' => $this->config->get('default_citylink_tax_class_id'),
 					'text'         => $this->currency->format($this->tax->calculate($cost, $this->config->get('default_citylink_tax_class_id'), $this->config->get('config_tax')))
