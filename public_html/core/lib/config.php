8c6877c8300ce79fb8ac0a64de7a1bf80cdac3a5
abolabo@gmail.com <negiroba_150577>
2012-11-02 14:48:56 +0200

    Core: misconfigured domain bugfix.

--- public_html/core/lib/config.php
+++ public_html/core/lib/config.php
@@ -17,17 +17,17 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if (! defined ( 'DIR_CORE' )) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE')) {
+	header('Location: static_pages/');
 }
 
 final class AConfig {
 	private $cnfg = array();
-  	private $registry;
-	public $groups = array('details', 'general', 'checkout', 'appearance', 'mail', 'api', 'system');
+	private $registry;
+	public $groups = array( 'details', 'general', 'checkout', 'appearance', 'mail', 'api', 'system' );
 
-	public function __construct( $registry ) {
-  		$this->registry = $registry;
+	public function __construct($registry) {
+		$this->registry = $registry;
 		$this->_load_settings();
 	}
 
@@ -37,9 +37,9 @@ final class AConfig {
 	 * @param $key - data key
 	 * @return requested data; null if no data wit such key
 	 */
-  	public function get($key) {
-    	return (isset($this->cnfg[$key]) ? $this->cnfg[$key] : NULL);
-  	}	
+	public function get($key) {
+		return (isset($this->cnfg[ $key ]) ? $this->cnfg[ $key ] : NULL);
+	}
 
 	/**
 	 * add data to config.
@@ -49,8 +49,8 @@ final class AConfig {
 	 * @return void
 	 */
 	public function set($key, $value) {
-    	$this->cnfg[$key] = $value;
-  	}
+		$this->cnfg[ $key ] = $value;
+	}
 
 	/**
 	 * check if data exist in config
@@ -59,8 +59,8 @@ final class AConfig {
 	 * @return bool
 	 */
 	public function has($key) {
-    	return isset($this->cnfg[$key]);
-  	}
+		return isset($this->cnfg[ $key ]);
+	}
 
 	/**
 	 * load data from config and merge with current data set
@@ -69,112 +69,133 @@ final class AConfig {
 	 * @param $filename
 	 * @return void
 	 */
-  	public function load($filename) {
+	public function load($filename) {
 		$file = DIR_CONFIG . $filename . '.php';
-		
-    	if (file_exists($file)) { 
-	  		$cfg = array();
-	  
-	  		require($file);
-	  
-	  		$this->data = array_merge($this->data, $cfg);
+
+		if (file_exists($file)) {
+			$cfg = array();
+
+			require($file);
+
+			$this->data = array_merge($this->data, $cfg);
 		} else {
 			throw new AException(AC_ERR_LOAD, 'Error: Could not load config ' . $filename . '!');
 		}
-  	}
+	}
 
-	private function _load_settings( ) {
+	private function _load_settings() {
 		$cache = $this->registry->get('cache');
 		$db = $this->registry->get('db');
 
 		// Load default store settings first
-		$settings = $cache->force_get('settings','',0);
-		if ( empty( $settings ) ) {
-		    // set global settings (without extensions settings)
+		$settings = $cache->force_get('settings', '', 0);
+		if (empty($settings)) {
+			// set global settings (without extensions settings)
 			$sql = "SELECT se.*
 					FROM " . DB_PREFIX . "settings se
 					LEFT JOIN " . DB_PREFIX . "extensions e ON TRIM(se.`group`) = TRIM(e.`key`)
 					WHERE se.store_id='0' AND e.extension_id IS NULL";
-			$query = $db->query( $sql );
+			$query = $db->query($sql);
 			$settings = $query->rows;
-			$cache->force_set('settings', $settings,'',0);
+			$cache->force_set('settings', $settings, '', 0);
 		}
 
 		foreach ($settings as $setting) {
-		    $this->cnfg[$setting['key']]  = $setting['value'];
+			$this->cnfg[ $setting[ 'key' ] ] = $setting[ 'value' ];
 		}
-		
+
 		//detect URL for the store
-		$url = str_replace('www.', '', $_SERVER['HTTP_HOST']) . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/';
-        if(defined('INSTALL')){
-            $url = str_replace('install/','',$url);
-        }
+		$url = str_replace('www.', '', $_SERVER[ 'HTTP_HOST' ]) . rtrim(dirname($_SERVER[ 'PHP_SELF' ]), '/.\\') . '/';
+		if (defined('INSTALL')) {
+			$url = str_replace('install/', '', $url);
+		}
 		// if storefront and not default store
 		// try to load setting for given url
-		if (!( $this->cnfg['config_url'] == 'http://'.$url || $this->cnfg['config_url'] == 'http://www.'.$url )) {
-		    $cache_name = 'settings.store.'.md5('http://'.$url);
-		   	$store_settings = $cache->force_get($cache_name);
-		   	if ( empty($store_settings) ) {
-		      $sql = "SELECT se.`key`, se.`value`, st.store_id
+		if (!($this->cnfg[ 'config_url' ] == 'http://' . $url || $this->cnfg[ 'config_url' ] == 'http://www.' . $url)) {
+			$cache_name = 'settings.store.' . md5('http://' . $url);
+			$store_settings = $cache->force_get($cache_name);
+			if (empty($store_settings)) {
+				$sql = "SELECT se.`key`, se.`value`, st.store_id
 		   			  FROM " . DB_PREFIX . "settings se
 		   			  RIGHT JOIN " . DB_PREFIX . "stores st ON se.store_id=st.store_id
 		   			  LEFT JOIN " . DB_PREFIX . "extensions e ON TRIM(se.`group`) = TRIM(e.`key`)
 		   			  WHERE se.store_id = (SELECT DISTINCT store_id FROM " . DB_PREFIX . "settings
 		   			                       WHERE `group`='details' AND `key` = 'config_url'
 		                                         AND (`value` = '" . $db->escape('http://www.' . $url) . "'
-		                                               OR `value` = '" . $db->escape('http://'.$url) . "')
+		                                               OR `value` = '" . $db->escape('http://' . $url) . "')
 		                                   LIMIT 0,1)
 		   					AND st.status = 1 AND e.extension_id IS NULL";
 
-		   		$query = $db->query($sql);
-		   		$store_settings = $query->rows;
-		   		$cache->force_set( $cache_name, $store_settings);
-		   	}
+				$query = $db->query($sql);
+				$store_settings = $query->rows;
+				$cache->force_set($cache_name, $store_settings);
+			}
 
-		   	if($store_settings){
-		    	//if(!IS_ADMIN){
-		    		foreach ($store_settings as $row) {
-		    			$this->cnfg[$row['key']] = $row['value'];
-		    		}
-		    	//}
-		   		$this->cnfg['config_store_id'] = $store_settings[0]['store_id'];
-		   	} else {
-		        $warning = new AWarning('Warning: Trying to access by unconfigured or unknown domain. Possibly missing or incorrect store URL set up. Loading default store configuration');
-		        $warning->toLog()->toMessages();
-		        //set config url to current domain
-		        $this->cnfg['config_url'] = 'http://' . REAL_HOST . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/';
-		    }
-		
-		   	if( !$this->cnfg['config_url'] ){
-		   		$this->cnfg['config_url'] = 'http://' . REAL_HOST . rtrim(dirname($_SERVER['PHP_SELF']), '/.\\') . '/';
-		   	}
-		
+			if ($store_settings) {
+				//if(!IS_ADMIN){
+				foreach ($store_settings as $row) {
+					$this->cnfg[ $row[ 'key' ] ] = $row[ 'value' ];
+				}
+				//}
+				$this->cnfg[ 'config_store_id' ] = $store_settings[ 0 ][ 'store_id' ];
+			} else {
+				$warning = new AWarning('Warning: Trying to access by unconfigured or unknown domain. Possibly missing or incorrect store URL set up. Loading default store configuration');
+				$warning->toLog()->toMessages();
+				//set config url to current domain
+				$this->cnfg[ 'config_url' ] = 'http://' . REAL_HOST . rtrim(dirname($_SERVER[ 'PHP_SELF' ]), '/.\\') . '/';
+			}
+
+			if (!$this->cnfg[ 'config_url' ]) {
+				$this->cnfg[ 'config_url' ] = 'http://' . REAL_HOST . rtrim(dirname($_SERVER[ 'PHP_SELF' ]), '/.\\') . '/';
+			}
+
 		}
-		
-		if( is_null($this->cnfg['config_store_id']) ){
-		   		$this->cnfg['config_store_id'] = 0;
+
+		if (is_null($this->cnfg[ 'config_store_id' ])) {
+			$this->cnfg[ 'config_store_id' ] = 0;
+			$this->_set_default_settings();
 		}
-		
+
 		// load extension settings
-		$cache_suffix = IS_ADMIN ? 'admin' : $this->cnfg['config_store_id'];
-		$settings = $cache->force_get('settings.extension.'.$cache_suffix);
-		if ( empty( $settings ) ) {
+		$cache_suffix = IS_ADMIN ? 'admin' : $this->cnfg[ 'config_store_id' ];
+		$settings = $cache->force_get('settings.extension.' . $cache_suffix);
+		if (empty($settings)) {
 			// all settings of default store without extensions settings
 			$sql = "SELECT se.*
 					FROM " . DB_PREFIX . "settings se
 					LEFT JOIN " . DB_PREFIX . "extensions e ON TRIM(se.`group`) = TRIM(e.`key`)
-					WHERE se.store_id='".(int)$this->cnfg['config_store_id']."' AND e.extension_id IS NOT NULL
+					WHERE se.store_id='" . (int)$this->cnfg[ 'config_store_id' ] . "' AND e.extension_id IS NOT NULL
 					ORDER BY se.store_id ASC, se.group ASC";
-		
-			$query = $db->query( $sql );
+
+			$query = $db->query($sql);
 			$settings = $query->rows;
-			$cache->force_set('settings.extension.'.$cache_suffix, $settings);
+			$cache->force_set('settings.extension.' . $cache_suffix, $settings);
 		}
-		
+
 		foreach ($settings as $setting) {
-			$this->cnfg[$setting['key']] = $setting['value'];
+			$this->cnfg[ $setting[ 'key' ] ] = $setting[ 'value' ];
 		}
 	}
 
+	private function _set_default_settings() {
+		$cache = $this->registry->get('cache');
+		$db = $this->registry->get('db');
+		$cache_name = 'settings.store.' . md5($this->cnfg[ 'config_url' ]);
+		$sql = "SELECT se.`key`, se.`value`, st.store_id
+					  FROM " . DB_PREFIX . "settings se
+					  RIGHT JOIN " . DB_PREFIX . "stores st ON se.store_id=st.store_id
+					  LEFT JOIN " . DB_PREFIX . "extensions e ON TRIM(se.`group`) = TRIM(e.`key`)
+					  WHERE se.store_id = 0 AND st.status = 1 AND e.extension_id IS NULL";
+
+		$query = $db->query($sql);
+		$store_settings = $query->rows;
+		foreach ($store_settings as $row) {
+			if ($row[ 'key' ] != 'config_url') {
+				$this->cnfg[ $row[ 'key' ] ] = $row[ 'value' ];
+			}
+		}
+		$cache->force_set($cache_name, $store_settings);
+	}
 }
+
 ?>
