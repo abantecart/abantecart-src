080ab929db2e63837f51dbf484be63df8c282e7e
abolabo@gmail.com <negiroba_150577>
2012-12-18 12:59:20 +0200

    Core: helper/utils -> new function for date formatting added Extension: banner manager dates bugfix

--- public_html/core/helper/utils.php
+++ public_html/core/helper/utils.php
@@ -17,47 +17,47 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.  
 ------------------------------------------------------------------------------*/
-if (! defined ( 'DIR_CORE' )) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE')) {
+	header('Location: static_pages/');
 }
 
-function isFunctionAvailable( $func_name ){
+function isFunctionAvailable($func_name) {
 	return function_exists($func_name);
 }
+
 /*
  * prepare prices and other floats for database writing,, based on locale settings of number formatting
  * */
-function preformatFloat($value, $decimal_point='.'){
-	if($decimal_point!='.'){
-		$value = str_replace('.','~',$value);
-		$value = str_replace($decimal_point,'.',$value);
+function preformatFloat($value, $decimal_point = '.') {
+	if ($decimal_point != '.') {
+		$value = str_replace('.', '~', $value);
+		$value = str_replace($decimal_point, '.', $value);
 	}
-	return (float)preg_replace('/[^0-9\-\.]/','',$value);
+	return (float)preg_replace('/[^0-9\-\.]/', '', $value);
 }
 
 /*
  * prepare integer for database writing
  * */
-function preformatInteger($value){
-	return (int)preg_replace('/[^0-9\-]/','',$value);
+function preformatInteger($value) {
+	return (int)preg_replace('/[^0-9\-]/', '', $value);
 }
 
 /*
  * prepare string for text id 
  * */
-function preformatTextID($value){
-	return strtolower( preg_replace("/[^A-Za-z0-9_]/", "", $value) );
+function preformatTextID($value) {
+	return strtolower(preg_replace("/[^A-Za-z0-9_]/", "", $value));
 }
 
 /*
  * check that argument variable has value (even 0 is a value)  
  * */
-function has_value( $value ){
-	if ( !is_array($value) && $value !== '' && !is_null($value) ) {
-		return true;	
-	}
-	else if (is_array($value) && count($value) > 0) {
-		return true;		
+function has_value($value) {
+	if (!is_array($value) && $value !== '' && !is_null($value)) {
+		return true;
+	} else if (is_array($value) && count($value) > 0) {
+		return true;
 	} else {
 		return false;
 	}
@@ -66,40 +66,21 @@ function has_value( $value ){
 /*
 *  Convert input text to alpaha numeric string for SEO URL use
 */
-function SEOEncode( $string_value ){
-	$seo_key = html_entity_decode($string_value, ENT_QUOTES,'UTF-8');
-	$seo_key = preg_replace( '/[^\w\d\s_-]/si', '', $seo_key );
-	$seo_key = trim( mb_strtolower( $seo_key ) );
-	$seo_key = htmlentities( preg_replace( '/\s+/', '_', $seo_key ) );
+function SEOEncode($string_value) {
+	$seo_key = html_entity_decode($string_value, ENT_QUOTES, 'UTF-8');
+	$seo_key = preg_replace('/[^\w\d\s_-]/si', '', $seo_key);
+	$seo_key = trim(mb_strtolower($seo_key));
+	$seo_key = htmlentities(preg_replace('/\s+/', '_', $seo_key));
 	return $seo_key;
-}
-
-/*
-*  Convert PHP date format to datepicker date format. 
-*  AbanteCart base date format on language setting date_format_short that is PHP date function format
-*  Convert to datepicker format
-*  References:
-*  http://docs.jquery.com/UI/Datepicker/formatDate
-*  http://php.net/manual/en/function.date.php
-*/
-function format2Datepicker( $date_format ){
-	$new_format = $date_format;
-	$new_format = preg_replace( '/d/', 'dd', $new_format );
-	$new_format = preg_replace( '/j/', 'd', $new_format );
-	$new_format = preg_replace( '/l/', 'DD', $new_format );
-	$new_format = preg_replace( '/z/', 'o', $new_format );
-	$new_format = preg_replace( '/m/', 'mm', $new_format );
-	$new_format = preg_replace( '/n/', 'm', $new_format );
-	$new_format = preg_replace( '/F/', 'MM', $new_format );
-	$new_format = preg_replace( '/Y/', 'yy', $new_format );
-	return $new_format;
 }
 
 /*
 * Echo array with readable formal. Useful in debugging of array data. 
 */
-function echo_array( $array_data ) {
-	echo "<pre> $sub_table_name: ";print_r( $array_data );echo'</pre>';
+function echo_array($array_data) {
+	echo "<pre> $sub_table_name: ";
+	print_r($array_data);
+	echo'</pre>';
 }
 
 
@@ -108,76 +89,127 @@ function echo_array( $array_data ) {
  */
 
 function getFilesInDir($dir, $file_ext = '') {
-    if(!is_dir($dir)) return false;
-    $dir = rtrim($dir, '\\/');
-    $result = array();
+	if (!is_dir($dir)) return false;
+	$dir = rtrim($dir, '\\/');
+	$result = array();
 
-    foreach (glob("$dir/*") as $f) {
-        if (is_dir($f)) {    // if is directory
-            $result = array_merge($result, getFilesInDir($f, $file_ext));
-        } else {
-            if($file_ext && substr($f,-3)!=$file_ext){
-                continue;
-            }
-            $result[] = $f;
-        }
-    }
-    return $result;
+	foreach (glob("$dir/*") as $f) {
+		if (is_dir($f)) { // if is directory
+			$result = array_merge($result, getFilesInDir($f, $file_ext));
+		} else {
+			if ($file_ext && substr($f, -3) != $file_ext) {
+				continue;
+			}
+			$result[ ] = $f;
+		}
+	}
+	return $result;
 }
+
 // function for version compare
-function versionCompare($version1, $version2, $operator){
-	$version1 = explode('.',preg_replace('/[^0-9\.]/', '', $version1));
-	$version2 = explode('.',preg_replace('/[^0-9\.]/', '', $version2));
-	$i=0;
-	while($i<3){
-		if(isset($version1[$i])){
-			$version1[$i] = (int)$version1[$i];
-		}else{
-			$version1[$i] = ($i==2  && isset($version2[$i])) ? (int)$version2[$i] : 99;
+function versionCompare($version1, $version2, $operator) {
+	$version1 = explode('.', preg_replace('/[^0-9\.]/', '', $version1));
+	$version2 = explode('.', preg_replace('/[^0-9\.]/', '', $version2));
+	$i = 0;
+	while ($i < 3) {
+		if (isset($version1[ $i ])) {
+			$version1[ $i ] = (int)$version1[ $i ];
+		} else {
+			$version1[ $i ] = ($i == 2 && isset($version2[ $i ])) ? (int)$version2[ $i ] : 99;
 		}
-		if(isset($version2[$i])){
-			$version2[$i] = (int)$version2[$i];
-		}else{
-			$version2[$i] = ($i==2  && isset($version1[$i])) ? (int)$version1[$i] : 99;;
+		if (isset($version2[ $i ])) {
+			$version2[ $i ] = (int)$version2[ $i ];
+		} else {
+			$version2[ $i ] = ($i == 2 && isset($version1[ $i ])) ? (int)$version1[ $i ] : 99;
+			;
 		}
-	$i++;
+		$i++;
 	}
 
-	if($version1[1]>$version2[1]){ // if major version of extension changed
+	if ($version1[ 1 ] > $version2[ 1 ]) { // if major version of extension changed
 		return false;
 	}
 
-	$version1 = implode('.',$version1);
-	$version2 = implode('.',$version2);
+	$version1 = implode('.', $version1);
+	$version2 = implode('.', $version2);
 
 	return version_compare($version1, $version2, $operator);
 }
 
-function getTextUploadError($error){
+function getTextUploadError($error) {
 	switch ($error) {
-	        case UPLOAD_ERR_INI_SIZE:
-	            $error_txt = 'The uploaded file exceeds the upload_max_filesize directive in php.ini (now '.ini_get('upload_max_filesize').')';
-	            break;
-	        case UPLOAD_ERR_FORM_SIZE:
-		        $error_txt = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
-		        break;
-			case UPLOAD_ERR_PARTIAL:
-		        $error_txt = 'The uploaded file was only partially uploaded';
-		        break;
-	        case UPLOAD_ERR_NO_FILE:
-		        $error_txt = 'No file was uploaded';
-		        break;
-	        case UPLOAD_ERR_NO_TMP_DIR:
-		        $error_txt = 'Missing a php temporary folder';
-		        break;
-	        case UPLOAD_ERR_CANT_WRITE:
-		        $error_txt = 'Failed to write file to disk';
-		        break;
-	        case UPLOAD_ERR_EXTENSION:
-		        $error_txt = 'File upload stopped by php-extension';
-		        break;
-	        default:
-		        $error_txt = 'Some problem happen with file upload. Check error log for more information';
+		case UPLOAD_ERR_INI_SIZE:
+			$error_txt = 'The uploaded file exceeds the upload_max_filesize directive in php.ini (now ' . ini_get('upload_max_filesize') . ')';
+			break;
+		case UPLOAD_ERR_FORM_SIZE:
+			$error_txt = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
+			break;
+		case UPLOAD_ERR_PARTIAL:
+			$error_txt = 'The uploaded file was only partially uploaded';
+			break;
+		case UPLOAD_ERR_NO_FILE:
+			$error_txt = 'No file was uploaded';
+			break;
+		case UPLOAD_ERR_NO_TMP_DIR:
+			$error_txt = 'Missing a php temporary folder';
+			break;
+		case UPLOAD_ERR_CANT_WRITE:
+			$error_txt = 'Failed to write file to disk';
+			break;
+		case UPLOAD_ERR_EXTENSION:
+			$error_txt = 'File upload stopped by php-extension';
+			break;
+		default:
+			$error_txt = 'Some problem happen with file upload. Check error log for more information';
 	}
 	return $error_txt;
+}
+
+/*
+ * DATETIME funtions
+ */
+
+/*
+*  Convert PHP date format to datepicker date format.
+*  AbanteCart base date format on language setting date_format_short that is PHP date function format
+*  Convert to datepicker format
+*  References:
+*  http://docs.jquery.com/UI/Datepicker/formatDate
+*  http://php.net/manual/en/function.date.php
+*/
+function format2Datepicker($date_format) {
+	$new_format = $date_format;
+	$new_format = preg_replace('/d/', 'dd', $new_format);
+	$new_format = preg_replace('/j/', 'd', $new_format);
+	$new_format = preg_replace('/l/', 'DD', $new_format);
+	$new_format = preg_replace('/z/', 'o', $new_format);
+	$new_format = preg_replace('/m/', 'mm', $new_format);
+	$new_format = preg_replace('/n/', 'm', $new_format);
+	$new_format = preg_replace('/F/', 'MM', $new_format);
+	$new_format = preg_replace('/Y/', 'yy', $new_format);
+	return $new_format;
+}
+
+function dateFromFormat($string_date, $date_format, $timezone = null) {
+	$date = new DateTime();
+	$timezone = is_null($timezone) ? $date->getTimezone() : $timezone;
+	if (empty($date_format)) return null;
+	$string_date = empty($string_date) ? date($date_format) : $string_date;
+	$iso_date = DateTime::createFromFormat($date_format, $string_date, $timezone);
+	$result = $iso_date ? $iso_date->getTimestamp() : null;
+	return $result;
+}
+
+function dateFromIso($string_date) {
+	return dateFromFormat($string_date, 'Y-m-d H:i:s');
+}
+
+function reformatDate($string_date, $date_format_src, $date_format_dst) {
+	if (empty($string_date) || empty($date_format_src) || empty($date_format_dst)) return null;
+	$dateint = dateFromFormat($string_date, $date_format_src);
+	if (!is_null($dateint)) {
+		return date($date_format_dst, $dateint);
+	} else {
+		return null;
+	}
 }
