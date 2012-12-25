07a73934819730d21d864f93c2d962356dc4e2b5
abolabo@gmail.com <negiroba_150577>
2012-12-25 20:28:58 +0200

    Core: lib/json minor bugfix

--- public_html/core/lib/json.php
+++ public_html/core/lib/json.php
@@ -17,8 +17,8 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if (! defined ( 'DIR_CORE' )) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE')) {
+	header('Location: static_pages/');
 }
 
 final class AJson {
@@ -29,36 +29,36 @@ final class AJson {
 			switch (gettype($data)) {
 				case 'boolean':
 					return $data ? 'true' : 'false';
-    			case 'integer':
-    			case 'double':
-      				return $data;
-    			case 'resource':
-    			case 'string':
-      				return '"' . str_replace(array("\r", "\n", "<", ">", "&"), array('\r', '\n', '\x3c', '\x3e', '\x26'), addslashes($data)) . '"';
+				case 'integer':
+				case 'double':
+					return $data;
+				case 'resource':
+				case 'string':
+					return '"' . str_replace(array( "\r", "\n", "<", ">", "&" ), array( '\r', '\n', '\x3c', '\x3e', '\x26' ), addslashes($data)) . '"';
 				case 'array':
 					if (empty($data) || array_keys($data) === range(0, sizeof($data) - 1)) {
 						$stdout = array();
-						
+
 						foreach ($data as $value) {
-							$stdout[] = Json::encode($value);
+							$stdout[ ] = AJson::encode($value);
 						}
-						
+
 						return '[ ' . implode(', ', $stdout) . ' ]';
 					}
-    			case 'object':
-      				$stdout = array();
-      				
+				case 'object':
+					$stdout = array();
+
 					foreach ($data as $key => $value) {
-        				$stdout[] = Json::encode(strval($key)) . ': ' . Json::encode($value);
+						$stdout[ ] = AJson::encode(strval($key)) . ': ' . AJson::encode($value);
 					}
-					
+
 					return '{ ' . implode(', ', $stdout) . ' }';
 				default:
 					return 'null';
 			}
 		}
 	}
-	
+
 	static public function decode($json, $assoc = FALSE) {
 		if (function_exists('json_decode')) {
 			return json_decode($json, $assoc);
@@ -76,40 +76,41 @@ final class AJson {
 			$m2s = array();
 
 			preg_match_all($match, $json, $m);
-			
-			foreach ($m[0] as $s) {
+
+			foreach ($m[ 0 ] as $s) {
 				$hash = '"' . md5($s) . '"';
-				$s2m[$s] = $hash;
-				$m2s[$hash] = str_replace('$', '\$', $s);
+				$s2m[ $s ] = $hash;
+				$m2s[ $hash ] = str_replace('$', '\$', $s);
 			}
 
 			$json = strtr($json, $s2m);
 
 			$a = ($assoc) ? '' : '(object) ';
-			
+
 			$data = array(
-				':' => '=>', 
-				'[' => 'array(', 
-				'{' => "{$a}array(", 
-				']' => ')', 
+				':' => '=>',
+				'[' => 'array(',
+				'{' => "{$a}array(",
+				']' => ')',
 				'}' => ')'
 			);
-			
+
 			$json = strtr($json, $data);
-  
-  			$json = preg_replace('~([\s\(,>])(-?)0~', '$1$2', $json);
-  
- 			$json = strtr($json, $m2s);
 
-  			$function = @create_function('', "return {$json};");
-  			$return = ($function) ? $function() : NULL;
+			$json = preg_replace('~([\s\(,>])(-?)0~', '$1$2', $json);
 
-  			unset($s2m); 
-			unset($m2s); 
+			$json = strtr($json, $m2s);
+
+			$function = @create_function('', "return {$json};");
+			$return = ($function) ? $function() : NULL;
+
+			unset($s2m);
+			unset($m2s);
 			unset($function);
 
-  			return $return;
+			return $return;
 		}
 	}
 }
+
 ?>
