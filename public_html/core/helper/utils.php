c921e235900fef5f9c9c261eaa75c68155b82ac3
abantecart <projkov@abantecart.com>
2013-05-18 14:06:21 -0400

    Windows strptime() fix

--- public_html/core/helper/utils.php
+++ public_html/core/helper/utils.php
@@ -342,7 +342,33 @@ function DateTimeCreateFromFormat($date_format, $string_date) {
 	return $int_date;
 }
 
-
+//strptime function with solution for windows
+if( !function_exists("strptime")) {
+	function strptime($date, $format) {
+	    $masks = array(
+	      '%d' => '(?P<d>[0-9]{2})',
+	      '%m' => '(?P<m>[0-9]{2})',
+	      '%Y' => '(?P<Y>[0-9]{4})',
+	      '%H' => '(?P<H>[0-9]{2})',
+	      '%M' => '(?P<M>[0-9]{2})',
+	      '%S' => '(?P<S>[0-9]{2})',
+	    );
+	
+	    $rexep = "#".strtr(preg_quote($format), $masks)."#";
+	    if(!preg_match($rexep, $date, $out))
+	      return false;
+	
+	    $ret = array(
+	      "tm_sec"  => (int) $out['S'],
+	      "tm_min"  => (int) $out['M'],
+	      "tm_hour" => (int) $out['H'],
+	      "tm_mday" => (int) $out['d'],
+	      "tm_mon"  => $out['m']?$out['m']-1:0,
+	      "tm_year" => $out['Y'] > 1900 ? $out['Y'] - 1900 : 0,
+	    );
+	    return $ret;
+	}
+}
 
 function checkRequirements() {
 	$error = '';
