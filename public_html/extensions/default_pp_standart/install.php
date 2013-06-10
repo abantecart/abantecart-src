0bd2682aa6c19c764a5e11b8525cee4018a7ec69
Andrei Mikhaylyk <djengobarm@gmail.com>
2013-06-10 19:29:18 +0300

    Extensions: Now icon for payment methods list will be added to Resource library upon install.

--- public_html/extensions/default_pp_standart/install.php
+++ public_html/extensions/default_pp_standart/install.php
@@ -6,6 +6,31 @@ if ( !defined ( 'DIR_CORE' )) {
 
 $language_list = $this->model_localisation_language->getLanguages();
 
-$settings['default_pp_standart_payment_storefront_icon'] = 'image/18/75/f.jpg';
+$rm = new AResourceManager();
+$rm->setType('image');
+
+$result = copy(DIR_EXT.'default_pp_standart/image/secure_paypal_icon.jpg', DIR_RESOURCE.'image/secure_paypal_icon.jpg');
+
+$resource = array(
+	'language_id' => $this->config->get('storefront_language_id'),
+	'name' => array(),
+	'title' => 'default_pp_standart_payment_storefront_icon',
+	'description' => 'Default PayPal Standart Default Storefront Icon',
+	'resource_path' => 'secure_paypal_icon.jpg',
+	'resource_code' => ''
+);
+
+foreach($language_list as $lang){
+	$resource['name'][$lang['language_id']] = 'secure_paypal_icon.jpg';
+}
+$resource_id = $rm->addResource($resource);
+
+if ( $resource_id ) {
+	// get hexpath of resource (RL moved given file from rl-image-directory in own dir tree)
+	$resource_info = $rm->getResource($resource_id, $this->config->get('admin_language_id'));
+	// write it path in settings (array from parent method "install" of extension manager)
+	$settings['default_pp_standart_payment_storefront_icon'] =  'image/'.$resource_info['resource_path'];
+
+}
 
 $settings['default_pp_standart_custom_logo'] = 'resources/' . $this->config->get('config_logo');
