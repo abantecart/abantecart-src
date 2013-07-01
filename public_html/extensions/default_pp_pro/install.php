0cdc55d73df58ac4d4fe20fa042e1557440ae599
abolabo@gmail.com <negiroba_150577>
2013-07-01 17:11:53 +0300

    Etxtensions: paypal pro default payment icon details fixed (RL related)

--- public_html/extensions/default_pp_pro/install.php
+++ public_html/extensions/default_pp_pro/install.php
@@ -14,14 +14,16 @@ $result = copy(DIR_EXT.'default_pp_pro/image/secure_paypal_icon.jpg', DIR_RESOUR
 $resource = array(
 	'language_id' => $this->config->get('storefront_language_id'),
 	'name' => array(),
-	'title' => 'default_pp_pro_payment_storefront_icon',
-	'description' => 'Default PayPal Pro Default Storefront Icon',
+	'title' => array(),
+	'description' => array(),
 	'resource_path' => 'secure_paypal_icon.jpg',
 	'resource_code' => ''
 );
 
 foreach($language_list as $lang){
 	$resource['name'][$lang['language_id']] = 'secure_paypal_icon.jpg';
+	$resource['title'][$lang['language_id']] = 'default_pp_pro_payment_storefront_icon';
+	$resource['description'][$lang['language_id']] = 'Default PayPal Pro Default Storefront Icon';
 }
 $resource_id = $rm->addResource($resource);
 
