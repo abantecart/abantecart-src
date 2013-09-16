dcb87dc67e892a7472005639b5d8637516d67cd5
Dmitry Suprunenko <abolabo@gmail.com>
2013-09-16 19:18:35 +0300

    install: modified upgrade scripts

--- public_html/install/abantecart_upgrade.php
+++ public_html/install/abantecart_upgrade.php
@@ -3,8 +3,9 @@
  * @var $this AController
  */
 // do account page as separate layout for html5 core template
-$layout_manager = new ALayoutManager('default_html5');
-$layout_manager->loadXML(array('file' => pathinfo(PHP_SELF,PATHINFO_DIRNAME).'/upgrade_layout.xml'));
+$file = $package_tmpdir . $package_dirname . '/upgrade_layout.xml';
+$layout = new ALayoutManager();
+$layout->loadXml(array('file' => $file));
 // bugfix of column meanings
 $balances = $this->db->query("SELECT customer_id, debit, credit FROM ".$this->db->table('customer_transactions'));
 foreach($balances->rows as $row){
@@ -14,3 +15,6 @@ foreach($balances->rows as $row){
 			WHERE customer_id='".$row['customer_id']."';";
 	$this->db->query($sql,true); // do safe update
 }
+
+// clear text definitions
+$this->db->query("DELETE FROM ".$this->db->table('language_definitions')." WHERE `section` = 1 AND `block` = 'sale_customer'");
