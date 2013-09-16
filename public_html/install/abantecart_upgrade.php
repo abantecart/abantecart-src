538d72780399373968ad7d8dbdbf1c6ef145ced4
Dmitry Suprunenko <abolabo@gmail.com>
2013-09-16 19:46:50 +0300

    install: modified upgrade scripts

--- public_html/install/abantecart_upgrade.php
+++ public_html/install/abantecart_upgrade.php
@@ -6,13 +6,13 @@
 $file = $package_tmpdir . $package_dirname . '/upgrade_layout.xml';
 $layout = new ALayoutManager();
 $layout->loadXml(array('file' => $file));
-// bugfix of column meanings
-$balances = $this->db->query("SELECT customer_id, debit, credit FROM ".$this->db->table('customer_transactions'));
+// bugfix of column names meanings
+$balances = $this->db->query("SELECT customer_transaction_id, debit, credit FROM ".$this->db->table('customer_transactions'));
 foreach($balances->rows as $row){
 	$sql = "UPDATE ".$this->db->table('customer_transactions')."
 			SET credit = ".$row['debit'].",
 				debit = ".$row['credit']."
-			WHERE customer_id='".$row['customer_id']."';";
+			WHERE customer_transaction_id='".$row['customer_transaction_id']."';";
 	$this->db->query($sql,true); // do safe update
 }
 
