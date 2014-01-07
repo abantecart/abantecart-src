f6664ed5cc25dbd74fc762aecd2c0fce53260dd9
abantecart <projkov@abantecart.com>
2014-01-07 14:44:02 -0500

    Improvment to marketplace and search for extensions

--- public_html/admin/controller/responses/tool/extensions_store.php
+++ public_html/admin/controller/responses/tool/extensions_store.php
@@ -38,7 +38,7 @@ class ControllerResponsesToolExtensionsStore extends AController {
 		$GET['store_id'] = UNIQUE_ID;
 		$GET['store_ip'] = $_SERVER ['SERVER_ADDR'];
 		$GET['store_url'] = HTTP_SERVER;
-		$GET['store_version'] = VERSION;
+		$GET['store_version'] = VERSION;		
 		$GET['language_code'] = $this->request->cookie ['language'];
 		
 		// place your affiliate id here
@@ -46,7 +46,6 @@ class ControllerResponsesToolExtensionsStore extends AController {
 		if(MP_AFFILIATE_ID){
 			$GET['aff_id'] = MP_AFFILIATE_ID;
 		}
-
 
 		$href .= '?'.http_build_query($GET);
 
@@ -60,7 +59,7 @@ class ControllerResponsesToolExtensionsStore extends AController {
 				$this->log->write($err);
 			}
 			$html = '<div style="padding: 10px 10px 10px 20px;	margin-bottom: 15px; background: #FFDFE0 !important;	border: 1px solid #FF9999;	font-size: 12px;">'.$this->language->get('error_connect').'</div>';
-        }else{
+        } else {
 			//then parse response
 			// get base href and remove it from response
 			preg_match('/\<base.*?href=(\")(.*?)(\")/is', $html, $basehref);
@@ -70,16 +69,31 @@ class ControllerResponsesToolExtensionsStore extends AController {
 			$html = str_replace('<link href="/','<link href="'.$basehref,$html);
 			$html = str_replace(' src="/',' src="'.$basehref,$html);
 			// then need to replace url of http-links except anchors and absolute urls
-			preg_match_all('/\<a .*?href=(\")(.*?)(\")/is', $html, $orig_hrefs);
-			foreach($orig_hrefs[2] as $ohref){
+			preg_match_all('/href=(\")(.*?)(\")/i', $html, $orig_hrefs);
+			for($i = 0; $i < count($orig_hrefs[0]); $i++ ) {
+				$replace = $orig_hrefs[0][$i];
+				$ohref = $orig_hrefs[2][$i];
+				$link1 = 'href="'.AEncryption::addEncoded_stid($ohref).(MP_AFFILIATE_ID ? '&aff_id='. MP_AFFILIATE_ID : '').'"';
+				$link2 = 'href="'.$this->html->getSecureUrl('tool/extensions_store','&path='.$ohref.(MP_AFFILIATE_ID ? '&aff_id='. MP_AFFILIATE_ID : '')).'"';
 				if( substr($ohref,0,4)=='http' || substr($ohref,0,1)=='#'){
-					$html = str_replace('"'.$ohref.'"','"'.AEncryption::addEncoded_stid($ohref).(MP_AFFILIATE_ID ? '&aff_id='. MP_AFFILIATE_ID : '').'"',$html);
-					continue; }
-				$html = str_replace('"'.$ohref.'"','"'.$this->html->getSecureUrl('tool/extensions_store','&path='.$ohref.(MP_AFFILIATE_ID ? '&aff_id='. MP_AFFILIATE_ID : '')).'"',$html);
+					$html = str_replace($replace, $link1, $html);
+				} else {
+					$html = str_replace($replace, $link2, $html);
+				}	
 			}
+			// construct action for search form
+			$action = $this->html->getSecureUrl('tool/extensions_store', (MP_AFFILIATE_ID ? '&aff_id='. MP_AFFILIATE_ID : ''));
+			$html = str_replace(' action="/',' action="'.$action,$html);
+			$hidden = '<input type="hidden" name="search_performed" value="Y">';
+			$hidden .= '<input type="hidden" name="rt" value="tool/extensions_store">';
+			$hidden .= '<input type="hidden" name="s" value="'.ADMIN_PATH.'">';
+			$hidden .= '<input type="hidden" name="token" value="'.$this->request->get['token'].'">';
+			$hidden .= '<input type="hidden" name="aff_id" value="'.MP_AFFILIATE_ID.'">';
+			$html = preg_replace('/<input.*name="search_performed".*>/', $hidden, $html);
 
 			$html = $this->html->convertLinks( $html );
 			$html = str_replace('<span>Store ID</span>','<span><font size="1"> '.UNIQUE_ID.'</font></span>',$html);
+
 		}
         $this->response->setOutput($html);
 	}
