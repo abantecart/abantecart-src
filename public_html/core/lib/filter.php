d4073546e3a95c332832fa8057c128a9344783dd
Dmitry Suprunenko <abolabo@gmail.com>
2014-02-13 12:58:50 +0200

    Core: AFilter sort order fix

--- public_html/core/lib/filter.php
+++ public_html/core/lib/filter.php
@@ -48,7 +48,7 @@ final class AFilter {
 		$this->data['sidx'] = $this->request->{$this->method}['sidx']; // get index row - i.e. user click to sort
         $this->data['sidx'] = $this->data['sidx'] ? $this->data['sidx'] : 'sort_order';
         $this->data['sord'] = $this->request->{$this->method}['sord']; // get the direction of sorting
-        $this->data['sord'] = $this->data['sord'] ? $this->data['sord'] : 'DESC';
+        $this->data['sord'] = $this->data['sord'] ? strtoupper($this->data['sord']) : 'DESC';
         $this->data['_search'] = $this->request->{$this->method}['_search'];
         $this->data['filters'] = $this->request->{$this->method}['filters'];
         //If this data is provided in the input override values of the request.
@@ -63,7 +63,7 @@ final class AFilter {
         //Build Filter Data for result output and model query
         $this->data['filter_data'] = array(
             'sort' => $this->data['sidx'],
-            'order' => strtoupper($this->data['sord']),
+            'order' => $this->data['sord'],
             'limit' => $this->data['rows'],
             'start' => ($this->data['page'] - 1) * $this->data['rows'],
             'content_language_id' => $this->session->data['content_language_id'],
@@ -107,7 +107,7 @@ final class AFilter {
             $this->data['filter_string'] = $fl_str;   
         }
         
-        $allowedSortDirection = array('asc', 'desc');
+        $allowedSortDirection = array('ASC', 'DESC');
         if (!in_array($this->data['sord'], $allowedSortDirection)) {
             $this->data['sord'] = 'DESC';
         }
