778963d4ae03f2ac8019d196c850ad2a95dd1364
Dmitry Suprunenko <abolabo@gmail.com>
2014-06-10 15:44:52 +0300

    Core: utils.php php5.2 compability fix

--- public_html/core/helper/utils.php
+++ public_html/core/helper/utils.php
@@ -741,3 +741,40 @@ function getGravatar( $email = '', $s = 80, $d = 'mm', $r = 'g') {
     $url .= "?s=$s&d=$d&r=$r";
     return $url;
 }
+
+
+if (!function_exists('array_replace_recursive')) {
+	/**
+	 * @deprecated since php 5.3
+	 */
+	function array_replace_recursive($array, $array1) {
+		function recurse($array, $array1) {
+			foreach ($array1 as $key => $value) {
+				// create new key in $array, if it is empty or not an array
+				if (!isset($array[$key]) || (isset($array[$key]) && !is_array($array[$key]))) {
+					$array[$key] = array();
+				}
+
+				// overwrite the value in the base array
+				if (is_array($value)) {
+					$value = recurse($array[$key], $value);
+				}
+				$array[$key] = $value;
+			}
+			return $array;
+		}
+
+		// handle the arguments, merge one by one
+		$args = func_get_args();
+		$array = $args[0];
+		if (!is_array($array)) {
+			return $array;
+		}
+		for ($i = 1; $i < count($args); $i++) {
+			if (is_array($args[$i])) {
+				$array = recurse($array, $args[$i]);
+			}
+		}
+		return $array;
+	}
+}
