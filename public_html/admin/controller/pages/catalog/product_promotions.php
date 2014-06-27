dda7592d4f64ffff9cc6c8851855493ee838c95f
Dmitry Suprunenko <abolabo@gmail.com>
2014-06-27 21:42:39 +0300

    Control Panel: product promotions changed for new template

--- public_html/admin/controller/pages/catalog/product_promotions.php
+++ public_html/admin/controller/pages/catalog/product_promotions.php
@@ -33,7 +33,7 @@ class ControllerPagesCatalogProductPromotions extends AController {
     	$this->document->setTitle( $this->language->get('heading_title') );
 		$this->loadModel('catalog/product');
 
-		if (isset($this->request->get['product_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+		if (isset($this->request->get['product_id']) && $this->request->is_GET()) {
       		$product_info = $this->model_catalog_product->getProduct($this->request->get['product_id']);
 			if ( !$product_info ) {
 				$this->session->data['warning'] = $this->language->get('error_product_not_found');
@@ -67,7 +67,8 @@ class ControllerPagesCatalogProductPromotions extends AController {
 		 $this->document->addBreadcrumb( array (
 			'href'      => $this->html->getSecureURL('catalog/product_promotions', '&product_id=' . $this->request->get['product_id'] ),
 			'text'      => $this->language->get('tab_promotions'),
-			'separator' => ' :: '
+			'separator' => ' :: ',
+			'current' => true
 		 ));
 
 		$this->loadModel('sale/customer_group');
@@ -81,12 +82,11 @@ class ControllerPagesCatalogProductPromotions extends AController {
 		$this->data['product_discounts'] = $this->model_catalog_product->getProductDiscounts($this->request->get['product_id']);
 		$this->data['delete_discount'] = $this->html->getSecureURL('catalog/product_discount/delete', '&product_id=' . $this->request->get['product_id'].'&product_discount_id=%ID%' );
 		$this->data['update_discount'] = $this->html->getSecureURL('catalog/product_discount/update', '&product_id=' . $this->request->get['product_id'].'&product_discount_id=%ID%' );
-		$this->data['insert_discount'] = $this->html->getSecureURL('catalog/product_discount/insert', '&product_id=' . $this->request->get['product_id'] );
 
 		$this->data['product_specials'] = $this->model_catalog_product->getProductSpecials($this->request->get['product_id']);
 		$this->data['delete_special'] = $this->html->getSecureURL('catalog/product_special/delete', '&product_id=' . $this->request->get['product_id'].'&product_special_id=%ID%' );
 		$this->data['update_special'] = $this->html->getSecureURL('catalog/product_special/update', '&product_id=' . $this->request->get['product_id'].'&product_special_id=%ID%' );
-		$this->data['insert_special'] = $this->html->getSecureURL('catalog/product_special/insert', '&product_id=' . $this->request->get['product_id'] );
+
 
 		foreach ($this->data['product_discounts'] as $i => $item) {
 			if ( $item['date_start'] == '0000-00-00' ) $this->data['product_discounts'][$i]['date_start'] = '';
@@ -97,22 +97,30 @@ class ControllerPagesCatalogProductPromotions extends AController {
 			if ( $item['date_end'] == '0000-00-00' ) $this->data['product_specials'][$i]['date_end'] = '';
 		}
 
-		$this->data['button_remove'] = $this->html->buildButton(array(
-			'text' => $this->language->get('button_remove'),
-			'style' => 'button2',
+		$this->data['button_remove'] = $this->html->buildElement(
+					array( 'type' => 'button',
+							'text' => $this->language->get('button_remove'),
+							'style' => 'button2',
 		));
-		$this->data['button_edit'] = $this->html->buildButton(array(
-			'text' => $this->language->get('button_edit'),
-			'style' => 'button2',
+		$this->data['button_edit'] = $this->html->buildElement(
+							array( 'type' => 'button',
+									'text' => $this->language->get('button_edit'),
+									'style' => 'button2',
 		));
-		$this->data['button_add_discount'] = $this->html->buildButton(array(
-			'text' => $this->language->get('button_add_discount'),
-			'style' => 'button1',
-		));
-		$this->data['button_add_special'] = $this->html->buildButton(array(
-			'text' => $this->language->get('button_add_special'),
-			'style' => 'button1',
-		));
+		$this->data['button_add_discount'] = $this->html->buildElement(
+																array(
+																	'type' => 'button',
+																	'text' => $this->language->get('button_add_discount'),
+																	'href' => $this->html->getSecureURL('catalog/product_discount/insert', '&product_id=' . $this->request->get['product_id'] ),
+																	'style' => 'button1'
+																));
+		$this->data['button_add_special'] = $this->html->buildElement(
+																array(
+																	'type' => 'button',
+																	'text' => $this->language->get('button_add_special'),
+																	'href' => $this->html->getSecureURL('catalog/product_special/insert', '&product_id=' . $this->request->get['product_id'] ),
+																	'style' => 'button1'
+																));
 
 		$this->data['active'] = 'promotions';
 		//load tabs controller
---
+++ public_html/admin/view/default/template/pages/catalog/product_promotion_form.tpl
@@ -0,0 +1,78 @@
+<?php if (!empty($error['warning'])) { ?>
+<div class="warning alert alert-error alert-danger"><?php echo $error['warning']; ?></div>
+<?php } ?>
+<?php if ($success) { ?>
+<div class="success alert alert-success"><?php echo $success; ?></div>
+<?php } ?>
+
+<?php echo $summary_form; ?>
+
+<?php echo $product_tabs ?>
+<div class="tab-content">
+
+	<div class="panel-heading">
+
+			<div class="pull-right">
+
+                <div class="btn-group mr10 toolbar">
+                    <?php echo $form_language_switch; ?>
+                </div>
+
+			    <div class="btn-group mr10 toolbar">
+                    <?php if (!empty ($help_url)){ ?>
+                    <a class="btn btn-white tooltips" href="<?php echo $help_url; ?>" target="new" data-toggle="tooltip" title="" data-original-title="Help">
+                    <i class="fa fa-question-circle"></i>
+                    </a>
+                    <?php } ?>
+			    </div>
+			</div>
+
+	</div>
+
+	<?php echo $form['form_open']; ?>
+
+	<div class="panel-body panel-body-nopadding">
+		<label class="h4 heading"><?php  echo $form_title; ?></label>
+			<?php foreach ($form['fields'] as $name => $field) { ?>
+			<?php
+				//Logic to cululate fileds width
+				$widthcasses = "col-sm-7";
+				if ( is_int(stripos($field->style, 'large-field')) ) {
+					$widthcasses = "col-sm-7";
+				} else if ( is_int(stripos($field->style, 'medium-field')) || is_int(stripos($field->style, 'date')) ) {
+					$widthcasses = "col-sm-5";
+				} else if ( is_int(stripos($field->style, 'small-field')) || is_int(stripos($field->style, 'btn_switch')) ) {
+					$widthcasses = "col-sm-3";
+				} else if ( is_int(stripos($field->style, 'tiny-field')) ) {
+					$widthcasses = "col-sm-2";
+				}
+				$widthcasses .= " col-xs-12";
+			?>
+			<div class="form-group <? if (!empty($error[$name])) { echo "has-error"; } ?>">
+				<label class="control-label col-sm-3 col-xs-12" for="<?php echo $field->element_id; ?>"><?php echo ${'entry_' . $name}; ?></label>
+				<div class="input-group afield <?php echo $widthcasses; ?> <?php echo ($name == 'description' ? 'ml_ckeditor' : '')?>">
+					<?php echo $field; ?>
+				</div>
+				<?php if (is_array($error[$name]) && !empty($error[$name])) { ?>
+				<span class="help-block field_err"><?php echo $error[$name]; ?></span>
+				<?php } else if (!empty($error[$name])) { ?>
+				<span class="help-block field_err"><?php echo $error[$name]; ?></span>
+				<?php } ?>
+			</div>
+		<?php }  ?><!-- <div class="fieldset"> -->
+	</div>
+
+		<div class="panel-footer">
+			<div class="row">
+			   <div class="col-sm-6 col-sm-offset-3">
+			     <button class="btn btn-primary">
+			     <i class="fa fa-save"></i> <?php echo $form['submit']->text; ?>
+			     </button>&nbsp;
+			     <a class="btn btn-default" href="<?php echo $cancel; ?>">
+			     <i class="fa fa-refresh"></i> <?php echo $form['cancel']->text; ?>
+			     </a>
+			   </div>
+			</div>
+		</div>
+		</form>
+</div>
