1a757e7f4e89f3e285f4e938749870f59481ccda
Dmitry Suprunenko <abolabo@gmail.com>
2014-08-06 00:17:28 +0300

    Control Panel: product-relations changes

--- public_html/admin/controller/pages/catalog/product_relations.php
+++ public_html/admin/controller/pages/catalog/product_relations.php
@@ -33,23 +33,7 @@ class ControllerPagesCatalogProductRelations extends AController {
         $this->document->setTitle($this->language->get('heading_title'));
         $this->loadModel('catalog/product');
 
-        if (!$this->registry->has('jqgrid_script')) {
-
-            $locale = $this->session->data['language'];
-            if (!file_exists(DIR_ROOT . '/' . RDIR_TEMPLATE . 'javascript/jqgrid/js/i18n/grid.locale-' . $locale . '.js')) {
-                $locale = 'en';
-            }
-            $this->document->addScript(RDIR_TEMPLATE . 'javascript/jqgrid/js/i18n/grid.locale-' . $locale . '.js');
-            $this->document->addScript(RDIR_TEMPLATE . 'javascript/jqgrid/js/minified/jquery.jqGrid.min.js');
-            $this->document->addScript(RDIR_TEMPLATE . 'javascript/jqgrid/plugins/jquery.grid.fluid.js');
-            $this->document->addScript(RDIR_TEMPLATE . 'javascript/jqgrid/plugins/jquery.ba-bbq.min.js');
-            $this->document->addScript(RDIR_TEMPLATE . 'javascript/jqgrid/plugins/grid.history.js');
-
-            //set flag to not include scripts/css twice
-            $this->registry->set('jqgrid_script', true);
-        }
-
-        if (isset($this->request->get['product_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+        if (isset($this->request->get['product_id']) && ($this->request->is_GET())) {
             $product_info = $this->model_catalog_product->getProduct($this->request->get['product_id']);
             if (!$product_info) {
                 $this->session->data['warning'] = $this->language->get('error_product_not_found');
@@ -57,29 +41,7 @@ class ControllerPagesCatalogProductRelations extends AController {
             }
         }
 
-        if ($this->request->server['REQUEST_METHOD'] == 'POST') {
-
-            $this->load->library('json');
-            $this->request->post['selected'][0] = AJson::decode(html_entity_decode($this->request->post['selected'][0]), true);
-            $this->request->post['selected'][1] = AJson::decode(html_entity_decode($this->request->post['selected'][1]), true);
-            if ($this->request->post['selected']) {
-                $related = array();
-                foreach ($this->request->post['selected'][1] as $id => $related_product) {
-                    if ($related_product['status']) {
-                        $related[] = $id;
-                    }
-                }
-                $product_category = array();
-                foreach ($this->request->post['selected'][0] as $id => $category) {
-                    if ($category['status']) {
-                        $product_category[] = $id;
-                    }
-                }
-                unset($this->request->post['selected']);
-                $this->request->post['product_related'] = $related;
-                $this->request->post['product_category'] = $product_category;
-            }
-
+        if ( $this->request->is_POST() ) {
 
             $this->model_catalog_product->updateProductLinks($this->request->get['product_id'], $this->request->post);
             $this->session->data['success'] = $this->language->get('text_success');
@@ -112,9 +74,9 @@ class ControllerPagesCatalogProductRelations extends AController {
         $this->document->addBreadcrumb(array(
             'href' => $this->html->getSecureURL('catalog/product_relations', '&product_id=' . $this->request->get['product_id']),
             'text' => $this->language->get('tab_relations'),
-            'separator' => ' :: '
+            'separator' => ' :: ',
+			'current' => true
         ));
-
 
         $this->loadModel('catalog/category');
         $this->data['categories'] = array();
@@ -127,7 +89,7 @@ class ControllerPagesCatalogProductRelations extends AController {
         $this->data['stores'] = array(0 => $this->language->get('text_default'));
         $results = $this->model_setting_store->getStores();
         foreach ($results as $r) {
-            $this->data['stores'][$r['store_id']] = $r['name'];
+        	$this->data['stores'][$r['store_id']] = $r['name'];
         }
 
         $this->data['product_category'] = $this->model_catalog_product->getProductCategories($this->request->get['product_id']);
@@ -157,7 +119,7 @@ class ControllerPagesCatalogProductRelations extends AController {
             'type' => 'form',
             'name' => 'productFrm',
             'action' => $this->data['action'],
-            'attr' => 'data-confirm-exit="true"',
+            'attr' => 'data-confirm-exit="true" class="aform form-horizontal"',
         ));
         $this->data['form']['submit'] = $form->getFieldHtml(array(
             'type' => 'button',
@@ -172,117 +134,47 @@ class ControllerPagesCatalogProductRelations extends AController {
             'text' => $this->language->get('button_cancel'),
             'style' => 'button2',
         ));
-        $this->data['form']['fields']['product_store'] = $form->getFieldHtml(array(
-            'type' => 'checkboxgroup',
-            'name' => 'product_store[]',
-            'value' => $this->data['product_store'],
-            'options' => $this->data['stores'],
-            'scrollbox' => true,
+
+		$this->loadModel('catalog/category');
+		$this->data['categories'] = array();
+		$results = $this->model_catalog_category->getCategories(0);
+
+		foreach( $results as $r ) {
+			$this->data['categories'][ $r['category_id'] ] = $r['name'];
+		}
+
+		$this->data['form']['fields']['category'] = $form->getFieldHtml(
+																	array(
+																			'type' => 'checkboxgroup',
+																			'name' => 'product_category[]',
+																			'value' => $this->data['product_category'],
+																			'options' => $this->data['categories'],
+																			'style' => 'chosen',
+																			'placeholder' => $this->language->get('text_select_category'),
+																	));
+		$this->data['products'] = array();
+		$results = $this->model_catalog_product->getProducts();
+		foreach( $results as $r ) {
+			$this->data['products'][ $r['product_id'] ] = $r['name'];
+		}
+
+		$this->data['form']['fields']['related'] = $form->getFieldHtml(
+																	array(
+																			'type' => 'checkboxgroup',
+																			'name' => 'product_related[]',
+																			'value' => $this->data['product_related'],
+																			'options' => $this->data['products'],
+																			'style' => 'chosen',
+																			'placeholder' => $this->language->get('text_select_product'),
+																	));
+
+		$this->data['form']['fields']['store'] = $form->getFieldHtml(array(
+	            'type' => 'checkboxgroup',
+	            'name' => 'product_store[]',
+	            'value' => $this->data['product_store'],
+	            'options' => $this->data['stores'],
+	            'scrollbox' => true,
         ));
-
-        $this->load->library('json');
-
-        $listing_data = array();
-        if ($this->data['product_related']) {
-
-            foreach ($this->data['product_related'] as $product_id) {
-                $product_info = $this->model_catalog_product->getProduct($product_id);
-                $listing_data[$product_id] = array('id' => $product_id,
-                    'name' => html_entity_decode($product_info['name'] . ' (' . $product_info['model'] . ')'),
-					'status' => 1);
-
-            }
-        }
-        // exclude this product from multivalue list. why we need relate recursion?
-        $this->session->data['multivalue_excludes']['product_id'] = $this->request->get['product_id'];
-        $this->data['form']['fields']['list'] = $form->getFieldHtml(array(
-            'id' => 'category_products_list',
-            'type' => 'multivaluelist',
-            'name' => 'category_products',
-            'content_url' => $this->html->getSecureUrl('product/product/related'),
-            'edit_url' => '',
-            'multivalue_hidden_id' => 'popup',
-            'values' => $listing_data,
-            'return_to' => 'productFrm_popup_item_count',
-            'text' => array(
-                'delete' => $this->language->get('button_delete'),
-                'delete_confirm' => $this->language->get('text_delete_confirm'),
-            )
-        ));
-
-
-        $this->data['form']['fields']['list_hidden'] = $form->getFieldHtml(
-            array('id' => 'popup',
-                'type' => 'multivalue',
-                'name' => 'popup',
-                'title' => $this->language->get('text_select_from_list'),
-                'selected' => ($listing_data ? AJson::encode($listing_data) : "{}"),
-                'content_url' => $this->html->getSecureUrl('catalog/product_listing', '&form_name=productFrm&multivalue_hidden_id=popup'),
-                'postvars' => '',
-                'return_to' => '', // placeholder's id of listing items count.
-                'popup_height' => 708,
-                'js' => array(
-                    'apply' => "productFrm_category_products_buildList();",
-                    'cancel' => 'productFrm_category_products_buildList();',
-                ),
-                'text' => array(
-                    'selected' => $this->language->get('text_count_selected'),
-                    'edit' => $this->language->get('text_save_edit'),
-                    'apply' => $this->language->get('text_apply'),
-                    'save' => $this->language->get('button_save'),
-                    'reset' => $this->language->get('button_reset')),
-            ));
-
-
-// CATEGORY
-        $listing_data = array();
-        if ($this->data['product_category']) {
-            foreach ($this->data['product_category'] as $category_id) {
-                $listing_data[$category_id] = array('id' => $category_id,
-                    'name' => html_entity_decode($this->model_catalog_category->getPath($category_id)),
-                    'status' => 1);
-            }
-        }
-
-        $this->data['form']['fields']['category_list'] = $form->getFieldHtml(array(
-            'id' => 'product_categories_list',
-            'type' => 'multivaluelist',
-            'name' => 'product_categories',
-            'content_url' => $this->html->getSecureUrl('product/product/product_categories'),
-            'edit_url' => '',
-            'multivalue_hidden_id' => 'cat_popup',
-            'values' => $listing_data,
-            'return_to' => '',
-            'text' => array(
-                'delete' => $this->language->get('button_delete'),
-                'delete_confirm' => $this->language->get('text_delete_confirm'),
-            )
-        ));
-
-
-        $this->data['form']['fields']['category_list_hidden'] = $form->getFieldHtml(
-            array('id' => 'cat_popup',
-                'type' => 'multivalue',
-                'name' => 'cat_popup',
-                'title' => $this->language->get('text_select_from_list'),
-                'selected' => ($listing_data ? AJson::encode($listing_data) : "{}"),
-                'content_url' => $this->html->getSecureUrl('catalog/category_listing',
-                    '&form_name=productFrm&multivalue_hidden_id=cat_popup'),
-                'postvars' => '',
-                'return_to' => '', // placeholder's id of listing items count.
-                'js' => array(
-                    'apply' => "productFrm_product_categories_buildList();",
-                    'cancel' => 'productFrm_product_categories_buildList();',
-                ),
-                'text' => array(
-                    'selected' => 'Count of selected items: ',
-                    'edit' => $this->language->get('text_save_edit'),
-                    'apply' => $this->language->get('text_apply'),
-                    'save' => $this->language->get('button_save'),
-                    'reset' => $this->language->get('button_reset')),
-            ));
-
-//end
 
         $this->addChild('pages/catalog/product_summary', 'summary_form', 'pages/catalog/product_summary.tpl');
         $this->view->assign('help_url', $this->gen_help_url('product_relations'));
@@ -292,5 +184,4 @@ class ControllerPagesCatalogProductRelations extends AController {
         //update controller data
         $this->extensions->hk_UpdateData($this, __FUNCTION__);
     }
-
 }
