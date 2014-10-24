f80a30a5e2adaf5302998af13c6538c23c9110c4
abolabo <abolabo@gmail.com>
2014-10-24 14:30:32 +0300

    Extensions: paypoint v1.0.1

--- public_html/extensions/default_paypoint/storefront/model/extension/default_paypoint.php
+++ public_html/extensions/default_paypoint/storefront/model/extension/default_paypoint.php
@@ -26,7 +26,11 @@ class ModelExtensionDefaultPayPoint extends Model {
 		$this->load->language('default_paypoint/default_paypoint');
 		
 		if ($this->config->get('default_paypoint_status')) {
-      		$query = $this->db->query("SELECT * FROM " . $this->db->table("zones_to_locations") . " WHERE location_id = '" . (int)$this->config->get('default_paypoint_location_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+      		$query = $this->db->query("SELECT *
+										FROM " . $this->db->table("zones_to_locations") . "
+										WHERE location_id = '" . (int)$this->config->get('default_paypoint_location_id') . "'
+											AND country_id = '" . (int)$address['country_id'] . "'
+											AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 			
 			if (!$this->config->get('default_paypoint_location_id')) {
         		$status = TRUE;
