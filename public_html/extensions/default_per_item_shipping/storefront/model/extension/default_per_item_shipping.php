9a0d3523ea9df0a52a12e234d946eb11a7f42b78
abolabo <abolabo@gmail.com>
2014-10-24 14:32:13 +0300

    Extensions: per item shipping 1.0.1

--- public_html/extensions/default_per_item_shipping/storefront/model/extension/default_per_item_shipping.php
+++ public_html/extensions/default_per_item_shipping/storefront/model/extension/default_per_item_shipping.php
@@ -17,22 +17,22 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if ( !defined ( 'DIR_CORE' )) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE')) {
+	header('Location: static_pages/');
 }
 
 class ModelExtensionDefaultPerItemShipping extends Model {
 	public function getQuote($address) {
 		$this->load->language('default_per_item_shipping/default_per_item_shipping');
-		
+
 		if ($this->config->get('default_per_item_shipping_status')) {
-      		$taxes = $this->tax->getTaxes((int)$address['country_id'], (int)$address['zone_id']);
+			$taxes = $this->tax->getTaxes((int)$address['country_id'], (int)$address['zone_id']);
 
 			if (!$this->config->get('default_per_item_shipping_location_id') || $taxes) {
-        		$status = TRUE;
-      		}else {
+				$status = TRUE;
+			} else {
 				$query = $this->db->query("SELECT *
-											FROM " . DB_PREFIX . "zones_to_locations
+											FROM " . $this->db->table('zones_to_locations') . "
 											WHERE location_id = '" . (int)$this->config->get('default_per_item_shipping_location_id') . "'
 												AND country_id = '" . (int)$address['country_id'] . "'
 												AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
@@ -42,56 +42,55 @@ class ModelExtensionDefaultPerItemShipping extends Model {
 					$status = FALSE;
 				}
 
-      		}
+			}
 		} else {
 			$status = FALSE;
 		}
-		
+
 		$method_data = array();
 
 		if ($status) {
 			$cost = 0;
 
 			//Process all products shipped together with not special shipping settings on a product level
-			$b_products = $this->cart->basicShippingProducts(); 
-			if ( count($b_products) > 0 ) {
+			$b_products = $this->cart->basicShippingProducts();
+			if (count($b_products) > 0) {
 				foreach ($b_products as $prd) {
 					$cost += $this->config->get('default_per_item_shipping_cost') * $prd['quantity'];
 				}
 			}
-			
+
 			//Process products that have special shipping settings
 			$special_ship_products = $this->cart->specialShippingProducts();
 			foreach ($special_ship_products as $product) {
-			    if ($product['free_shipping']) {
-			    	continue;
-			    } else if($product['shipping_price'] > 0) {
-			    	$cost += $product['shipping_price'] * $product['quantity'];
-			    } else {
+				if ($product['free_shipping']) {
+					continue;
+				} else if ($product['shipping_price'] > 0) {
+					$cost += $product['shipping_price'] * $product['quantity'];
+				} else {
 					$cost += $this->config->get('default_per_item_shipping_cost') * $product['quantity'];
-			    }
+				}
 			}
 
 			$quote_data = array();
-			
-      		$quote_data['default_per_item_shipping'] = array(
-        		'id'           => 'default_per_item_shipping.default_per_item_shipping',
-        		'title'        => $this->language->get('text_description'),
-        		'cost'         =>  $cost,
-         		'tax_class_id' => $this->config->get('default_per_item_shipping_tax'),
-				'text'         => $this->currency->format($this->tax->calculate($cost, $this->config->get('default_per_item_shipping_tax'), $this->config->get('config_tax')))
-      		);
 
-      		$method_data = array(
-        		'id'         => 'default_per_item_shipping',
-        		'title'      => $this->language->get('text_title'),
-        		'quote'      => $quote_data,
-				'sort_order' => $this->config->get('default_per_item_shipping_sort_order'),
-        		'error'      => FALSE
-      		);
+			$quote_data['default_per_item_shipping'] = array(
+					'id' => 'default_per_item_shipping.default_per_item_shipping',
+					'title' => $this->language->get('text_description'),
+					'cost' => $cost,
+					'tax_class_id' => $this->config->get('default_per_item_shipping_tax'),
+					'text' => $this->currency->format($this->tax->calculate($cost, $this->config->get('default_per_item_shipping_tax'), $this->config->get('config_tax')))
+			);
+
+			$method_data = array(
+					'id' => 'default_per_item_shipping',
+					'title' => $this->language->get('text_title'),
+					'quote' => $quote_data,
+					'sort_order' => $this->config->get('default_per_item_shipping_sort_order'),
+					'error' => FALSE
+			);
 		}
-	
+
 		return $method_data;
 	}
 }
-?>
