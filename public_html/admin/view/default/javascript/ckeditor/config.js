6388231847b480a8ec720060eaee38e23e9e6306
abantecart <projkov@abantecart.com>
2014-12-03 10:15:16 -0500

    Fix 131

--- public_html/admin/view/default/javascript/ckeditor/config.js
+++ public_html/admin/view/default/javascript/ckeditor/config.js
@@ -30,7 +30,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// need to have in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Se the most common block elements.
+	// Se the most common block elements.	
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Make dialogs simpler.
@@ -42,4 +42,6 @@ CKEDITOR.editorConfig = function( config ) {
     config.enterMode = CKEDITOR.ENTER_BR;
     config.shiftEnterMode = CKEDITOR.ENTER_BR;
     config.disableNativeSpellChecker = false;
+    
+    config.allowedContent = true;
 };
