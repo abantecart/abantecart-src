af4027f1b09bcc634827ed345f0050481c833b26
abolabo <abolabo@gmail.com>
2014-12-09 01:54:29 +0200

    Control Panel: extensions upgrades related changes

--- public_html/admin/controller/pages/index/login.php
+++ public_html/admin/controller/pages/index/login.php
@@ -51,7 +51,8 @@ class ControllerPagesIndexLogin extends AController {
 
 		if ($this->request->is_POST() && $this->_validate()) {
 			$this->session->data['token'] = AEncryption::getHash(mt_rand());
-			//login is sussessful redirect to otiginaly requested page 
+			$this->session->data['checkupdates'] = true; // sign to run ajax-request to check for updates. see common/head for details
+			//login is sussessful redirect to originaly requested page
 			if (isset($this->request->post['redirect']) && !preg_match("/rt=index\/login/i", $this->request->post['redirect'])) {
                 $redirect = $this->html->filterQueryParams( $this->request->post['redirect'], array('token')  );
                 $redirect .=  "&token=".$this->session->data['token'];
