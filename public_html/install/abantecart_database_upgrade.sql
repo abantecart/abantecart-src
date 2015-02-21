cc06598dbcc6969a14e522ca3abf9cc784d896e2
abolabo <abolabo@gmail.com>
2015-02-21 18:10:56 +0200

    upgrade.php changed

--- public_html/install/abantecart_database_upgrade.sql
+++ public_html/install/abantecart_database_upgrade.sql
@@ -21,8 +21,8 @@ ALTER TABLE `ac_products_featured` ADD PRIMARY KEY (`product_id`);
 
 ALTER TABLE `ac_resource_map`
 ADD PRIMARY KEY (`resource_id`, `object_name`, `object_id`),
-DROP INDEX `group_id` ,
-DROP INDEX `ac_resource_map_idx` ;
+DROP INDEX IF EXISTS `group_id` ,
+DROP INDEX IF EXISTS `ac_resource_map_idx` ;
 
 UPDATE `ac_settings`
 SET `group` = 'details'
