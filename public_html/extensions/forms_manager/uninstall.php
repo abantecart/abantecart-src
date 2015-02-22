6388183ad9178ee5e0a07ee5ac44dd1ef47986b1
abantecart <projkov@abantecart.com>
2015-02-22 10:54:48 -0500

    Copyright update

--- public_html/extensions/forms_manager/uninstall.php
+++ public_html/extensions/forms_manager/uninstall.php
@@ -2,11 +2,20 @@
 /*------------------------------------------------------------------------------
   $Id$
 
-  For Abante Cart, E-commerce Solution
+  AbanteCart, Ideal OpenSource Ecommerce Solution
   http://www.AbanteCart.com
 
-  Copyright (c) 2011, 2012 AlgoZone, Inc
+  Copyright © 2011-2015 Belavier Commerce LLC
 
+  This source file is subject to Open Software License (OSL 3.0)
+  Lincence details is bundled with this package in the file LICENSE.txt.
+  It is also available at this URL:
+  <http://www.opensource.org/licenses/OSL-3.0>
+
+ UPGRADE NOTE:
+   Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+   versions in the future. If you wish to customize AbanteCart for your
+   needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
 if (! defined ( 'DIR_CORE' )) {
 	header ( 'Location: static_pages/' );
