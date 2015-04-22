c51382b62e3cd603e837b0d9b4a7e603142f8a2d
abolabo <abolabo@gmail.com>
2015-04-22 12:36:12 +0300

    Control Panel: validation of customer details fix

--- public_html/storefront/model/account/customer.php
+++ public_html/storefront/model/account/customer.php
@@ -428,7 +428,7 @@ class ModelAccountCustomer extends Model {
 			$error['warning'] = $this->language->get('error_exists');
 		}
 
-		if ((mb_strlen($data['telephone']) < 3) || (mb_strlen($data['telephone']) > 32)) {
+		if ($data['telephone'] && (mb_strlen($data['telephone']) < 3 || mb_strlen($data['telephone']) > 32)) {
 			$error['telephone'] = $this->language->get('error_telephone');
 		}
 
