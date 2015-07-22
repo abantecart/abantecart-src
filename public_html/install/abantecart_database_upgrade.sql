282a1249541103852112524fda75725e6d6647f5
abolabo <abolabo@gmail.com>
2015-07-23 01:27:55 +0300

    upgrade scripts reset

--- public_html/install/abantecart_database_upgrade.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `ac_product_descriptions`
-ADD COLUMN `blurb` TEXT COLLATE utf8_general_ci  NOT NULL COMMENT 'translatable';
-
-INSERT INTO `ac_settings` (`group`, `key`, `value`) VALUES
-('general','config_embed_status',1),
-('general','config_embed_click_action', 'modal');
-
