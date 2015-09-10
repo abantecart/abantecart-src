2c633ea0791f5a76b19e6c71742802c6eea755db
Cali-gula <info@goldenfoxi.com>
2015-09-10 11:30:26 -0300

    Cache-Control

--- public_html/core/index.php
+++ public_html/core/index.php
@@ -1,2 +1,29 @@
 <?php
-    header('Location: ../index.php');
+/**
+* 2011-2015 AbanteCart, Ideal OpenSource Ecommerce Solution
+* http://www.AbanteCart.com
+*
+* Copyright © 2011-2015 Belavier Commerce LLC
+*
+* This source file is subject to Open Software License (OSL 3.0)
+* License details is bundled with this package in the file LICENSE.txt.
+* It is also available at this URL:
+* <http://www.opensource.org/licenses/OSL-3.0>
+*
+* UPGRADE NOTE:
+* Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+* versions in the future. If you wish to customize AbanteCart for your
+* needs please refer to http://www.AbanteCart.com for more information.
+*
+*  @author: Belavier Commerce LLC
+*  @copyright: 2011-2015 Belavier Commerce LLC
+*  @license: http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*/
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Location: ../');
+exit;
