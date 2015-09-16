44d2292d6e1936c155b1f1c8f32cd7e3e90db34f
abolabo <abolabo@gmail.com>
2015-09-16 16:57:25 +0300

    upgrade.sql fix

--- public_html/install/abantecart_database_upgrade.sql
+++ public_html/install/abantecart_database_upgrade.sql
@@ -1,8 +1,8 @@
 update `ac_dataset_values` set value_varchar = 'design/template' where dataset_column_id = 12 and row_id = 131;
 
-alter table `abc_blocks` modify column `block_txt_id` varchar(255) NOT NULL;
-alter table `abc_blocks` modify column `controller` varchar(255) NOT NULL;
-alter table `abc_block_templates` modify column `template` varchar(255) NOT NULL;
+alter table `ac_blocks` modify column `block_txt_id` varchar(255) NOT NULL;
+alter table `ac_blocks` modify column `controller` varchar(255) NOT NULL;
+alter table `ac_block_templates` modify column `template` varchar(255) NOT NULL;
 
 UPDATE `ac_dataset_values`
 SET value_varchar = 'window.open(\'http://docs.abantecart.com\');'
