a9ceef47539cbc9c0fbd85311fbe22d6bfdc7f83
abolabo <abolabo@gmail.com>
2015-11-05 18:43:22 +0200

    https://github.com/abantecart/abantecart-src/issues/293

--- public_html/storefront/controller/pages/content/contact.php
+++ public_html/storefront/controller/pages/content/contact.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*------------------------------------------------------------------------------
   $Id$
 
@@ -17,27 +17,29 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if (! defined ( 'DIR_CORE' )) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE')){
+	header('Location: static_pages/');
 }
-class ControllerPagesContentContact extends AController {
-	private $error = array();
+
+class ControllerPagesContentContact extends AController{
+	private $error = array ();
 	/**
 	 * @var AForm
 	 */
 	private $form;
-	public function main() {
+
+	public function main(){
 
 		//init controller data
-		$this->extensions->hk_InitData($this,__FUNCTION__);
+		$this->extensions->hk_InitData($this, __FUNCTION__);
 
-		$this->document->setTitle( $this->language->get('heading_title') );
+		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->form = new AForm('ContactUsFrm');
 		$this->form->loadFromDb('ContactUsFrm');
 		$form = $this->form->getForm();
 
-		if ($this->request->is_POST() && $this->_validate()) {
+		if ($this->request->is_POST() && $this->_validate()){
 			// move all uploaded files to their directories
 			$file_pathes = $this->form->processFileUploads($this->request->files);
 			$template = new ATemplate();
@@ -45,7 +47,7 @@ class ControllerPagesContentContact extends AController {
 			$subject = sprintf($this->language->get('email_subject'), $this->request->post['name']);
 			$template->data['subject'] = $subject;
 
-			$mail = new AMail( $this->config );
+			$mail = new AMail($this->config);
 			$mail->setTo($this->config->get('store_main_email'));
 			$mail->setFrom($this->request->post['email']);
 			$mail->setSender($this->request->post['first_name']);
@@ -56,10 +58,12 @@ class ControllerPagesContentContact extends AController {
 			$template->data['store_name'] = $this->config->get('store_name');
 			$template->data['store_url'] = $this->config->get('config_url');
 			$template->data['text_project_label'] = project_base();
-			$template->data['entry_enquiry'] = $this->language->get('entry_enquiry');
-			$template->data['enquiry'] = $msg = $this->request->post['enquiry'] . "\r\n";
+			$template->data['entry_enquiry'] = $msg = $this->language->get('entry_enquiry');
+			$msg .= "\r\n" . $this->request->post['enquiry'] . "\r\n";
+			$template->data['enquiry'] = nl2br($this->request->post['enquiry'] . "\r\n");
 
 			$form_fields = $this->form->getFields();
+			$template->data['form_fields'] = array();
 			foreach ($form_fields as $field_name => $field_info){
 				if (has_value($this->request->post[$field_name]) && !in_array($field_name, array ('first_name', 'email', 'enquiry', 'captcha'))){
 					$field_details = $this->form->getField($field_name);
@@ -68,13 +72,13 @@ class ControllerPagesContentContact extends AController {
 				}
 			}
 
-			if($file_pathes){
-				$msg .= "\r\n".$this->language->get('entry_attached').": \r\n";
-				foreach($file_pathes as $file_info){
-					$basename = pathinfo(str_replace(' ','_',$file_info['path']),PATHINFO_BASENAME);
-					$msg .= "\t" .$file_info['display_name'] . ': ' . $basename . " (". round(filesize($file_info['path'])/1024,2) ."Kb)\r\n";
+			if ($file_pathes){
+				$msg .= "\r\n" . $this->language->get('entry_attached') . ": \r\n";
+				foreach ($file_pathes as $file_info){
+					$basename = pathinfo(str_replace(' ', '_', $file_info['path']), PATHINFO_BASENAME);
+					$msg .= "\t" . $file_info['display_name'] . ': ' . $basename . " (" . round(filesize($file_info['path']) / 1024, 2) . "Kb)\r\n";
 					$mail->addAttachment($file_info['path'], $basename);
-					$template->data['form_fields'][ $file_info['display_name'] ] = $basename . " (". round(filesize($file_info['path'])/1024,2) ."Kb)";
+					$template->data['form_fields'][$file_info['display_name']] = $basename . " (" . round(filesize($file_info['path']) / 1024, 2) . "Kb)";
 				}
 			}
 			$mail_html = $template->fetch('mail/contact.tpl');
@@ -84,106 +88,106 @@ class ControllerPagesContentContact extends AController {
 			$mail->setText(strip_tags(html_entity_decode($msg, ENT_QUOTES, 'UTF-8')));
 			$mail->send();
 			//get success_page
-			if($form['success_page']){
+			if ($form['success_page']){
 				$success_url = $this->html->getSecureURL($form['success_page']);
-			}else{
+			} else{
 				$success_url = $this->html->getSecureURL('content/contact/success');
 			}
 			$this->redirect($success_url);
 		}
 
 		if ($this->request->is_POST()){
-		  foreach($this->request->post as $name => $value){
-			  $this->form->assign($name,$value);
-		  }
+			foreach ($this->request->post as $name => $value){
+				$this->form->assign($name, $value);
+			}
 		}
 
 		$this->document->resetBreadcrumbs();
 
-		$this->document->addBreadcrumb( array (
-			'href'      => $this->html->getURL('index/home'),
-			'text'      => $this->language->get('text_home'),
-			'separator' => FALSE
-		 ));
+		$this->document->addBreadcrumb(array (
+				'href'      => $this->html->getURL('index/home'),
+				'text'      => $this->language->get('text_home'),
+				'separator' => false
+		));
 
-		$this->document->addBreadcrumb( array (
-			'href'      => $this->html->getURL('content/contact'),
-			'text'      => $this->language->get('heading_title'),
-			'separator' => $this->language->get('text_separator')
-		 ));
+		$this->document->addBreadcrumb(array (
+				'href'      => $this->html->getURL('content/contact'),
+				'text'      => $this->language->get('heading_title'),
+				'separator' => $this->language->get('text_separator')
+		));
 
-		$this->view->assign('form_output', $this->form->getFormHtml() );
+		$this->view->assign('form_output', $this->form->getFormHtml());
 
 		$this->view->assign('action', $this->html->getURL('content/contact'));
 		$this->view->assign('store', $this->config->get('store_name'));
-		$this->view->assign('address', nl2br($this->config->get('config_address')) );
-		$this->view->assign('telephone', $this->config->get('config_telephone') );
-		$this->view->assign('fax', $this->config->get('config_fax') );
-	
-		$this->processTemplate('pages/content/contact.tpl' );
+		$this->view->assign('address', nl2br($this->config->get('config_address')));
+		$this->view->assign('telephone', $this->config->get('config_telephone'));
+		$this->view->assign('fax', $this->config->get('config_fax'));
+
+		$this->processTemplate('pages/content/contact.tpl');
 
 		//init controller data
-		$this->extensions->hk_UpdateData($this,__FUNCTION__);
+		$this->extensions->hk_UpdateData($this, __FUNCTION__);
 	}
 
-	public function success() {
+	public function success(){
 
 		//init controller data
-		$this->extensions->hk_InitData($this,__FUNCTION__);
+		$this->extensions->hk_InitData($this, __FUNCTION__);
 
-		$this->document->setTitle( $this->language->get('heading_title') ); 
+		$this->document->setTitle($this->language->get('heading_title'));
 
 		$this->document->resetBreadcrumbs();
 
-		$this->document->addBreadcrumb( array (
-			'href'      => $this->html->getURL('index/home'),
-			'text'      => $this->language->get('text_home'),
-			'separator' => FALSE
-		 ));
+		$this->document->addBreadcrumb(array (
+				'href'      => $this->html->getURL('index/home'),
+				'text'      => $this->language->get('text_home'),
+				'separator' => false
+		));
 
-		$this->document->addBreadcrumb( array (
-			'href'      => $this->html->getURL('content/contact'),
-			'text'      => $this->language->get('heading_title'),
-			'separator' => $this->language->get('text_separator')
-		 ));
+		$this->document->addBreadcrumb(array (
+				'href'      => $this->html->getURL('content/contact'),
+				'text'      => $this->language->get('heading_title'),
+				'separator' => $this->language->get('text_separator')
+		));
 
-		if($this->config->get('embed_mode') == true){
+		if ($this->config->get('embed_mode') == true){
 			$continue_url = $this->html->getURL('product/category');
-		}else{
+		} else{
 			$continue_url = $this->html->getURL('index/home');
 		}
 
 		$this->view->assign('continue', $continue_url);
 
-		$continue = HtmlElementFactory::create( array ('type' => 'button',
-													   'name' => 'continue_button',
-													   'text'=> $this->language->get('button_continue'),
-													   'style' => 'button'));
+		$continue = HtmlElementFactory::create(array ('type'  => 'button',
+		                                              'name'  => 'continue_button',
+		                                              'text'  => $this->language->get('button_continue'),
+		                                              'style' => 'button'));
 		$this->view->assign('continue_button', $continue);
 
-		if($this->config->get('embed_mode') == true){
+		if ($this->config->get('embed_mode') == true){
 			//load special headers
 			$this->addChild('responses/embed/head', 'head');
 			$this->addChild('responses/embed/footer', 'footer');
 			$this->processTemplate('embed/common/success.tpl');
-		} else {
+		} else{
 			$this->processTemplate('common/success.tpl');
 		}
 
 		//init controller data
-		$this->extensions->hk_UpdateData($this,__FUNCTION__);
+		$this->extensions->hk_UpdateData($this, __FUNCTION__);
 	}
 
 	/**
 	 * @return bool
 	 */
-	private function _validate() {
-		$this->error = array_merge($this->form->validateFormData($this->request->post),$this->error);
-		if (!$this->error) {
-			return TRUE;
-		} else {
+	private function _validate(){
+		$this->error = array_merge($this->form->validateFormData($this->request->post), $this->error);
+		if (!$this->error){
+			return true;
+		} else{
 			$this->form->setErrors($this->error);
-			return FALSE;
-		}  	  
+			return false;
+		}
 	}
 }
