b30d3f69b80187a414ff7a7e153ead9b0d8c1e67
abolabo <abolabo@gmail.com>
2016-03-04 00:39:46 +0200

    https://github.com/abantecart/abantecart-src/issues/469

--- public_html/admin/model/sale/customer_transaction.php
+++ public_html/admin/model/sale/customer_transaction.php
@@ -23,6 +23,7 @@ if (! defined ( 'DIR_CORE' ) || !IS_ADMIN) {
 /**
  * Class ModelSaleCustomerTransaction
  * @property ModelSaleCustomer $model_sale_customer
+ * @property ModelSettingStore $model_setting_store
  */
 class ModelSaleCustomerTransaction extends Model {
 		
@@ -159,25 +160,85 @@ class ModelSaleCustomerTransaction extends Model {
     }
 
     public function addCustomerTransaction($data=array()){
-        if(((float)$data['credit'] || (float)$data['debit']) && (int)$data['customer_id']){
-            $sql = "INSERT INTO " . $this->db->table("customer_transactions") . " (`customer_id`,`order_id`,`created_by`,`credit`,`debit`,`section`, `transaction_type`,`comment`,`description`,`date_added`)
-					VALUES (
-							'".(int)$data['customer_id']."',
-							'".(int)$data['order_id']."',
-							'".$this->user->getId()."',
-							'".(float)$data['credit']."',
-							'".(float)$data['debit']."',
-							'1',
-							'".$this->db->escape($data['transaction_type'])."',
-							'".$this->db->escape($data['comment'])."',
-							'".$this->db->escape($data['description'])."',
-							NOW()
-							)";
-            $this->db->query($sql);
-            $this->cache->delete('balance.'.(int)$data['customer_id']);
+        if((!(float)$data['credit'] && !(float)$data['debit']) || !(int)$data['customer_id']){
+            return false;
+        }
+        $sql = "INSERT INTO " . $this->db->table("customer_transactions") . " (`customer_id`,`order_id`,`created_by`,`credit`,`debit`,`section`, `transaction_type`,`comment`,`description`,`date_added`)
+                VALUES (
+                        '".(int)$data['customer_id']."',
+                        '".(int)$data['order_id']."',
+                        '".$this->user->getId()."',
+                        '".(float)$data['credit']."',
+                        '".(float)$data['debit']."',
+                        '1',
+                        '".$this->db->escape($data['transaction_type'])."',
+                        '".$this->db->escape($data['comment'])."',
+                        '".$this->db->escape($data['description'])."',
+                        NOW()
+                        )";
+        $this->db->query($sql);
+        $this->cache->delete('balance.'.(int)$data['customer_id']);
+        $transaction_id = $this->db->getLastId();
+
+        if($data['notify']){
+
+            $this->load->model('sale/customer');
+            $customer_info = $this->model_sale_customer->getCustomer($data['customer_id']);
+
+            if($customer_info){
+                //detect customer's language
+                $sql = "SELECT language_id
+                        FROM ". $this->db->table('orders')."
+                        WHERE customer_id = '".(int)$data['customer_id']."'
+                        ORDER BY date_added DESC";
+                $result = $this->db->query($sql);
+                $language_code = '';
+                if($result->row['language_id']){
+                    $lang = $this->language->getLanguageDetailsByID($result->row['language_id']);
+                    $language_code = $lang['code'];
+                }
+
+                if(!$language_code){
+                    $language_code = $this->language->getDefaultLanguageCode();
+                }
+
+                //load language specific for the order in admin section
+                $language = new ALanguage(Registry::getInstance(), $language_code, 1);
+                $language->load('sale/customer');
+
+                $this->load->model('setting/store');
+
+                $store_info = $this->model_setting_store->getStore((int)$this->session->data['current_store_id']);
+
+                $subject = sprintf($language->get('text_transaction_notification_subject'), $store_info['store_name']);
+
+                $url = html_entity_decode($store_info['config_url'] . 'index.php?rt=account/transactions', ENT_QUOTES, 'UTF-8');
+	            $amount = $this->currency->format($data['credit']);
+                $message = sprintf($language->get('text_transaction_notification_message'),
+                                    $store_info['store_name'],
+                                    $amount,
+				                    $store_info['store_name'])."\n\n" ;
+	            $message .= $url."\n\n";
+                $message .= $data['description'];
+                $mail = new AMail($this->config);
+                $mail->setTo($customer_info['email']);
+                $mail->setFrom($store_info['store_main_email']);
+                $mail->setSender($store_info['store_name']);
+                $mail->setSubject($subject);
+                $mail->setText(html_entity_decode($message, ENT_QUOTES, 'UTF-8'));
+                $mail->send();
+
+                $this->im->sendToCustomer(
+		                $data['customer_id'],
+		                'customer_account_update',
+		                array($store_info['store_name'], $amount, $store_info['store_name']));
+
+            }
+
         }
 
-        return $this->db->getLastId();
+
+        return $transaction_id;
     }
 
 
