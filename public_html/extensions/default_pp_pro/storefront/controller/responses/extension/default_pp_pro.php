d6023fff5779dacc1190051fec071389849b7aa4
abolabo <abolabo@gmail.com>
2016-03-10 12:43:47 +0200

    https://github.com/abantecart/abantecart-src/issues/483

--- public_html/extensions/default_pp_pro/storefront/controller/responses/extension/default_pp_pro.php
+++ public_html/extensions/default_pp_pro/storefront/controller/responses/extension/default_pp_pro.php
@@ -259,43 +259,45 @@ class ControllerResponsesExtensionDefaultPPPro extends AController {
 
 		$response = curl_exec($curl);
 
- 
-		if (!$response) {
-			exit('DoDirectPayment failed: ' . curl_error($curl) . '(' . curl_errno($curl) . ')');
-		}
-		curl_close($curl);
- 		$response_data = array();
-
-		parse_str($response, $response_data);
 		$json = array();
-		if (($response_data['ACK'] == 'Success') || ($response_data['ACK'] == 'SuccessWithWarning')) {
-			$this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('config_order_status_id'));
-			
-			$message = '';
-			
-			if (isset($response_data['AVSCODE'])) {
-				$message .= 'AVSCODE: ' . $response_data['AVSCODE'] . "\n";
+		if (!$response) {
+			$json['error'] = 'Cannot establish a connection to the server';
+			$err = new AError('Paypal Pro Error: DoDirectPayment failed: ' . curl_error($curl) . '(' . curl_errno($curl) . ')');
+			$err->toLog()->toMessages()->toDebug();
+		}else{
+			curl_close($curl);
+			$response_data = array ();
+
+			parse_str($response, $response_data);
+
+			if (($response_data['ACK'] == 'Success') || ($response_data['ACK'] == 'SuccessWithWarning')){
+				$this->model_checkout_order->confirm($this->session->data['order_id'], $this->config->get('config_order_status_id'));
+
+				$message = '';
+
+				if (isset($response_data['AVSCODE'])){
+					$message .= 'AVSCODE: ' . $response_data['AVSCODE'] . "\n";
+				}
+
+				if (isset($response_data['CVV2MATCH'])){
+					$message .= 'CVV2MATCH: ' . $response_data['CVV2MATCH'] . "\n";
+				}
+
+				if (isset($response_data['TRANSACTIONID'])){
+					$message .= 'TRANSACTIONID: ' . $response_data['TRANSACTIONID'] . "\n";
+				}
+
+				$response_data['PAYMENTACTION'] = $payment_type;
+				$response_data['payment_method'] = 'default_pp_pro';
+
+				$this->model_checkout_order->updatePaymentMethodData($this->session->data['order_id'], serialize($response_data));
+				$this->model_checkout_order->update($this->session->data['order_id'], $this->config->get('default_pp_pro_order_status_id'), $message, false);
+
+				$json['success'] = $this->html->getSecureURL('checkout/success');
+			} else{
+				$json['error'] = $response_data['L_LONGMESSAGE0'];
 			}
-
-			if (isset($response_data['CVV2MATCH'])) {
-				$message .= 'CVV2MATCH: ' . $response_data['CVV2MATCH'] . "\n";
-			}
-
-			if (isset($response_data['TRANSACTIONID'])) {
-				$message .= 'TRANSACTIONID: ' . $response_data['TRANSACTIONID'] . "\n";
-			}
-
-			$response_data['PAYMENTACTION'] = $payment_type;
-			$response_data['payment_method'] = 'default_pp_pro';
-
-			$this->model_checkout_order->updatePaymentMethodData($this->session->data['order_id'], serialize($response_data));
-			$this->model_checkout_order->update($this->session->data['order_id'], $this->config->get('default_pp_pro_order_status_id'), $message, FALSE);
-		
-			$json['success'] = $this->html->getSecureURL('checkout/success');
-		} else {
-        	$json['error'] = $response_data['L_LONGMESSAGE0'];
-        }
-
+		}
 		$this->load->library('json');
 		$this->response->setOutput(AJson::encode($json));
 	}
