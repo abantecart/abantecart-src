0cb6456b06f3ad6d3ddd1a05dc5bf348aa02260c
abantecart <projkov@abantecart.com>
2016-08-09 21:28:42 -0400

    Storefront header controller cleanup

--- public_html/storefront/controller/common/header.php
+++ public_html/storefront/controller/common/header.php
@@ -27,9 +27,6 @@ class ControllerCommonHeader extends AController {
 		//init controller data
         $this->extensions->hk_InitData($this,__FUNCTION__);
 
-		$this->data['title'] = $this->document->getTitle();
-        $this->data['template'] = $this->config->get('config_storefront_template');
-		$this->data['breadcrumbs'] = $this->document->getBreadcrumbs();
 		$this->data['store'] = $this->config->get('store_name');
         $this->data['logo'] = $this->config->get('config_logo');
 		$logo_path = DIR_RESOURCE . $this->data['logo'];
@@ -51,78 +48,7 @@ class ControllerCommonHeader extends AController {
 		$info = get_image_size($logo_path);
 		$this->data['logo_width'] = $info['width'];
 		$this->data['logo_height'] = $info['height'];
-        
-		$this->data['text_special'] = $this->language->get('text_special');
-		$this->data['text_contact'] = $this->language->get('text_contact');
-		$this->data['text_sitemap'] = $this->language->get('text_sitemap');
-		$this->data['text_bookmark'] = $this->language->get('text_bookmark');
-		$this->data['text_category'] = $this->language->get('text_category');
-		$this->data['text_advanced'] = $this->language->get('text_advanced');
-		$this->data['entry_search'] = $this->language->get('entry_search');
-		
-		$this->data['button_go'] = $this->language->get('button_go');
-
-		$this->data['homepage'] = HTTPS===true ? $this->config->get('config_ssl_url') : $this->config->get('config_url');
-		$this->data['special'] = $this->html->getURL('product/special');
-		$this->data['contact'] = $this->html->getURL('content/contact');
-    	$this->data['sitemap'] = $this->html->getURL('content/sitemap');
-		$this->data['logged'] = $this->customer->isLogged();
-		$this->data['checkout'] = $this->html->getSecureURL('checkout/shipping');
-
-		if (isset($this->request->get['category_id'])) {
-			$this->data['category_id'] = $this->request->get['category_id'];
-		} elseif (isset($this->request->get['path'])) {
-			$path = explode('_', $this->request->get['path']);
-			$this->data['category_id'] = end($path);
-		} else {
-			$this->data['category_id'] = '';
-		}
-		
-		$this->data['advanced'] = $this->html->getURL('product/search');
-		$this->data['action'] = $this->html->getURL('index/home');
-
-		if (!isset($this->request->get['rt'])) {
-			$this->data['redirect'] = $this->html->getURL('index/home');
-		} else {
-			$this->loadModel('tool/seo_url');
-			
-			$data = $this->request->get;
-			unset($data['_route_']);
-			$route = $data['rt'];
-			unset($data['rt']);
-			
-			$url = '';
-			if ($data) {
-				$url = '&' . urldecode(http_build_query($data));
-			}
-			$this->data['redirect'] = $this->html->getSEOURL( $route,  $url, '&encode');
-		}
-		
-		$this->data['language_code'] = $this->session->data['language'];
-		
-		$this->data['languages'] = array();
-		$this->data['languages'] = $this->language->getActiveLanguages();	
-		
-		$this->data['currency_code'] = $this->currency->getCode();
-		$this->loadModel('localisation/currency');
-		$this->data['currencies'] = array();
-
-		$this->data['search'] = $this->html->buildInput(
-										array (
-			                                    'name'=>'filter_keyword',
-			                                    'value'=> (isset($this->request->get['keyword']) ? $this->request->get['keyword'] : $this->language->get('text_keyword')),
-			                                    'attr'=> (!isset($this->request->get['keyword']) ? ' onclick="this.value=\'\'" ' : ''),
-
-										));
-		 
-		$results = $this->model_localisation_currency->getCurrencies();	
-		
-		foreach ($results as $result) {
-			if ($result['status']) {
-   				$this->data['currencies'] = array('title' => $result['title'],
-												  'code'  => $result['code']);
-			}
-		}
+        						
 		$this->view->batchAssign($this->data);
 		$this->processTemplate('common/header.tpl');
         //init controller data
