fb8943329e9d15ba7c676c678ba68c03cc35489c
BasaraABC <beremenko@abantecart.com>
2017-03-21 17:52:17 +0700

    Czech locale

--- public_html/extensions/default_czech/install.php
+++ public_html/extensions/default_czech/install.php
@@ -13,7 +13,7 @@ if (! defined ( 'DIR_CORE' )) {
 $lng_code = "cs";
 $lng_name = "Čeština";
 $lng_directory = "czech";
-$lng_locale = "cs_CS.UTF-8,cs_CS,cs-cs,czech";
+$lng_locale = "cs-CZ.UTF-8,cs_CZ,cs-cz,czech";
 $lng_flag_path = "extensions/default_czech/storefront/language/czech/flag.png";
 $lng_sort = 2; // sorting order with other languages
 $lng_status = 0; // Status on installation of extension
@@ -28,12 +28,12 @@ if ($query->row["language_id"]) {
 	return false;
 }
 
-$this->db->query("INSERT INTO ".$this->db->table("languages")." 
+$this->db->query("INSERT INTO ".$this->db->table("languages")."
 				(`name`,`code`,`locale`,`image`,`directory`,`filename`,`sort_order`, `status`)
 				VALUES (
-				'".$this->db->escape($lng_name)."', 
-				'".$this->db->escape($lng_code)."', 
-				'".$this->db->escape($lng_locale)."', 
+				'".$this->db->escape($lng_name)."',
+				'".$this->db->escape($lng_code)."',
+				'".$this->db->escape($lng_locale)."',
 				'".$this->db->escape($lng_flag_path)."',
 				'".$this->db->escape($lng_directory)."',
 				'".$lng_directory."',
