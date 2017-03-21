7c952ff5d040fbf49551af37230152830dbb1519
BasaraABC <beremenko@abantecart.com>
2017-03-21 17:50:04 +0700

    locale fix

--- public_html/extensions/default_hebrew/install.php
+++ public_html/extensions/default_hebrew/install.php
@@ -13,7 +13,7 @@ if (! defined ( 'DIR_CORE' )) {
 $lng_code = "he";
 $lng_name = "עברית";
 $lng_directory = "hebrew";
-$lng_locale = "he_HE.UTF-8,he_HE,he-he,hebrew";
+$lng_locale = "he-il.UTF-8,he_IL,he-il,hebrew";
 $lng_flag_path = "extensions/default_hebrew/storefront/language/hebrew/flag.png";
 $lng_sort = 2; // sorting order with other languages
 $lng_status = 0; // Status on installation of extension
@@ -28,12 +28,12 @@ if ($query->row["language_id"]) {
 	return false;
 }
 
-$this->db->query("INSERT INTO ".$this->db->table("languages")." 
+$this->db->query("INSERT INTO ".$this->db->table("languages")."
 				(`name`,`code`,`locale`,`image`,`directory`,`filename`,`sort_order`, `status`)
 				VALUES (
-				'".$this->db->escape($lng_name)."', 
-				'".$this->db->escape($lng_code)."', 
-				'".$this->db->escape($lng_locale)."', 
+				'".$this->db->escape($lng_name)."',
+				'".$this->db->escape($lng_code)."',
+				'".$this->db->escape($lng_locale)."',
 				'".$this->db->escape($lng_flag_path)."',
 				'".$this->db->escape($lng_directory)."',
 				'".$lng_directory."',
