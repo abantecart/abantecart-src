e3e417bdfde239182628d2e87c405d356231014f
abolabo <abolabo@gmail.com>
2017-03-21 17:28:54 +0200

    #763

--- public_html/storefront/controller/pages/checkout/success.php
+++ public_html/storefront/controller/pages/checkout/success.php
@@ -17,7 +17,7 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if (!defined('DIR_CORE')){
+if (!defined('DIR_CORE')) {
 	header('Location: static_pages/');
 }
 
@@ -31,28 +31,29 @@ class ControllerPagesCheckoutSuccess extends AController{
 		$this->extensions->hk_InitData($this, __FUNCTION__);
 		$order_id = (int)$this->session->data['order_id'];
 
-		if ($order_id && $this->validate($order_id)){
+		if ($order_id && $this->validate($order_id)) {
 
 			// in default currency
 			$amount = $this->session->data['used_balance'];
-			if ($amount){
+			if ($amount) {
 				$transaction_data = array (
 						'order_id'         => $order_id,
 						'amount'           => $amount,
 						'transaction_type' => 'order',
 						'created_by'       => $this->customer->getId(),
 						'description'      => sprintf($this->language->get('text_applied_balance_to_order'),
-														$this->currency->format($this->currency->convert($amount,
-																										$this->config->get('config_currency'),
-																										$this->session->data['currency']),
-																				$this->session->data['currency'], 1),
-													$order_id)
+								$this->currency->format($this->currency->convert($amount,
+										$this->config->get('config_currency'),
+										$this->session->data['currency']),
+										$this->session->data['currency'], 1),
+								$order_id)
 				);
 
 				try{
 					$this->customer->debitTransaction($transaction_data);
-				} catch(AException $e){
-					$error = new AError('Error: Debit transaction cannot be applied.' . var_export($transaction_data, true) . "\n" . $e->getMessage() . "\n" . $e->getFile());
+				} catch (AException $e){
+					$error = new AError('Error: Debit transaction cannot be applied.' . var_export($transaction_data,
+									true) . "\n" . $e->getMessage() . "\n" . $e->getFile());
 					$error->toLog()->toMessages();
 				}
 			}
@@ -66,16 +67,16 @@ class ControllerPagesCheckoutSuccess extends AController{
 			//Redirect back to load new page with cleared shopping cart content
 			redirect($this->html->getSecureURL('checkout/success'));
 		} //when validation failed
-		elseif ($order_id){
+		elseif ($order_id) {
 			$this->session->data['processed_order_id'] = $order_id;
-		} else{
+		} else {
 			$order_id = $this->session->data['processed_order_id'];
 		}
 
 		//check if payment was processed
-		if (!(int)$this->session->data['processed_order_id']){
+		if (!(int)$this->session->data['processed_order_id']) {
 			redirect($this->html->getURL('index/home'));
-		} elseif (!$order_id && (int)$this->session->data['processed_order_id']){
+		} elseif (!$order_id && (int)$this->session->data['processed_order_id']) {
 			$order_id = (int)$this->session->data['processed_order_id'];
 		}
 		unset($this->session->data['processed_order_id']);
@@ -97,7 +98,7 @@ class ControllerPagesCheckoutSuccess extends AController{
 				'separator' => $this->language->get('text_separator')
 		));
 
-		if ($this->customer->isLogged()){
+		if ($this->customer->isLogged()) {
 			$this->document->addBreadcrumb(array (
 					'href'      => $this->html->getSecureURL('checkout/shipping'),
 					'text'      => $this->language->get('text_shipping'),
@@ -115,7 +116,7 @@ class ControllerPagesCheckoutSuccess extends AController{
 					'text'      => $this->language->get('text_confirm'),
 					'separator' => $this->language->get('text_separator')
 			));
-		} else{
+		} else {
 			$this->document->addBreadcrumb(array (
 					'href'      => $this->html->getSecureURL('checkout/guest'),
 					'text'      => $this->language->get('text_guest'),
@@ -137,27 +138,27 @@ class ControllerPagesCheckoutSuccess extends AController{
 
 		$this->loadModel('account/order');
 		$order_info = $this->model_account_order->getOrder($order_id);
-		if ($order_info){
+		if ($order_info) {
 			$order_info['order_products'] = $this->model_account_order->getOrderProducts($order_id);
 		}
 
 		$order_totals = $this->model_account_order->getOrderTotals($order_id);
 		$this->_google_analytics($order_info, $order_totals);
 
-		if ($this->errors){
+		if ($this->errors) {
 			$this->view->assign('text_message', implode('<br>', $this->errors));
-		} elseif ($this->session->data['account'] == 'guest'){
+		} elseif ($this->session->data['account'] == 'guest') {
 			//give link on order page for quest
 			$enc = new AEncryption($this->config->get('encryption_key'));
 			$order_token = $enc->encrypt($order_id . '::' . $order_info['email']);
 			$order_url = $this->html->getSecureURL('account/invoice', '&ot=' . $order_token);
 			$this->view->assign('text_message',
-								sprintf($this->language->get('text_message_guest'),
-										$order_url,
-										$this->html->getURL('content/contact')
-								)
+					sprintf($this->language->get('text_message_guest'),
+							$order_url,
+							$this->html->getURL('content/contact')
+					)
 			);
-		} else{
+		} else {
 			$text_message = sprintf($this->language->get('text_message_account'),
 					$order_id,
 					$this->html->getSecureURL('account/invoice', '&order_id=' . $order_id),
@@ -172,17 +173,18 @@ class ControllerPagesCheckoutSuccess extends AController{
 						'type'  => 'button',
 						'name'  => 'continue_button',
 						'text'  => $this->language->get('button_continue'),
-						'style' => 'button'));
+						'style' => 'button'
+				));
 		$this->view->assign('continue_button', $continue);
 		//clear session anyway
 		$this->clearOrderSession();
 
-		if ($this->config->get('embed_mode') == true){
+		if ($this->config->get('embed_mode') == true) {
 			//load special headers
 			$this->addChild('responses/embed/head', 'head');
 			$this->addChild('responses/embed/footer', 'footer');
 			$this->processTemplate('embed/common/success.tpl');
-		} else{
+		} else {
 			$this->processTemplate('common/success.tpl');
 		}
 
@@ -190,13 +192,18 @@ class ControllerPagesCheckoutSuccess extends AController{
 		$this->extensions->hk_UpdateData($this, __FUNCTION__);
 	}
 
+	/**
+	 * Validating order data for different cases
+	 * @param int $order_id
+	 * @return bool
+	 */
 	protected function validate($order_id){
 
 		//check is order incomplete
 		$this->loadModel('checkout/order');
 		$order_info = $this->model_checkout_order->getOrder($order_id);
-		//when order exists but still incomplete by some reasons
-		if ((int)$order_info['order_status_id'] == 0){
+		//when order exists but still incomplete by some reasons - mark it as failed
+		if ((int)$order_info['order_status_id'] == $this->order_status->getStatusByTextId('incomplete')) {
 			$new_status_id = $this->order_status->getStatusByTextId('failed');
 			$this->model_checkout_order->confirm($order_id, $new_status_id);
 			$this->messages->saveWarning(
@@ -207,11 +214,12 @@ class ControllerPagesCheckoutSuccess extends AController{
 			$this->errors[] = $text_message;
 		}
 
+		//perform additional custom order validation in extensions
 		$this->extensions->hk_ValidateData($this);
 
-		if ($this->errors){
+		if ($this->errors) {
 			return false;
-		} else{
+		} else {
 			return true;
 		}
 	}
@@ -238,23 +246,23 @@ class ControllerPagesCheckoutSuccess extends AController{
 
 		// google analytics data for js-script in footer.tpl
 		$order_tax = $order_total = $order_shipping = 0.0;
-		foreach ($order_totals as $i => $total){
-			if ($total['type'] == 'total'){
+		foreach ($order_totals as $i => $total) {
+			if ($total['type'] == 'total') {
 				$order_total += $total['value'];
-			} elseif ($total['type'] == 'tax'){
+			} elseif ($total['type'] == 'tax') {
 				$order_tax += $total['value'];
-			} elseif ($total['type'] == 'shipping'){
+			} elseif ($total['type'] == 'shipping') {
 				$order_shipping += $total['value'];
 			}
 		}
 
-		if (!$order_data['shipping_city']){
+		if (!$order_data['shipping_city']) {
 			$addr = array (
 					'city'    => $order_data['payment_city'],
 					'state'   => $order_data['payment_zone'],
 					'country' => $order_data['payment_country']
 			);
-		} else{
+		} else {
 			$addr = array (
 					'city'    => $order_data['shipping_city'],
 					'state'   => $order_data['shipping_zone'],
@@ -263,17 +271,18 @@ class ControllerPagesCheckoutSuccess extends AController{
 		}
 
 		$ga_data = array_merge(
-				array ('transaction_id' => (int)$order_data['order_id'],
-				       'store_name'     => $this->config->get('store_name'),
-				       'currency_code'  => $order_data['currency'],
-				       'total'          => $this->currency->format_number($order_total),
-				       'tax'            => $this->currency->format_number($order_tax),
-				       'shipping'       => $this->currency->format_number($order_shipping)
+				array (
+						'transaction_id' => (int)$order_data['order_id'],
+						'store_name'     => $this->config->get('store_name'),
+						'currency_code'  => $order_data['currency'],
+						'total'          => $this->currency->format_number($order_total),
+						'tax'            => $this->currency->format_number($order_tax),
+						'shipping'       => $this->currency->format_number($order_shipping)
 				), $addr);
 
-		if ($order_data['order_products']){
+		if ($order_data['order_products']) {
 			$ga_data['items'] = array ();
-			foreach ($order_data['order_products'] as $product){
+			foreach ($order_data['order_products'] as $product) {
 				$ga_data['items'][] = array (
 						'id'       => (int)$order_data['order_id'],
 						'name'     => $product['name'],
