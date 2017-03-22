1daa8989d5325c1372d6948f7cf120eca6e7a005
BasaraABC <beremenko@abantecart.com>
2017-03-22 18:04:44 +0700

    Gaeilge (Irish) language

---
+++ public_html/extensions/default_irish/uninstall.php
@@ -0,0 +1,30 @@
+<?php
+
+
+if (! defined ( 'DIR_CORE' )) {
+ header ( 'Location: static_pages/' );
+}
+
+
+
+
+		
+$language_code = "ga";
+$language_directory = "irish";
+
+$query = $this->db->query(
+	"SELECT language_id FROM ".$this->db->table("languages")." 
+	WHERE code='".$language_code."' AND directory='".$language_directory."'");
+$language_id = $query->row["language_id"];
+//delete menu
+$storefront_menu = new AMenu_Storefront();
+$storefront_menu->deleteLanguage($language_id);
+
+//delete all other language related tables
+$lm = new ALanguageManager($this->registry, $language_code);
+$lm->deleteAllLanguageEntries($language_id);
+
+//delete language
+$this->db->query("DELETE FROM ".$this->db->table("languages")." WHERE `code`='".$language_code."'");
+
+$this->cache->remove("localization");
