ed609e97427e665b35e7cd1b065e89132ac87d40
abolabo <abolabo@gmail.com>
2017-03-27 18:18:51 +0300

    #813

--- public_html/core/database/apdomysql.php
+++ public_html/core/database/apdomysql.php
@@ -20,7 +20,14 @@ final class APDOMySQL{
 		} catch(AException $e){
 			$err = new AError('Cannot establish database connection to ' . $database.' using ' . $username . '@' . $hostname);
 			$err->toLog();
-            throw new AException(AC_ERR_MYSQL, 'Cannot establish database connection. Check your database setting in configuration file.');
+
+			$message = 'Cannot establish database connection. ';
+			if(filesize(DIR_SYSTEM.'config.php')){
+				$message .= 'Check your database setting in configuration file.';
+			}else{
+				$message .= 'Check your database credentials.';
+			}
+            throw new AException(AC_ERR_MYSQL, $message);
 		}
 		$this->registry = Registry::getInstance();
 
--- public_html/core/database/mysql.php
+++ public_html/core/database/mysql.php
@@ -47,7 +47,13 @@ final class MySQL {
 		if (!$connection) {
 			$err = new AError('Cannot establish database connection to ' . $database.' using ' . $username . '@' . $hostname);
 			$err->toLog();
-            throw new AException(AC_ERR_MYSQL, 'Cannot establish database connection. Check your database setting in configuration file.');
+			$message = 'Cannot establish database connection. ';
+			if(filesize(DIR_SYSTEM.'config.php')){
+				$message .= 'Check your database setting in configuration file.';
+			}else{
+				$message .= 'Check your database credentials.';
+			}
+            throw new AException(AC_ERR_MYSQL, $message);
     	}
 
     	if (!mysql_select_db($database, $connection)) {
