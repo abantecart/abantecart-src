f2e48df5f2db61c2de3115a0c1e60a1ea8e3d8ff
abolabo <abolabo@gmail.com>
2017-05-03 13:05:06 +0300

    csrf-token check fix

--- public_html/core/lib/csrf_token.php
+++ public_html/core/lib/csrf_token.php
@@ -34,19 +34,19 @@ class CSRFToken{
 	 * @var registry - access to application registry
 	 */
 	protected $registry;
-    /**
-     * @var string CSRF-token
-     */
-    private $token;
-    /**
-     * @var int CSRF-token instance
-     */
-    private $instance;
+	/**
+	 * @var string CSRF-token
+	 */
+	private $token;
+	/**
+	 * @var int CSRF-token instance
+	 */
+	private $instance;
 
 	public function __construct(){
 		$this->registry = Registry::getInstance();
 		$this->errors = array ();
-        $this->token = $this->session->data['csrftoken'];
+		$this->token = $this->session->data['csrftoken'];
 	}
 
 	/**
@@ -60,63 +60,64 @@ class CSRFToken{
 	/**
 	 * @param  string $key - key to save data in registry
 	 * @param  mixed $value - key to save data in registry
-	 * @return mixed  - data from registry
+	 * @void
 	 */
 	public function __set($key, $value){
 		$this->registry->set($key, $value);
 	}
 
-    /**
-     * set CSRF Token Instance
-     *
-     * @param void
-     * @return string
-     */
-    public function setInstance(){
-        $this->instance = 0;
-        if(is_array($this->session->data['csrftoken']) && count($this->session->data['csrftoken'])) {
-            end($this->session->data['csrftoken']);
-            $this->instance = key($this->session->data['csrftoken']) + 1;
-        }
-        return $this->instance;
-    }
+	/**
+	 * set CSRF Token Instance
+	 *
+	 * @param void
+	 * @return string
+	 */
+	public function setInstance(){
+		$this->instance = 0;
+		if(is_array($this->session->data['csrftoken']) && count($this->session->data['csrftoken'])) {
+			end($this->session->data['csrftoken']);
+			$this->instance = key($this->session->data['csrftoken']) + 1;
+		}
+		return $this->instance;
+	}
 
-    /**
-     * set CSRF Token
-     *
-     * @param void
-     * @return string
-     */
-    public function setToken(){
-        if(!$this->instance) {
-            //create new token instance
-            $this->instance = $this->setInstance();
-        }
-        $this->token = genToken();
-        $this->session->data['csrftoken'][$this->instance] = $this->token;
-        return $this->token;
-    }
+	/**
+	 * set CSRF Token
+	 *
+	 * @param void
+	 * @return string
+	 */
+	public function setToken(){
+		if(!$this->instance) {
+			//create new token instance
+			$this->instance = $this->setInstance();
+		}
+		$this->token = genToken();
 
-    /**
-     * Validate CSRF Token. Can be validated only one time
-     *
-     * @param int
-     * @param string
-     * @return bool
-     */
-    public function isTokenValid($instance = 0, $token = ''){
-        if(!$instance && !$token) {
-            $instance = $this->request->get_or_post('csrfinstance');
-            $token = $this->request->get_or_post('csrftoken');
-        }
-        if(!empty($token) && !empty($instance) && $this->session->data['csrftoken'][$instance] === $token){
-            $this->instance = $instance;
-            $this->token = $this->session->data['csrftoken'][$instance];
-            unset($this->session->data['csrftoken'][$instance]);
-            return true;
-        } else {
-            return false;
-        }
-    }
+		$this->session->data['csrftoken'][$this->instance] = $this->token;
+		return $this->token;
+	}
 
+	/**
+	 * Validate CSRF Token. Can be validated only one time
+	 *
+	 * @param int
+	 * @param string
+	 * @return bool
+	 */
+	public function isTokenValid($instance = 0, $token = ''){
+		if(!$instance && !$token) {
+			$instance = $this->request->get_or_post('csrfinstance');
+			$token = $this->request->get_or_post('csrftoken');
+		}
+		//note: $instance can be zero!
+		if(!empty($token) && $instance !== null && $this->session->data['csrftoken'][$instance] === $token){
+			$this->instance = $instance;
+			$this->token = $this->session->data['csrftoken'][$instance];
+			unset($this->session->data['csrftoken'][$instance]);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
