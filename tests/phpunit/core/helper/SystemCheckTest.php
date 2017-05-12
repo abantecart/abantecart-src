1a9f3480cd0361a343e928d53d843e9f0d09e216
abolabo <abolabo@gmail.com>
2017-05-12 10:25:58 +0300

    travis-ci

--- tests/phpunit/core/helper/SystemCheckTest.php
+++ tests/phpunit/core/helper/SystemCheckTest.php
@@ -14,27 +14,27 @@ class SystemCheckTest extends AbanteCartTest{
 		ini_set('session.save_path', $this->original_save_path);
 	}
 
-	public function testCheckSessionSavePath(){
+	public function testCheckEmptySessionSavePath(){
 		ini_set('session.save_path', '');
 		$result = check_session_save_path();
-		var_Dump(ini_get('session.save_path'));
 		$this->assertEquals($result, array());
+	}
 
+	public function testCheckTmpSessionSavePath(){
 		ini_set('session.save_path', '/tmp');
-		var_Dump(ini_get('session.save_path'));
 		$result = check_session_save_path();
 		$this->assertEquals($result, array());
+	}
 
+	public function testCheckTmp2SessionSavePath(){
 		ini_set('session.save_path', '1;/tmp');
-		var_Dump(ini_get('session.save_path'));
 		$result = check_session_save_path();
 		$this->assertEquals($result, array());
+	}
 
-
+	public function testCheckTmp3SessionSavePath(){
 		ini_set('session.save_path', '1;0666;/tmp');
-		var_Dump(ini_get('session.save_path'));
 		$result = check_session_save_path();
 		$this->assertEquals($result, array());
-
 	}
 }
