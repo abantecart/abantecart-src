9ee6914b7c8a8b3d20778cbdab2ddcdedfee0f91
abolabo <abolabo@gmail.com>
2017-05-19 18:37:21 +0300

    #870

--- public_html/admin/model/catalog/product.php
+++ public_html/admin/model/catalog/product.php
@@ -196,10 +196,10 @@ class ModelCatalogProduct extends Model{
 	 */
 	public function addProductDiscount($product_id, $data){
 		$data['price'] = str_replace(" ", "", $data['price']);
-		if (!empty($data['date_start'])){
+		if (!empty($data['date_start']) && !$data['iso_date']){
 			$data['date_start'] = dateDisplay2ISO($data['date_start'], $this->language->get('date_format_short'));
 		}
-		if (!empty($data['date_end'])){
+		if (!empty($data['date_end']) && !$data['iso_date']){
 			$data['date_end'] = dateDisplay2ISO($data['date_end'], $this->language->get('date_format_short'));
 		}
 		$this->db->query(
@@ -223,10 +223,10 @@ class ModelCatalogProduct extends Model{
 	 */
 	public function addProductSpecial($product_id, $data){
 		$data['price'] = str_replace(" ", "", $data['price']);
-		if (!empty($data['date_start'])){
+		if (!empty($data['date_start']) && !$data['iso_date']){
 			$data['date_start'] = dateDisplay2ISO($data['date_start'], $this->language->get('date_format_short'));
 		}
-		if (!empty($data['date_end'])){
+		if (!empty($data['date_end']) && !$data['iso_date']){
 			$data['date_end'] = dateDisplay2ISO($data['date_end'], $this->language->get('date_format_short'));
 		}
 
@@ -1005,9 +1005,12 @@ class ModelCatalogProduct extends Model{
 		$new_product_id = $this->addProduct($data);
 
 		foreach ($data['product_discount'] as $item){
+			//sign to prevent converting date from display format to iso
+			$item['iso_date'] = true;
 			$this->addProductDiscount($new_product_id, $item);
 		}
 		foreach ($data['product_special'] as $item){
+			$item['iso_date'] = true;
 			$this->addProductSpecial($new_product_id, $item);
 		}
 
