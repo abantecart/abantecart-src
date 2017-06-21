ac26ff94b364dfdaeda29eec362cd5b59517b780
abolabo <abolabo@gmail.com>
2017-06-21 14:25:36 +0300

    #879

--- public_html/extensions/default_parcelforce_48/storefront/model/extension/default_parcelforce_48.php
+++ public_html/extensions/default_parcelforce_48/storefront/model/extension/default_parcelforce_48.php
@@ -8,7 +8,7 @@
   Copyright © 2011-2017 Belavier Commerce LLC
 
   This source file is subject to Open Software License (OSL 3.0)
-  Lincence details is bundled with this package in the file LICENSE.txt.
+  Licence details is bundled with this package in the file LICENSE.txt.
   It is also available at this URL:
   <http://www.opensource.org/licenses/OSL-3.0>
 
@@ -21,43 +21,43 @@ if (!defined('DIR_CORE')) {
 	header('Location: static_pages/');
 }
 
+/**
+ * Class ModelExtensionDefaultParcelforce48
+ * @property AWeight $weight
+ */
 class ModelExtensionDefaultParcelforce48 extends Model {
 	function getQuote($address) {
 		$this->load->language('default_parcelforce_48/default_parcelforce_48');
-
 		if ($this->config->get('default_parcelforce_48_status')) {
-
 			if (!$this->config->get('default_parcelforce_48_location_id')) {
 				$status = TRUE;
 			} else {
 				$query = $this->db->query("SELECT *
-                                            FROM " . $this->db->table('zones_to_locations') . "
-                                            WHERE location_id = '" . (int)$this->config->get('default_parcelforce_48_location_id') . "'
-                                                AND country_id = '" . (int)$address['country_id'] . "'
-                                                AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
-                if ($query->num_rows) {
-                    $status = TRUE;
-                } else {
-                    $status = FALSE;
-                }
+											FROM " . $this->db->table('zones_to_locations') . "
+											WHERE location_id = '" . (int)$this->config->get('default_parcelforce_48_location_id') . "'
+												AND country_id = '" . (int)$address['country_id'] . "'
+												AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+				if ($query->num_rows) {
+					$status = TRUE;
+				} else {
+					$status = FALSE;
+				}
 			}
 		} else {
 			$status = FALSE;
 		}
 
 		$method_data = array();
-
 		if (!$status) {
 			return $method_data;
 		}
-
 
 		$basic_products = $this->cart->basicShippingProducts();
 		$product_ids = array();
 		foreach ($basic_products as $product) {
 			$product_ids[] = $product['product_id'];
 		}
-
+		$weight = 0.0;
 		if($product_ids){
 			$weight = $this->weight->convert($this->cart->getWeight($product_ids), $this->config->get('config_weight_class'), 'kgs');
 		}
@@ -74,7 +74,7 @@ class ModelExtensionDefaultParcelforce48 extends Model {
 			$weight = $this->weight->convert($this->cart->getWeight(array($product['product_id'])), $this->config->get('config_weight_class'), 'kgs');
 			if ($product['shipping_price'] > 0) {
 				$fixed_cost = $product['shipping_price'];
-				//If ship individually count every quintaty
+				//If ship individually count every quantity
 				if ($product['ship_individually']) {
 					$fixed_cost = $fixed_cost * $product['quantity'];
 				}
@@ -96,41 +96,41 @@ class ModelExtensionDefaultParcelforce48 extends Model {
 				}
 
 				if (!$all_free_shipping) {
-				    $quote_data[$key]['text'] = $this->currency->format(
+					$quote_data[$key]['text'] = $this->currency->format(
 						$this->tax->calculate(
 								$this->currency->convert($quote_data[$key]['cost'], $this->config->get('config_currency'), $this->currency->getCode()),
 								$this->config->get('default_parcelforce_48_tax'), $this->config->get('config_tax')
 								)
-				                );
+								);
 				} else {
 					//all products are marked free for shipping
 					$quote_data[$key]['cost'] = 0.0;
-				    $quote_data[$key]['text'] = $this->language->get('text_free');	            
+					$quote_data[$key]['text'] = $this->language->get('text_free');
 				}
 			}
 		} else if ($new_quote_data) {
 			$quote_data = $new_quote_data;
 		}
 
-		//for case when only products with fixed shippig price are in the cart
+		//for case when only products with fixed shipping price are in the cart
 		$title = $this->language->get('text_title');
 		if(!$basic_products && $special_ship_products){
 			$quote_data = array('default_parcelforce_48' => array(
-			                    'id'           => 'default_parcelforce_48.default_parcelforce_48',
-			                    'title'        => $title,
-			                    'cost'         => $total_fixed_cost,
-			                    'tax_class_id' => 0,
-			                    'text'         => $this->currency->format( $total_fixed_cost )
+								'id'           => 'default_parcelforce_48.default_parcelforce_48',
+								'title'        => $title,
+								'cost'         => $total_fixed_cost,
+								'tax_class_id' => 0,
+								'text'         => $this->currency->format( $total_fixed_cost )
 			));
 		}
 		//when only products with free shipping are in the cart
 		if(!$basic_products && $special_ship_products && !$total_fixed_cost){
 			$quote_data = array('default_parcelforce_48' => array(
-								                    'id'           => 'default_parcelforce_48.default_parcelforce_48',
-								                    'title'        => $title,
-								                    'cost'         => 0,
-								                    'tax_class_id' => 0,
-								                    'text'         => $this->language->get('text_free')
+													'id'           => 'default_parcelforce_48.default_parcelforce_48',
+													'title'        => $title,
+													'cost'         => 0,
+													'tax_class_id' => 0,
+													'text'         => $this->language->get('text_free')
 			));
 		}
 
@@ -152,21 +152,19 @@ class ModelExtensionDefaultParcelforce48 extends Model {
 	private function _processRate($weight, $sub_total) {
 
 		$rates = explode(',', $this->config->get('default_parcelforce_48_rate'));
-
+		$cost = 0.0;
 		foreach ($rates as $rate) {
 			$data = explode(':', $rate);
-
 			if ($data[0] >= $weight) {
 				if (isset($data[1])) {
 					$cost = $data[1];
 				}
-
 				break;
 			}
 		}
 
 		$rates = explode(',', $this->config->get('default_parcelforce_48_compensation'));
-
+		$compensation = 0.0;
 		foreach ($rates as $rate) {
 			$data = explode(':', $rate);
 
