b1216fba255ea3fddd6cb9710f75d4ab23405837
abolabo <abolabo@gmail.com>
2017-06-22 13:53:50 +0300

    #854

--- public_html/admin/controller/pages/extension/extension_summary.php
+++ public_html/admin/controller/pages/extension/extension_summary.php
@@ -22,63 +22,56 @@ if (! defined ( 'DIR_CORE' ) || !IS_ADMIN) {
 }
 class ControllerPagesExtensionExtensionSummary extends AController {
 	public $data = array();
-  	public function main() {
+	public function main() {
 		//Load input arguments for gid settings
-	    $this->data = func_get_arg(0);
-          //init controller data
-        $this->extensions->hk_InitData($this,__FUNCTION__);
+		$this->data = func_get_arg(0);
+		  //init controller data
+		$this->extensions->hk_InitData($this,__FUNCTION__);
 
-    	$this->loadLanguage('extension/extensions');
+		$this->loadLanguage('extension/extensions');
+		$extension = $this->request->get['extension'];
+		if($extension && !$this->data['extension_info']){
+			$this->data['extension_info'] = $this->extensions->getExtensionInfo($extension);
+		}
 
-	    $extension = $this->request->get['extension'];
+		$icon_ext_img_url = HTTPS_EXT . $extension . '/image/icon.png';
+		$icon_ext_dir = DIR_EXT . $extension . '/image/icon.png';
+		$icon = (is_file($icon_ext_dir) ? $icon_ext_img_url : RDIR_TEMPLATE . 'image/default_extension.png');
 
-	    if($extension && !$this->data['extension_info']){
-		    $this->data['extension_info'] = $this->extensions->getExtensionInfo($extension);
-	    }
+		$this->data['extension_info']['icon'] = $icon;
+		$this->data['extension_info']['name'] = $this->language->get($extension . '_name');
 
-	    $icon_ext_img_url = HTTP_CATALOG . 'extensions/' . $extension . '/image/icon.png';
-        $icon_ext_dir = DIR_EXT . $extension . '/image/icon.png';
-        $icon = (is_file($icon_ext_dir) ? $icon_ext_img_url : RDIR_TEMPLATE . 'image/default_extension.png');
+		$datetime_format = $this->language->get('date_format_short').' '.$this->language->get('time_format');
 
-	    $this->data['extension_info']['icon'] = $icon;
-        $this->data['extension_info']['name'] = $this->language->get($extension . '_name');
+		if($this->data['extension_info']['date_installed']){
+			$this->data['extension_info']['installed'] = dateISO2Display($this->data['extension_info']['date_installed'], $datetime_format );
+		}
+		if($this->data['extension_info']['date_added']){
+			$this->data['extension_info']['date_added'] =  dateISO2Display($this->data['extension_info']['date_added'], $datetime_format );
+		}
+		$updates = $this->cache->pull('extensions.updates');
 
-        $datetime_format = $this->language->get('date_format_short').' '.$this->language->get('time_format');
+		// if update available
+		if( is_array($updates) && in_array($extension,array_keys($updates)) ){
+			if ($updates[$extension]['installation_key']){
+				$update_now_url = $this->html->getSecureURL('tool/package_installer', '&extension_key=' . $updates[$extension]['installation_key']);
+			} else{
+				$update_now_url = $updates[$extension]['url'];
+			}
+			$this->data['upgrade_button'] = $this->html->buildElement(
+								array(  'type'=> 'button',
+										'name' => 'btn_upgrade',
+										'id' => 'upgradenow',
+										'href' => $update_now_url,
+										'text' => $this->language->get('button_upgrade')
+								));
+		}
 
-        if($this->data['extension_info']['date_installed']){
-            $this->data['extension_info']['installed'] = dateISO2Display($this->data['extension_info']['date_installed'], $datetime_format );
-        }
-        if($this->data['extension_info']['date_added']){
-            $this->data['extension_info']['date_added'] =  dateISO2Display($this->data['extension_info']['date_added'], $datetime_format );
-        }
-
-        $updates = $this->cache->pull('extensions.updates');
-
-        // if update available
-        if( is_array($updates) && in_array($extension,array_keys($updates)) ){
-            if ($updates[$extension]['installation_key']){
-                $update_now_url = $this->html->getSecureURL('tool/package_installer', '&extension_key=' . $updates[$extension]['installation_key']);
-            } else{
-                $update_now_url = $updates[$extension]['url'];
-            }
-
-
-            $this->data['upgrade_button'] = $this->html->buildElement(
-                                array(  'type'=> 'button',
-                                        'name' => 'btn_upgrade',
-                                        'id' => 'upgradenow',
-                                        'href' => $update_now_url,
-                                        'text' => $this->language->get('button_upgrade')
-                                ));
-        }
-
-        $this->data['extension_info']['license'] = $this->data['extension_info']['license_key'];
-
-        $this->view->batchAssign( $this->data );
+		$this->data['extension_info']['license'] = $this->data['extension_info']['license_key'];
+		$this->view->batchAssign( $this->data );
 		$this->processTemplate('pages/extension/extension_summary.tpl' );
 
-          //update controller data
-        $this->extensions->hk_UpdateData($this,__FUNCTION__);
+		//update controller data
+		$this->extensions->hk_UpdateData($this,__FUNCTION__);
 	}
-
 }
