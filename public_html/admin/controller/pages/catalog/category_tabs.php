a1d69b523cc87960370cc898f2ad47225bc2b88d
Cali-gula <info@goldenfoxi.com>
2017-10-14 15:48:07 -0300

    FOUND 36 ERRORS AND 4 WARNINGS AFFECTING 23 LINES - PHPCBF CAN FIX THE 35 MARKED SNIFF VIOLATIONS AUTOMATICALLY -

--- public_html/admin/controller/pages/catalog/category_tabs.php
+++ public_html/admin/controller/pages/catalog/category_tabs.php
@@ -1,57 +1,61 @@
 <?php
-/*------------------------------------------------------------------------------
-  $Id$
+/**
+*  AbanteCart 2011-2017
+*
+* AbanteCart, Ideal OpenSource Ecommerce Solution
+*
+* http://www.AbanteCart.com
+*
+*  NOTICE OF LICENSE
+*
+* Copyright 2011-2017 Belavier Commerce LLC
+* This source file is subject to Open Software License (OSL 3.0)
+* License details is bundled with this package in the file LICENSE.txt.
+* It is also available at this URL:
+*
+* http://www.opensource.org/licenses/OSL-3.0
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+* versions in the future. If you wish to customize AbanteCart for your
+* needs please refer to http://www.AbanteCart.com for more information.
+*/
 
-  AbanteCart, Ideal OpenSource Ecommerce Solution
-  http://www.AbanteCart.com
-
-  Copyright © 2011-2017 Belavier Commerce LLC
-
-  This source file is subject to Open Software License (OSL 3.0)
-  License details is bundled with this package in the file LICENSE.txt.
-  It is also available at this URL:
-  <http://www.opensource.org/licenses/OSL-3.0>
-  
- UPGRADE NOTE: 
-   Do not edit or add to this file if you wish to upgrade AbanteCart to newer
-   versions in the future. If you wish to customize AbanteCart for your
-   needs please refer to http://www.AbanteCart.com for more information.  
-------------------------------------------------------------------------------*/
-if (! defined ( 'DIR_CORE' ) || !IS_ADMIN) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE') || !IS_ADMIN) {
+    header('Location: static_pages/');
 }
-class ControllerPagesCatalogCategoryTabs extends AController {
 
-	public $data = array();
-     
-  	public function main() {
+class ControllerPagesCatalogCategoryTabs extends AController
+{
+    public $data = array();
 
-        //Load input arguments for gid settings
+    public function main()
+    {
+        /* Load input arguments for gid settings */
         $this->data = func_get_arg(0);
         if (!is_array($this->data)) {
-            throw new AException (AC_ERR_LOAD, 'Error: Could not create grid. Grid definition is not array.');
+            throw new AException(AC_ERR_LOAD, 'Error: Could not create grid. Grid definition is not array.');
         }
-        //init controller data
-        $this->extensions->hk_InitData($this,__FUNCTION__);
-
-		$this->loadLanguage('catalog/category');
-
-		$this->data['groups'] = array('general', 'data' );
-
-		foreach ($this->data['groups'] as $group) {
-			$this->data['link_' . $group] = $this->html->getSecureURL('catalog/category/'.($this->data['category_id'] ? 'update' : 'insert'),
-																	 ($this->data['category_id'] ? '&category_id='.$this->data['category_id'] : '')). '#'.$group;
-		}
-
-		if($this->data['category_id']){
-			$this->data['groups'][] = 'layout';
-			$this->data['link_layout'] = $this->html->getSecureURL('catalog/category/edit_layout', '&category_id='.$this->data['category_id']);
-		}
-
-		$this->view->batchAssign( $this->data );
-		$this->processTemplate('pages/catalog/category_tabs.tpl');
-
-		$this->extensions->hk_UpdateData($this, __FUNCTION__);
-	}
+        /* init controller data */
+        $this->extensions->hk_InitData($this, __FUNCTION__);
+        $this->loadLanguage('catalog/category');
+        $this->data['groups'] = array('general', 'data' );
+        foreach ($this->data['groups'] as $group) {
+            $this->data['link_'.$group] = $this->html->getSecureURL(
+                'catalog/category/'.($this->data['category_id'] ? 'update' : 'insert'),
+                $this->data['category_id'] ? '&category_id='.$this->data['category_id'] : ''
+            ).'#'.$group;
+        }
+        if ($this->data['category_id']) {
+            $this->data['groups'][] = 'layout';
+            $this->data['link_layout'] = $this->html->getSecureURL(
+                'catalog/category/edit_layout',
+                '&category_id='.$this->data['category_id']
+            );
+        }
+        $this->view->batchAssign($this->data);
+        $this->processTemplate('pages/catalog/category_tabs.tpl');
+        $this->extensions->hk_UpdateData($this, __FUNCTION__);
+    }
 }
-
