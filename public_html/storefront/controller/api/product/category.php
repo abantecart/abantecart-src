81f752476572de54b47c124b57622793e724fbca
abantecart <projkov@abantecart.com>
2018-05-28 08:27:29 -0400

    Add SEO data returned to API

--- public_html/storefront/controller/api/product/category.php
+++ public_html/storefront/controller/api/product/category.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 /*------------------------------------------------------------------------------
   $Id$
 
@@ -17,100 +17,112 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if (! defined ( 'DIR_CORE' )) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE')) {
+    header('Location: static_pages/');
 }
-class ControllerApiProductCategory extends AControllerAPI {
-	
-	public function get() {
-		$this->extensions->hk_InitData($this,__FUNCTION__);
-			
-		if(!isset($this->request->get['path']) && isset($this->request->get['category_id']) ){
-			$this->request->get['path'] = $this->request->get['category_id'];
-		}  
 
-		if ( !isset($this->request->get['path']) && !isset($this->request->get['category_id']) ) {
-			$this->rest->setResponseData( array('Error' => 'Missing one of required category parameters') );
-			$this->rest->sendResponse(200);
-			return null;
-		}
+class ControllerApiProductCategory extends AControllerAPI
+{
 
-		if (isset($this->request->get['path']) && $this->request->get['path'] != 0) {
-			$parts = explode('_', $this->request->get['path']);
-			$category_id = array_pop($parts);
-			$category_info = $this->getCategoryDetails($category_id);
-		} else {
-			$category_info['category_id'] = 0;
-			$category_info['subcategories'] = $this->getCategories( );
-		}
-		$this->extensions->hk_UpdateData($this,__FUNCTION__);
-		$this->rest->setResponseData( $category_info );
-		$this->rest->sendResponse( 200 );
-	}
+    public function get()
+    {
+        $this->extensions->hk_InitData($this, __FUNCTION__);
 
+        if (!isset($this->request->get['path']) && isset($this->request->get['category_id'])) {
+            $this->request->get['path'] = $this->request->get['category_id'];
+        }
 
-	public function getCategoryDetails( $category_id ) {
-		$this->extensions->hk_InitData($this,__FUNCTION__);
-		$this->loadModel('catalog/category');
-		$this->loadModel('catalog/product');
-		$this->loadModel('tool/image'); 
+        if (!isset($this->request->get['path']) && !isset($this->request->get['category_id'])) {
+            $this->rest->setResponseData(array('Error' => 'Missing one of required category parameters'));
+            $this->rest->sendResponse(200);
+            return null;
+        }
 
-		$category_info = $this->model_catalog_category->getCategory($category_id);
-		if (!$category_info) {
-			return array( 'message' => 'category not found' );
-		}
-		$resource = new AResource('image');
-		$thumbnail = $resource->getMainThumb('categories',
-												 $category_id,
-												 $this->config->get('config_image_category_width'),
-												 $this->config->get('config_image_category_height'));
+        if (isset($this->request->get['path']) && $this->request->get['path'] != 0) {
+            $parts = explode('_', $this->request->get['path']);
+            $category_id = array_pop($parts);
+            $category_info = $this->getCategoryDetails($category_id);
 
-		//typo fix with backwards compatibility
-		//TODO: remove this in the future
-		$category_info['thumbnail'] = $category_info['tumbnail'] = $thumbnail['thumb_url'];
+            $this->loadModel('tool/seo_url');
+            $keyword = $this->model_tool_seo_url->getSEOKeyword('category', 'category_id', $category_id, $this->config->get('storefront_language_id'));
+            if ($keyword) {
+                $url = defined('HTTP_SERVER') ? HTTP_SERVER : 'http://'.REAL_HOST.get_url_path($_SERVER['PHP_SELF']);
+                $category_info['seo_url'] = $url.'/'.$keyword;
+            }
+        } else {
+            $category_info['category_id'] = 0;
+            $category_info['subcategories'] = $this->getCategories();
+        }
 
-		//Process data for category 
-		$category_info['description'] = html_entity_decode($category_info['description'], ENT_QUOTES, 'UTF-8'); 
-		$category_info['total_products'] = $this->model_catalog_product->getTotalProductsByCategoryId($category_id);
-		$category_info['total_subcategories'] = $this->model_catalog_category->getTotalCategoriesByCategoryId($category_id);
-		if ($category_info['total_products']) {
-			$category_info['subcategories'] = $this->getCategories( $category_id );
-		} 
-		$this->extensions->hk_UpdateData($this,__FUNCTION__);
-		return $category_info;
-	}
+        $this->extensions->hk_UpdateData($this, __FUNCTION__);
+        $this->rest->setResponseData($category_info);
+        $this->rest->sendResponse(200);
+    }
 
-	public function getCategories( $parent_categ_id = 0 ) {
-		$this->extensions->hk_InitData($this,__FUNCTION__);
-		$this->loadModel('catalog/category');
-		$results = $this->model_catalog_category->getCategories($parent_categ_id);
+    public function getCategoryDetails($category_id)
+    {
+        $this->extensions->hk_InitData($this, __FUNCTION__);
+        $this->loadModel('catalog/category');
+        $this->loadModel('catalog/product');
+        $this->loadModel('tool/image');
 
-		$category_ids = array();
-		foreach($results as $result){
-			$category_ids[] = (int)$result['category_id'];
-		}
-		//get thumbnails by one pass
-		$resource = new AResource('image');
-		$thumbnails = $resource->getMainThumbList(
-				'categories',
-				$category_ids,
-				$this->config->get('config_image_category_width'),
-				$this->config->get('config_image_category_height')
-		);
+        $category_info = $this->model_catalog_category->getCategory($category_id);
+        if (!$category_info) {
+            return array('message' => 'category not found');
+        }
+        $resource = new AResource('image');
+        $thumbnail = $resource->getMainThumb('categories',
+            $category_id,
+            $this->config->get('config_image_category_width'),
+            $this->config->get('config_image_category_height'));
 
-		foreach ($results as $result) {
-				$thumbnail = $thumbnails[ $result['category_id'] ];
-				
-				$categories[] = array(
-						'name'  => $result['name'],
-						'category_id'	=> $result['category_id'],
-						'sort_order'	=> $result['sort_order'],
-						'thumb' => $thumbnail['thumb_url'],
-						'total_subcategories' => $this->model_catalog_category->getTotalCategoriesByCategoryId($result['category_id'])
-						);
-		}
-	
-		$this->extensions->hk_UpdateData($this,__FUNCTION__);
-		return $categories;
-	}		
+        //typo fix with backwards compatibility
+        //TODO: remove this in the future
+        $category_info['thumbnail'] = $category_info['tumbnail'] = $thumbnail['thumb_url'];
+
+        //Process data for category
+        $category_info['description'] = html_entity_decode($category_info['description'], ENT_QUOTES, 'UTF-8');
+        $category_info['total_products'] = $this->model_catalog_product->getTotalProductsByCategoryId($category_id);
+        $category_info['total_subcategories'] = $this->model_catalog_category->getTotalCategoriesByCategoryId($category_id);
+        if ($category_info['total_products']) {
+            $category_info['subcategories'] = $this->getCategories($category_id);
+        }
+        $this->extensions->hk_UpdateData($this, __FUNCTION__);
+        return $category_info;
+    }
+
+    public function getCategories($parent_categ_id = 0)
+    {
+        $this->extensions->hk_InitData($this, __FUNCTION__);
+        $this->loadModel('catalog/category');
+        $results = $this->model_catalog_category->getCategories($parent_categ_id);
+
+        $category_ids = array();
+        foreach ($results as $result) {
+            $category_ids[] = (int)$result['category_id'];
+        }
+        //get thumbnails by one pass
+        $resource = new AResource('image');
+        $thumbnails = $resource->getMainThumbList(
+            'categories',
+            $category_ids,
+            $this->config->get('config_image_category_width'),
+            $this->config->get('config_image_category_height')
+        );
+
+        foreach ($results as $result) {
+            $thumbnail = $thumbnails[$result['category_id']];
+
+            $categories[] = array(
+                'name'                => $result['name'],
+                'category_id'         => $result['category_id'],
+                'sort_order'          => $result['sort_order'],
+                'thumb'               => $thumbnail['thumb_url'],
+                'total_subcategories' => $this->model_catalog_category->getTotalCategoriesByCategoryId($result['category_id']),
+            );
+        }
+
+        $this->extensions->hk_UpdateData($this, __FUNCTION__);
+        return $categories;
+    }
 }
--- public_html/storefront/controller/api/product/product.php
+++ public_html/storefront/controller/api/product/product.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 /*------------------------------------------------------------------------------
   $Id$
 
@@ -17,131 +17,139 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if (! defined ( 'DIR_CORE' )) {
-	header ( 'Location: static_pages/' );
+if (!defined('DIR_CORE')) {
+    header('Location: static_pages/');
 }
-class ControllerApiProductProduct extends AControllerAPI {
-	
-	public function get() {
-        $this->extensions->hk_InitData($this,__FUNCTION__);
+
+class ControllerApiProductProduct extends AControllerAPI
+{
+
+    public function get()
+    {
+        $this->extensions->hk_InitData($this, __FUNCTION__);
         $request = $this->rest->getRequestParams();
 
-		$product_id = $request['product_id'];
+        $product_id = $request['product_id'];
 
-		if (empty($product_id) || !is_numeric($product_id)) {
-			$this->rest->setResponseData( array('Error' => 'Missing or incorrect format product ID') );
-			$this->rest->sendResponse(200);
-			return null;
-		}
+        if (empty($product_id) || !is_numeric($product_id)) {
+            $this->rest->setResponseData(array('Error' => 'Missing or incorrect format product ID'));
+            $this->rest->sendResponse(200);
+            return null;
+        }
 
-		//Load all the data from the model
-    	$this->loadModel('catalog/product');
-		$product_info = $this->model_catalog_product->getProduct( $product_id );
-		if ( count ($product_info) <= 0 ) {
-			$this->rest->setResponseData( array('Error' => 'No product found') );
-			$this->rest->sendResponse(200);	
-			return null;
-		}
-		//Add and edit data based on the more details 
-		
-		//load resource library 
-		$resource = new AResource('image');
-		$thumbnail = $resource->getMainThumb('products',
-			                                 $product_id,
-			                                 $this->config->get('config_image_thumb_width'),
-			                                 $this->config->get('config_image_thumb_height'));
-		$product_info['thumbnail'] = $thumbnail['thumb_url'];
+        //Load all the data from the model
+        $this->loadModel('catalog/product');
+        $product_info = $this->model_catalog_product->getProduct($product_id);
+        if (count($product_info) <= 0) {
+            $this->rest->setResponseData(array('Error' => 'No product found'));
+            $this->rest->sendResponse(200);
+            return null;
+        }
+        //Add and edit data based on the more details
+        $this->loadModel('tool/seo_url');
+        $keyword = $this->model_tool_seo_url->getSEOKeyword('product', 'product_id', $product_id, $this->config->get('storefront_language_id'));
+        if ($keyword) {
+            $url = defined('HTTP_SERVER') ? HTTP_SERVER : 'http://' . REAL_HOST . get_url_path($_SERVER['PHP_SELF']);
+            $product_info['seo_url'] = $url . '/' . $keyword;
+        }
 
-		$promotion = new APromotion();
-		if ($this->config->get('config_customer_price') || $this->customer->isLogged() ) {
-			$product_price = $product_info['price'];
-			$discount = $promotion->getProductDiscount($product_id);
-			if ($discount) {
-				$product_price = $discount;
-				$product_info['price'] = $this->currency->format($this->tax->calculate($discount,
-					                                                                           $product_info['tax_class_id'],
-					                                                                           $this->config->get('config_tax')));
-				$product_info['special'] = FALSE;
-			} else {
-				$product_info['price'] = $this->currency->format($this->tax->calculate($product_info['price'],
-				                                                                           $product_info['tax_class_id'],
-				                                                                           $this->config->get('config_tax')));
-				
-				$special = $this->model_catalog_product->getProductSpecial($product_id);
-				
-				if ($special) {
-					$product_price = $special;
-					$product_info['special'] = $this->currency->format($this->tax->calculate($special,
-					                                                                             $product_info['tax_class_id'],
-					                                                                             $this->config->get('config_tax')));
-				} else {
-					$product_info['special'] = FALSE;
-				}
-			}
-			$product_discounts = $promotion->getProductDiscounts($product_id);
-			$discounts = array();
-			if($product_discounts){
-				foreach ($product_discounts as $discount) {
-						$discounts[] = array(
-							'quantity' => $discount['quantity'],
-							'price'    => $this->currency->format($this->tax->calculate($discount['price'], $product_info['tax_class_id'], $this->config->get('config_tax')))
-						);
-				}
-			}
-			$product_info['discounts'] = $discounts;
-	        $product_info['product_price'] = $product_price;
-		} else {
-			//Do not Show price if setting and not logged in
-			$product_info['product_price'] = '';
-			$product_info['price'] = '';
-		}
+        //load resource library
+        $resource = new AResource('image');
+        $thumbnail = $resource->getMainThumb('products',
+            $product_id,
+            $this->config->get('config_image_thumb_width'),
+            $this->config->get('config_image_thumb_height'));
+        $product_info['thumbnail'] = $thumbnail['thumb_url'];
 
-		if ($product_info['quantity'] <= 0) {
-			$product_info['stock'] = $product_info['stock_status'];
-		} else {
-			if ($this->config->get('config_stock_display')) {
-				$product_info['stock'] = $product_info['quantity'];
-			} else {
-				$product_info['stock'] = $this->language->get('text_instock');
-			}
-		}
-		//hide quantity
-		unset($product_info['quantity']);
-			
-			
-		if (!$product_info['minimum']) {
-			$product_info['minimum'] = 1;
-		}
+        $promotion = new APromotion();
+        if ($this->config->get('config_customer_price') || $this->customer->isLogged()) {
+            $product_price = $product_info['price'];
+            $discount = $promotion->getProductDiscount($product_id);
+            if ($discount) {
+                $product_price = $discount;
+                $product_info['price'] = $this->currency->format($this->tax->calculate($discount,
+                    $product_info['tax_class_id'],
+                    $this->config->get('config_tax')));
+                $product_info['special'] = false;
+            } else {
+                $product_info['price'] = $this->currency->format($this->tax->calculate($product_info['price'],
+                    $product_info['tax_class_id'],
+                    $this->config->get('config_tax')));
 
-		$product_info['description'] = html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8');
+                $special = $this->model_catalog_product->getProductSpecial($product_id);
 
-		$product_info['options'] = $this->model_catalog_product->getProductOptions($product_id);			
+                if ($special) {
+                    $product_price = $special;
+                    $product_info['special'] = $this->currency->format($this->tax->calculate($special,
+                        $product_info['tax_class_id'],
+                        $this->config->get('config_tax')));
+                } else {
+                    $product_info['special'] = false;
+                }
+            }
+            $product_discounts = $promotion->getProductDiscounts($product_id);
+            $discounts = array();
+            if ($product_discounts) {
+                foreach ($product_discounts as $discount) {
+                    $discounts[] = array(
+                        'quantity' => $discount['quantity'],
+                        'price'    => $this->currency->format($this->tax->calculate($discount['price'], $product_info['tax_class_id'], $this->config->get('config_tax'))),
+                    );
+                }
+            }
+            $product_info['discounts'] = $discounts;
+            $product_info['product_price'] = $product_price;
+        } else {
+            //Do not Show price if setting and not logged in
+            $product_info['product_price'] = '';
+            $product_info['price'] = '';
+        }
 
-		$this->loadModel('catalog/review');
-		if ($this->config->get('enable_reviews')) {
-			$average = $this->model_catalog_review->getAverageRating( $product_id );	
-			$product_info['text_stars'] = sprintf($this->language->get('text_stars'), $average);
-			$product_info['stars'] = sprintf($this->language->get('text_stars'), $rating);
-			$product_info['average'] = $average;
-		}
+        if ($product_info['quantity'] <= 0) {
+            $product_info['stock'] = $product_info['stock_status'];
+        } else {
+            if ($this->config->get('config_stock_display')) {
+                $product_info['stock'] = $product_info['quantity'];
+            } else {
+                $product_info['stock'] = $this->language->get('text_instock');
+            }
+        }
+        //hide quantity
+        unset($product_info['quantity']);
 
-		$this->model_catalog_product->updateViewed($product_id);
-			
-		$tags = array();
-		$results = $this->model_catalog_product->getProductTags($product_id);
-		if($results){
-			foreach ($results as $result) {
-				if ($result['tag']) {
-					$tags[] = array( 'tag'	=> $result['tag'] );
-				}
-			}
-		}
-		$product_info['tags'] = $tags;
+        if (!$product_info['minimum']) {
+            $product_info['minimum'] = 1;
+        }
 
-        $this->extensions->hk_UpdateData($this,__FUNCTION__);
+        $product_info['description'] = html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8');
 
-		$this->rest->setResponseData( $product_info );
-		$this->rest->sendResponse(200);
-	}
-		
+        $product_info['options'] = $this->model_catalog_product->getProductOptions($product_id);
+
+        $this->loadModel('catalog/review');
+        if ($this->config->get('enable_reviews')) {
+            $average = $this->model_catalog_review->getAverageRating($product_id);
+            $product_info['text_stars'] = sprintf($this->language->get('text_stars'), $average);
+            $product_info['stars'] = sprintf($this->language->get('text_stars'), $rating);
+            $product_info['average'] = $average;
+        }
+
+        $this->model_catalog_product->updateViewed($product_id);
+
+        $tags = array();
+        $results = $this->model_catalog_product->getProductTags($product_id);
+        if ($results) {
+            foreach ($results as $result) {
+                if ($result['tag']) {
+                    $tags[] = array('tag' => $result['tag']);
+                }
+            }
+        }
+        $product_info['tags'] = $tags;
+
+        $this->extensions->hk_UpdateData($this, __FUNCTION__);
+
+        $this->rest->setResponseData($product_info);
+        $this->rest->sendResponse(200);
+    }
+
 }
