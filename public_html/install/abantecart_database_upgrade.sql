c745ace6f083d5dbcabb30f8e5db49f145edac2a
abolabo <abolabo@gmail.com>
2018-06-08 16:47:53 +0300

    install: sql fix of transactions table

--- public_html/install/abantecart_database_upgrade.sql
+++ public_html/install/abantecart_database_upgrade.sql
@@ -3,3 +3,7 @@ CHANGE COLUMN `description` `description` LONGTEXT NULL DEFAULT '' COMMENT 'tran
 CHANGE COLUMN `title` `title` LONGTEXT NULL DEFAULT '' COMMENT 'translatable' ,
 CHANGE COLUMN `meta_description` `meta_description` LONGTEXT NULL DEFAULT '' COMMENT 'translatable' ,
 CHANGE COLUMN `meta_keywords` `meta_keywords` LONGTEXT NULL DEFAULT '' COMMENT 'translatable' ;
+
+ALTER TABLE `ac_customer_transactions`
+  CHANGE COLUMN `credit` `credit` DECIMAL(14,4) NULL DEFAULT '0',
+  CHANGE COLUMN `debit` `debit` DECIMAL(14,4) NULL DEFAULT '0';
