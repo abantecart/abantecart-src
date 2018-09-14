b1990566e19e5c4736697708cac6b8583f8a6e22
BrianHCombes <briancombes59@yahoo.com>
2018-09-14 11:41:05 -0600

    config.php probably updated as I set up Abante Cart for local development work.

---
+++ public_html/system/config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+	AbanteCart, Ideal OpenSource Ecommerce Solution
+	http://www.AbanteCart.com
+	Copyright © 2011-2018 Belavier Commerce LLC
+
+	Released under the Open Software License (OSL 3.0)
+*/
+
+define('SERVER_NAME', '');
+// Admin Section Configuration. You can change this value to any name. Will use ?s=name to access the admin
+define('ADMIN_PATH', 'ttd_admin_2018');
+
+// Database Configuration
+define('DB_DRIVER', 'amysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'algoResponse811');
+define('DB_DATABASE', 'ttd_abante_cart');
+define('DB_PREFIX', 'ttd_');
+
+define('CACHE_DRIVER', 'file');
+// Unique AbanteCart store ID
+define('UNIQUE_ID', '383fa35b3fd12f88e518451828750568');
+// Encryption key for protecting sensitive information. NOTE: Change of this key will cause a loss of all existing encrypted information!
+define('ENCRYPTION_KEY', '6GN3QV');
