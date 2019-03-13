f1e9ab4792ad361afb8ad58d24cd94d1fb7ebd4b
abolabo <abolabo@gmail.com>
2019-03-13 14:56:38 +0200

    #1185

--- public_html/storefront/controller/pages/product/search.php
+++ public_html/storefront/controller/pages/product/search.php
@@ -30,16 +30,24 @@ class ControllerPagesProductSearch extends AController
     public function __construct(Registry $registry, $instance_id, $controller, $parent_controller = '')
     {
         parent::__construct($registry, $instance_id, $controller, $parent_controller);
+
+        $default_sorting = $this->config->get('config_product_default_sort_order');
+        $sort_prefix = '';
+        if (strpos($default_sorting, 'name-') === 0) {
+            $sort_prefix = 'pd.';
+        } elseif (strpos($default_sorting, 'price-') === 0) {
+            $sort_prefix = 'p.';
+        }
         $this->data['sorts'] = array(
-            'p.sort_order-ASC'   => $this->language->get('text_default'),
-            'pd.name-ASC'        => $this->language->get('text_sorting_name_asc'),
-            'pd.name-DESC'       => $this->language->get('text_sorting_name_desc'),
-            'p.price-ASC'        => $this->language->get('text_sorting_price_asc'),
-            'p.price-DESC'       => $this->language->get('text_sorting_price_desc'),
-            'rating-DESC'        => $this->language->get('text_sorting_rating_desc'),
-            'rating-ASC'         => $this->language->get('text_sorting_rating_asc'),
-            'date_modified-DESC' => $this->language->get('text_sorting_date_desc'),
-            'date_modified-ASC'  => $this->language->get('text_sorting_date_asc'),
+            $sort_prefix.$default_sorting => $this->language->get('text_default'),
+            'pd.name-ASC'                 => $this->language->get('text_sorting_name_asc'),
+            'pd.name-DESC'                => $this->language->get('text_sorting_name_desc'),
+            'p.price-ASC'                 => $this->language->get('text_sorting_price_asc'),
+            'p.price-DESC'                => $this->language->get('text_sorting_price_desc'),
+            'rating-DESC'                 => $this->language->get('text_sorting_rating_desc'),
+            'rating-ASC'                  => $this->language->get('text_sorting_rating_asc'),
+            'date_modified-DESC'          => $this->language->get('text_sorting_date_desc'),
+            'date_modified-ASC'           => $this->language->get('text_sorting_date_asc'),
         );
     }
 
