afaf10e0738be83def3660ae3508358b4d633c91
abolabo <abolabo@gmail.com>
2019-03-15 16:48:32 +0200

    #1233

--- public_html/core/lib/cart.php
+++ public_html/core/lib/cart.php
@@ -373,7 +373,14 @@ class ACart
         // product downloads
         $download_data = $this->download->getProductOrderDownloads($product_id);
 
-
+        //check if we need to check main product stock. Do only if no stock trackable options selected
+        if (!$options
+            && $product_query['subtract']
+            && $product_query['quantity'] < $quantity
+            && !$product_query['stock_checkout']
+        ) {
+            $stock = false;
+        }
 
         $result = array(
             'product_id'         => $product_query['product_id'],
