1d8f4bc03064563c66dcb85e895ab3075f540ec4
abolabo <abolabo@gmail.com>
2019-03-19 17:59:28 +0200

    version changed to 1.2.15

--- public_html/install/abantecart_database.sql
+++ public_html/install/abantecart_database.sql
@@ -12093,7 +12093,7 @@ VALUES  (20, NOW(),'1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
 VALUES  (21,'AbanteCart','1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
-VALUES  (22,'1.2.14','1');
+VALUES  (22,'1.2.15','1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
 VALUES  (23,'','1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
