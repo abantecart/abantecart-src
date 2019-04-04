e74f347b037bc0d600a4b750c3e8d10bfcdd0602
abolabo <abolabo@gmail.com>
2019-04-04 14:53:41 +0300

    #1245 Admin Menu fix related to permissions

--- public_html/admin/controller/common/menu.php
+++ public_html/admin/controller/common/menu.php
@@ -17,15 +17,12 @@
    versions in the future. If you wish to customize AbanteCart for your
    needs please refer to http://www.AbanteCart.com for more information.
 ------------------------------------------------------------------------------*/
-if (!defined('DIR_CORE') || !IS_ADMIN) {
-    header('Location: static_pages/');
-}
+
 
 class ControllerCommonMenu extends AController
 {
-
     public $data = array();
-    protected $permission = array();
+    protected $permissions = array();
     protected $groupID;
     const TOP_ADMIN_GROUP = 1;
 
@@ -42,10 +39,6 @@ class ControllerCommonMenu extends AController
         if ($this->groupID !== self::TOP_ADMIN_GROUP) {
             $user_group = $this->model_user_user_group->getUserGroup($this->groupID);
             $this->permissions = $user_group['permission'];
-        }
-
-        foreach ($menu_items as $item) {
-            $this->data['menu_items'][] = $item;
         }
 
         //use to update data before render
@@ -133,7 +126,7 @@ class ControllerCommonMenu extends AController
                 if (isset($item ['language'])) {
                     $this->loadLanguage($item ['language'], 'silent');
                 }
-                $childen = $this->_getChildItems($item['item_id'], $menu_items);
+                $children = $this->_getChildItems($item['item_id'], $menu_items);
                 $rt = '';
                 $menu_link = '';
                 if (preg_match("/(http|https):/", $item['item_url'])) {
@@ -146,14 +139,14 @@ class ControllerCommonMenu extends AController
                     }
                 }
 
-                $link_keyname = strpos($item ['item_url'], "http") ? "onclick" : "href";
+                $link_key_name = strpos($item ['item_url'], "http") ? "onclick" : "href";
 
                 $icon = $rm->getResource($item ['item_icon_rl_id']);
                 $icon = $icon['resource_code'] ? $icon['resource_code'] : '';
 
                 $temp = array(
                     'id'          => $item ['item_id'],
-                    $link_keyname => $menu_link,
+                    $link_key_name => $menu_link,
                     'text'        => $this->language->get($item ['item_text']),
                     'icon'        => $icon,
                 );
@@ -162,8 +155,8 @@ class ControllerCommonMenu extends AController
                     $temp['rt'] = $rt;
                 }
 
-                if ($childen) {
-                    $temp['children'] = $childen;
+                if ($children) {
+                    $temp['children'] = $children;
                 }
                 elseif ($this->groupID !== self::TOP_ADMIN_GROUP && !$this->permissions['access'][$rt]) {
                     //skip top menus with no access permission
