7ef4f77d0fbfa993b084395ef8e951c8d44ce5ce
denis <vdvpie22@gmail.com>
2019-10-03 10:56:03 +0300

    Travis fix

--- tests/phpunit/core/helper/SystemCheckTest.php
+++ tests/phpunit/core/helper/SystemCheckTest.php
@@ -3,38 +3,46 @@
 /**
  * Class SystemCheckTest
  */
-class SystemCheckTest extends AbanteCartTest{
-	private $original_save_path='';
-	protected function setUp(){
-		parent::__construct();
-		$this->original_save_path = ini_get('session.save_path');
-	}
+class SystemCheckTest extends AbanteCartTest
+{
+    private $original_save_path = '';
 
-	protected function tearDown(){
-		ini_set('session.save_path', $this->original_save_path);
-	}
+    public function setUp(): void
+    {
+        parent::__construct();
+        $this->original_save_path = ini_get('session.save_path');
+    }
 
-	public function testCheckEmptySessionSavePath(){
-		ini_set('session.save_path', '');
-		$result = check_session_save_path();
-		$this::assertEquals($result, array());
-	}
+    protected function tearDown()
+    {
+        ini_set('session.save_path', $this->original_save_path);
+    }
 
-	public function testCheckTmpSessionSavePath(){
-		ini_set('session.save_path', '/tmp');
-		$result = check_session_save_path();
-		$this->assertEquals($result, array());
-	}
+    public function testCheckEmptySessionSavePath()
+    {
+        ini_set('session.save_path', '');
+        $result = check_session_save_path();
+        $this::assertEquals($result, array());
+    }
 
-	public function testCheckTmp2SessionSavePath(){
-		ini_set('session.save_path', '1;/tmp');
-		$result = check_session_save_path();
-		$this->assertEquals($result, array());
-	}
+    public function testCheckTmpSessionSavePath()
+    {
+        ini_set('session.save_path', '/tmp');
+        $result = check_session_save_path();
+        $this->assertEquals($result, array());
+    }
 
-	public function testCheckTmp3SessionSavePath(){
-		ini_set('session.save_path', '1;0666;/tmp');
-		$result = check_session_save_path();
-		$this->assertEquals($result, array());
-	}
+    public function testCheckTmp2SessionSavePath()
+    {
+        ini_set('session.save_path', '1;/tmp');
+        $result = check_session_save_path();
+        $this->assertEquals($result, array());
+    }
+
+    public function testCheckTmp3SessionSavePath()
+    {
+        ini_set('session.save_path', '1;0666;/tmp');
+        $result = check_session_save_path();
+        $this->assertEquals($result, array());
+    }
 }
