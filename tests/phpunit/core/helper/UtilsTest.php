3c0294ecc467fc3bd9a09af8837795160b6d99ec
denis <vdvpie22@gmail.com>
2019-10-04 12:23:59 +0300

    Travis fix

--- tests/phpunit/core/helper/UtilsTest.php
+++ tests/phpunit/core/helper/UtilsTest.php
@@ -2,52 +2,57 @@
 
 /**
  * Class UtilsTest
+ *
  * @property ADB $db
  */
-class UtilsTest extends AbanteCartTest{
+class UtilsTest extends AbanteCartTest
+{
 
-	protected function setUp(){
-		parent::__construct();
-	}
+    protected function setUp(): void
+    {
+        parent::__construct();
+    }
 
-	protected function tearDown(){
-		$this->db->query(
-				"DELETE FROM " . $this->db->table("url_aliases") . "
+    protected function tearDown(): void
+    {
+        $this->db->query(
+            "DELETE FROM ".$this->db->table("url_aliases")."
 				WHERE keyword  LIKE '%phpunit%'"
-		);
-	}
+        );
+    }
 
-	public function testGetUniqueSeoKeyword(){
+    public function testGetUniqueSeoKeyword()
+    {
 
-		$result = getUniqueSeoKeyword('phpunit', 'product', 10000);
-		$this->assertEquals($result, 'phpunit');
+        $result = getUniqueSeoKeyword('phpunit', 'product', 10000);
+        $this->assertEquals($result, 'phpunit');
 
-		$this->db->query(
-				"INSERT INTO " . $this->db->table("url_aliases") . "
+        $this->db->query(
+            "INSERT INTO ".$this->db->table("url_aliases")."
 					(`keyword`, `query`)
 				 VALUES ('phpunit', 'product_id=10000')"
-		);
+        );
 
-		$result = getUniqueSeoKeyword('phpunit', 'product', 10001);
-		$this->assertEquals($result, 'phpunit-10001');
+        $result = getUniqueSeoKeyword('phpunit', 'product', 10001);
+        $this->assertEquals($result, 'phpunit-10001');
 
-		$this->db->query(
-				"INSERT INTO " . $this->db->table("url_aliases") . "
+        $this->db->query(
+            "INSERT INTO ".$this->db->table("url_aliases")."
 					(`keyword`, `query`)
 				 VALUES ('phpunit-10001', 'product_id=10001')"
-		);
+        );
 
-		$result = getUniqueSeoKeyword('phpunit', 'manufacturer', 100);
-		$this->assertEquals($result, 'phpunit-100');
+        $result = getUniqueSeoKeyword('phpunit', 'manufacturer', 100);
+        $this->assertEquals($result, 'phpunit-100');
 
-		$this->db->query(
-				"INSERT INTO " . $this->db->table("url_aliases") . "
+        $this->db->query(
+            "INSERT INTO ".$this->db->table("url_aliases")."
 					(`keyword`, `query`)
 				 VALUES ('phpunit-100', 'product_id=100')"
-		);
+        );
 
-		$result = getUniqueSeoKeyword('phpunit', 'manufacturer', 10001);
-		$this->assertEquals($result, 'phpunit-10001-10001');
-		//$this->assertTrue(false);
-	}
+        $result = getUniqueSeoKeyword('phpunit', 'manufacturer', 10001);
+        $this->assertEquals($result, 'phpunit-10001-10001');
+        //$this->assertTrue(false);
+    }
 }
