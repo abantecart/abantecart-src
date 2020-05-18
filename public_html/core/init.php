5e699bdccd1719fe04bbc154c35af80fc9337efc
abolabo <abolabo@gmail.com>
2020-05-18 13:15:51 +0300

    removed payment handlers

--- public_html/core/init.php
+++ public_html/core/init.php
@@ -273,8 +273,6 @@ try {
     require_once(DIR_CORE.'lib/task_manager.php');
     require_once(DIR_CORE.'lib/im.php');
     require_once(DIR_CORE.'lib/csrf_token.php');
-    require_once(DIR_CORE.'lib/PaymentHandlerInterface.php');
-    require_once(DIR_CORE.'lib/BasePaymentHandler.php');
 
 //Admin manager classes
     if (IS_ADMIN === true) {
