cba7838609889b8fce71c94e9e175395907bf21e
abolabo <abolabo@gmail.com>
2020-09-11 17:28:10 +0300

    https://github.com/abantecart/abantecart-marketplace/issues/234

--- public_html/admin/model/tool/updater.php
+++ public_html/admin/model/tool/updater.php
@@ -1,4 +1,5 @@
 <?php
+
 /*------------------------------------------------------------------------------
   $Id$
 
@@ -24,28 +25,22 @@ if (!defined('DIR_CORE') || !IS_ADMIN) {
 /**
  * Class ModelToolUpdater
  *
- * @property ModelToolMPAPI $model_tool_mp_api
  */
 class ModelToolUpdater extends Model
 {
-
     /**
      * error text array
      *
      * @var array
      */
-    public $error = array();
-    /**
-     * size of data in bytes
-     *
-     * @var int
-     */
-    public $dataSize = 0;
+    public $error = [];
 
     /**
      * this method checks for updates on remote server if date about updates absent in cache (cache expires about day)
      *
      * @param bool $force - sign to do request to mp-server forcibly
+     *
+     * @throws AException
      */
     public function check4Updates($force = false)
     {
@@ -64,11 +59,14 @@ class ModelToolUpdater extends Model
         }
     }
 
-    private function getExtensionsList()
+    /**
+     * @return array
+     */
+    protected function getExtensionsList()
     {
         $e = new AExtensionManager();
         $extensions_list = $e->getExtensionsList();
-        $list = array();
+        $list = [];
         $installed_extensions = $this->extensions->getInstalled('');
         if ($extensions_list->num_rows) {
             foreach ($extensions_list->rows as $extension) {
@@ -78,17 +76,20 @@ class ModelToolUpdater extends Model
                 }
                 // if extension is installed
                 if (in_array($extension['key'], $installed_extensions)) {
-                    $status = $extension['status'] ? $this->language->get('text_enabled') : $this->language->get('text_disabled');
+                    $status = $extension['status']
+                        ? $this->language->get('text_enabled') : $this->language->get(
+                            'text_disabled'
+                        );
 
                     $extension_name = trim($this->extensions->getExtensionName($extension['key']));
-                    $list[$extension['key']] = array(
+                    $list[$extension['key']] = [
                         'name'        => $extension_name,
                         'type'        => $extension['type'],
                         'category'    => $extension['category'],
                         'status'      => $status,
                         'license_key' => $extension['license_key'],
                         'version'     => $extension['version'],
-                    );
+                    ];
                 }
             }
         }
@@ -100,54 +101,60 @@ class ModelToolUpdater extends Model
      * this method gets json-formatted response from remote server and write it to cache
      *
      * @return array
+     * @throws AException
      */
-    private function _getUpdateInfo()
+    protected function _getUpdateInfo()
     {
         $el = $this->getExtensionsList();
-
-        $this->load->model('tool/mp_api');
-        $url = $this->model_tool_mp_api->getMPURL().'?rt=a/product/updates';
+        /** @var ModelToolMPAPI $mdl */
+        $mdl = $this->load->model('tool/mp_api');
+        $url = $mdl->getMPURL().'?rt=a/product/updates';
         $url .= "&store_id=".UNIQUE_ID;
         $url .= "&store_ip=".$_SERVER ['SERVER_ADDR'];
         $url .= "&store_url=".HTTP_SERVER;
         $url .= "&software_name=AbanteCart";
         $url .= "&software_version=".VERSION;
         $url .= "&language_code=".$this->language->getLanguageCode();
-        foreach ($el as $key => $extension) {
-            $url .= '&extensions['.$key.']='.$extension['version'];
-            $installed[$key] = $extension['version'];
+        foreach ($el as $extKey => $extension) {
+            $url .= '&extensions['.$extKey.']='.$extension['version'];
+            $installed[$extKey] = $extension['version'];
         }
         //do connect without any http-redirects
         $pack = new AConnect(true, true);
-        $info = $pack->getData($url);
+        $updatesInfo = $pack->getData($url);
 
         // get array with updates information
-        if (!$info) {
-            return array();
+        if (!$updatesInfo) {
+            return [];
         }
 
         //filter data
-        $output = array();
-        foreach ($info as $key => $versions) {
+        $output = [];
+        foreach ($updatesInfo as $extKey => $versions) {
             foreach ($versions as $version => $version_info) {
                 //skip not installed
-                if (!isset($installed[$key])) {
+                if (!isset($installed[$extKey])) {
                     continue 1;
                 }
+
                 //skip not supported by cart
                 if (!$version_info['cart_versions'] || !in_array(VERSION, $version_info['cart_versions'])) {
                     continue;
                 }
                 //skip old or current versions
-                if (version_compare($installed[$key], $version, '>=')) {
+                if (version_compare($installed[$extKey], $version, '>=')) {
                     continue;
                 }
-                //if we have 2 or more versions for cart version
-                if (!isset($output[$key][$version])
-                    || version_compare($installed[$key], $version, '<')
+                //if we have 2 or more versions of extension for asked cart version
+                if (
+                    //check is version older than installed
+                    (!isset($output[$extKey][$version])
+                        || version_compare($installed[$extKey], $version, '<'))
+                    // check for newer version in the list to take last
+                    && (!isset($output[$extKey]) || version_compare($output[$extKey]['version'], $version, '<'))
                 ) {
                     $version_info['version'] = $version;
-                    $output[$key] = $version_info;
+                    $output[$extKey] = $version_info;
                 }
             }
         }
