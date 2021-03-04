87d035fff01d769e40928949737d66470ea30167
abolabo <abolabo@gmail.com>
2021-03-04 18:21:58 +0200

    phpunit related fixes

--- tests/phpunit/core/lib/AConfigTest.php
+++ tests/phpunit/core/lib/AConfigTest.php
@@ -2,21 +2,19 @@
 
 class AConfigTest extends AbanteCartTest
 {
-    /**
-     * @var AConfig
-     */
+    /** @var AConfig */
     private $abc_object;
-    private $stores = array();
+    private $stores = [];
 
     protected function setUp(): void
     {
         parent::__construct();
 
-        $stores[0] = array('store_id' => 0);
+        $stores[0] = ['store_id' => 0];
 
         $sql = "SELECT *
-				FROM ".$this->db->table('settings')."
-				WHERE store_id = 0";
+                FROM ".$this->db->table('settings')."
+                WHERE store_id = 0";
         $result = $this->db->query($sql);
         foreach ($result->rows as $row) {
             if ($row['key'] == 'config_url') {
@@ -27,40 +25,42 @@ class AConfigTest extends AbanteCartTest
         }
 
         $stores[1] =
-            array(
+            [
                 'name'    => 'phpunit test store 1',
                 'alias'   => 'phpunit-test-store-1',
                 'url'     => 'http://phpunit-1.abantecart.com/',
                 'ssl_url' => 'https://phpunit-1.abantecart.com/',
-            );
-        $stores[2] = array(
+            ];
+        $stores[2] = [
             'name'    => 'phpunit test store 2',
             'alias'   => 'phpunit-test-store-2',
             'url'     => 'http://phpunit-2.abantecart.com/',
             'ssl_url' => 'https://phpunit-2.abantecart.com/',
-        );
+        ];
 
         foreach ($stores as $k => &$store) {
             if (!$k) {
                 continue;
             }
-            $this->db->query("INSERT INTO ".$this->db->table("stores")."
-						SET `name` = '".$this->db->escape($store['name'])."',
-							`alias` = '".$this->db->escape($store['alias'])."',
-							`status` = 1");
-            $store_id = (int)$this->db->getLastId();
+            $this->db->query(
+                "INSERT INTO ".$this->db->table("stores")."
+                SET `name` = '".$this->db->escape($store['name'])."',
+                    `alias` = '".$this->db->escape($store['alias'])."',
+                    `status` = 1"
+            );
+            $store_id = (int) $this->db->getLastId();
             $store['store_id'] = $store_id;
 
             // add settings of extension of default store to new store settings
             // NOTE: we do this because of extension status in settings table. It used to recognize is extension installed or not
-            $sql = "INSERT INTO ".$this->db->table("settings")." (store_id, `group`, `key`, `value`)
-				VALUES
-				('".$store_id."', 'details', 'store_name', '".$this->db->escape($store['name'])."'),
-				('".$store_id."', 'details', 'config_ssl', 1),
-				('".$store_id."', 'details', 'store_id', ".$store_id."),
-				('".$store_id."', 'details', 'config_url', '".$this->db->escape($store['url'])."'),
-				('".$store_id."', 'details', 'config_ssl_url', '".$this->db->escape($store['ssl_url'])."')
-				";
+            $sql = "INSERT INTO ".$this->db->table("settings")." 
+                        (store_id, `group`, `key`, `value`)
+                    VALUES
+                        ('".$store_id."', 'details', 'store_name', '".$this->db->escape($store['name'])."'),
+                        ('".$store_id."', 'details', 'config_ssl', 1),
+                        ('".$store_id."', 'details', 'store_id', ".$store_id."),
+                        ('".$store_id."', 'details', 'config_url', '".$this->db->escape($store['url'])."'),
+                        ('".$store_id."', 'details', 'config_ssl_url', '".$this->db->escape($store['ssl_url'])."') ";
             $this->db->query($sql);
         }
 
@@ -77,13 +77,15 @@ class AConfigTest extends AbanteCartTest
                 continue;
             }
 
-            $this->db->query("DELETE FROM ".$this->db->table("stores")."
-							WHERE store_id  = '".(int)$store['store_id']."'");
-            $this->db->query("DELETE FROM ".$this->db->table("settings")."
-							WHERE store_id  = '".(int)$store['store_id']."'");
+            $this->db->query(
+                "DELETE FROM ".$this->db->table("stores")." WHERE store_id  = '".(int) $store['store_id']."'"
+            );
+            $this->db->query(
+                "DELETE FROM ".$this->db->table("settings")." WHERE store_id  = '".(int) $store['store_id']."'"
+            );
         }
 
-        $this->stores = array();
+        $this->stores = [];
     }
 
     public function testSettings()
