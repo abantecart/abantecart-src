cbcd52765aee47d37ae3b3c608ec9a5b346e7f65
abolabo <abolabo@gmail.com>
2021-03-11 12:45:32 +0200

    ACORN-465

--- public_html/admin/controller/pages/catalog/product_relations.php
+++ public_html/admin/controller/pages/catalog/product_relations.php
@@ -93,10 +93,11 @@ class ControllerPagesCatalogProductRelations extends AController
             ]
         );
 
+        $this->loadModel('setting/store');
         $this->loadModel('catalog/category');
         $this->data['categories'] = [];
 
-        $products_stores = $this->model_catalog_product->getProductStores($product_id);
+        $products_stores = array_column($this->model_setting_store->getStores(), 'store_id');
 
         $results = $this->model_catalog_category->getCategories(ROOT_CATEGORY_ID, $products_stores);
 
