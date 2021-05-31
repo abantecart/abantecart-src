1a736901b8d6f886ecac805228ab00a02f5ec623
abolabo <abolabo@gmail.com>
2021-05-31 12:26:18 +0300

    Admin: product option value url fix

--- public_html/admin/controller/pages/catalog/product_options.php
+++ public_html/admin/controller/pages/catalog/product_options.php
@@ -6,7 +6,7 @@
   AbanteCart, Ideal OpenSource Ecommerce Solution
   http://www.AbanteCart.com
 
-  Copyright © 2011-2020 Belavier Commerce LLC
+  Copyright © 2011-2021 Belavier Commerce LLC
 
   This source file is subject to Open Software License (OSL 3.0)
   License details is bundled with this package in the file LICENSE.txt.
@@ -26,7 +26,6 @@ class ControllerPagesCatalogProductOptions extends AController
 {
     public $error = [];
     protected $attribute_manager;
-    public $data = [];
 
     public function main()
     {
@@ -68,7 +67,8 @@ class ControllerPagesCatalogProductOptions extends AController
                             AND ga.attribute_parent_id = 0 ",
                 'sort'   => 'sort_order',
                 'order'  => 'ASC',
-                'limit'  => 1000 // !we can not have unlimited, so set 1000 for now
+                // !we can not have unlimited, so set 1000 for now
+                'limit'  => 1000
             ],
             $this->session->data['content_language_id']
         );
@@ -83,17 +83,17 @@ class ControllerPagesCatalogProductOptions extends AController
         $default_language_id = $this->language->getDefaultLanguageID();
         foreach ($product_options as &$option) {
             $option_name = trim($option['language'][$content_language_id]['name']);
-            $option['language'][$content_language_id]['name'] = $option_name ? $option_name : 'n/a';
+            $option['language'][$content_language_id]['name'] = $option_name ? : 'n/a';
             $option_name = trim($option['language'][$default_language_id]['name']);
-            $option['language'][$default_language_id]['name'] = $option_name ? $option_name : 'n/a';
+            $option['language'][$default_language_id]['name'] = $option_name ? : 'n/a';
         }
         unset($option);
 
         $this->data['product_options'] = $product_options;
         $this->data['language_id'] = $this->session->data['content_language_id'];
         $this->data['url']['load_option'] = $this->html->getSecureURL(
-                'product/product/load_option',
-                '&product_id='.$product_id
+            'product/product/load_option',
+            '&product_id='.$product_id
         );
         $this->data['url']['update_option'] = $this->html->getSecureURL(
             'product/product/update_option',
@@ -198,9 +198,7 @@ class ControllerPagesCatalogProductOptions extends AController
         foreach ($product_options as $option) {
             $product_opt[$option['product_option_id']] = $option['language'][$content_language_id]['name'];
         }
-        $product_option_id = $this->request->get['product_option_id']
-            ? $this->request->get['product_option_id']
-            : $this->data['product_option_id'];
+        $product_option_id = $this->request->get['product_option_id'] ? : $this->data['product_option_id'];
         $this->data['options'] = $form->getFieldHtml(
             [
                 'type'    => 'selectbox',
@@ -297,7 +295,7 @@ class ControllerPagesCatalogProductOptions extends AController
 
         $this->addChild('pages/catalog/product_summary', 'summary_form', 'pages/catalog/product_summary.tpl');
         $object_title = $this->language->get('text_product').' '.$this->language->get('text_option_value');
-        $params = '&object_name=product_option_value&object_title='.$object_title;
+        $params = '&object_name=product_option_value&object_title='.urlencode($object_title);
         $this->data['rl_resource_library'] = $this->html->getSecureURL('common/resource_library', $params);
         $this->data['rl_resources'] = $this->html->getSecureURL('common/resource_library/resources', $params);
 
@@ -318,7 +316,7 @@ class ControllerPagesCatalogProductOptions extends AController
                 'object_id'   => '',
                 'types'       => ['image'],
                 //sign loading thumbs on page load. disable it for hidden attribute values info
-                'onload'      => false
+                'onload'      => false,
             ]
         );
         if ($this->config->get('config_embed_status')) {
@@ -343,10 +341,10 @@ class ControllerPagesCatalogProductOptions extends AController
             $this->error['warning'] = $this->language->get('error_permission');
         }
         if ($this->model_catalog_product->isProductGroupOption(
-                $this->request->get['product_id'],
-                $this->request->post['attribute_id']
-            )
-        ){
+            $this->request->get['product_id'],
+            $this->request->post['attribute_id']
+        )
+        ) {
             $this->error['warning'] = $this->language->get('error_option_in_group');
         }
 
