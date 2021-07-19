4c046aeac576a86d61fa507ebe059d6be3f46fd3
yonghan79 <yonghan79@gmail.com>
2021-07-19 14:36:03 +0700

    Update abantecart_database_upgrade.sql
    Fix incorrect sql statement

--- public_html/install/abantecart_database_upgrade.sql
+++ public_html/install/abantecart_database_upgrade.sql
@@ -15,10 +15,10 @@ ALTER TABLE `ac_product_option_values`
     ADD COLUMN `cost` DECIMAL(15,4) NOT NULL AFTER `price`;
 
 ALTER TABLE `ac_order_products`
-    ADD COLUMN `cost` DECIMAL(15,4) NOT NULL AFTER `price` DEFAULT '0.0000';
+    ADD COLUMN `cost` DECIMAL(15,4) NOT NULL DEFAULT '0.0000' AFTER `price`;
 
 ALTER TABLE `ac_order_options`
-    ADD COLUMN `cost` DECIMAL(15,4) NOT NULL AFTER `price` DEFAULT '0.0000';
+    ADD COLUMN `cost` DECIMAL(15,4) NOT NULL DEFAULT '0.0000' AFTER `price`;
 
 --
 -- DDL for table `coupon_categories`
