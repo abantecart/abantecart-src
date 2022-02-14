c6221742b65b0fb509aefc85aaa4b8010a9b0f55
abolabo <abolabo@gmail.com>
2022-02-14 12:01:18 +0200

    Admin: product page bugfix related to default weight class for new products

--- public_html/admin/controller/pages/catalog/product.php
+++ public_html/admin/controller/pages/catalog/product.php
@@ -669,11 +669,11 @@ class ControllerPagesCatalogProduct extends AController
             $this->config->get('config_weight_class')
         );
         if (isset($this->request->post['weight_class_id'])) {
-            $this->data['weight_class_id'] = $this->request->post['weight_class_id'];
-        } elseif (isset($product_info)) {
-            $this->data['weight_class_id'] = $product_info['weight_class_id'];
+            $this->data['weight_class_id'] = (int)$this->request->post['weight_class_id'];
+        } elseif ($product_info) {
+            $this->data['weight_class_id'] = (int)$product_info['weight_class_id'];
         } elseif (isset($weight_info)) {
-            $this->data['weight_class_id'] = $weight_info['weight_class_id'];
+            $this->data['weight_class_id'] = (int)$weight_info['weight_class_id'];
         } else {
             $this->data['weight_class_id'] = '';
         }
