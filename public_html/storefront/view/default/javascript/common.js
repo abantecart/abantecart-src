4b396b52b8e52ccc19fb87ec4b205e8761c0ef8a
abolabo <abolabo@gmail.com>
2022-03-11 23:10:49 +0200

    Storefront: PB related changes of default template. ACORN-677

--- public_html/storefront/view/default/javascript/common.js
+++ public_html/storefront/view/default/javascript/common.js
@@ -1,12 +1,12 @@
 //fill default values of common variables if not set
 if (!window.hasOwnProperty("baseUrl")) {
-    window.baseUrl = window.location.protocol
+    window.baseUrl = parent.window.location.protocol
         + '//'
-        + window.location.host
-        + window.location.pathname.replace('index.php', '');
+        + parent.window.location.host
+        + parent.window.location.pathname.replace('index.php', '');
 }
 if (!window.hasOwnProperty("samesite")) {
-    window.samesite = window.location.protocol === 'https:' ? 'None; secure=1;' : 'lax; secure=0;';
+    window.samesite = parent.window.location.protocol === 'https:' ? 'None; secure=1;' : 'lax; secure=0;';
 }
 
 if (!window.hasOwnProperty("cart_url")) {
