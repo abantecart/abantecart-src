c8b5b720fba7eef46805de728dc120e4e7ef9e2c
yonghan79 <yonghan79@gmail.com>
2023-05-01 08:19:47 +0700

    Update special.php

--- public_html/storefront/controller/pages/product/special.php
+++ public_html/storefront/controller/pages/product/special.php
@@ -93,7 +93,11 @@ class ControllerPagesProductSpecial extends AController
             $page = 1;
         }
 
-        $limit = $this->config->get('config_catalog_limit');
+        if (isset($this->request->get['limit'])) {
+            $limit = (int) $this->request->get['limit'];
+        } else {  
+            $limit = $this->config->get('config_catalog_limit');
+        }
 
         $sorting_href = $request['sort'];
         if (!$sorting_href || !isset($this->data['sorts'][$request['sort']])) {
