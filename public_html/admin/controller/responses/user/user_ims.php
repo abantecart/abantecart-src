6efd6f4aec836d5dab75df064752907369b603be
dedblood94 <dedblood94@gmail.com>
2023-11-14 15:56:05 +0200

    Done reformat code

--- public_html/admin/controller/responses/user/user_ims.php
+++ public_html/admin/controller/responses/user/user_ims.php
@@ -186,7 +186,9 @@ class ControllerResponsesUserUserIMs extends AController
             );
             $this->load->model('setting/setting');
             $this->model_setting_setting->editSetting('product_out_of_stock_threshold',
-                array('product_out_of_stock_threshold'=>isset($this->request->post['product_out_of_stock_threshold'])));
+                array(
+                    'product_out_of_stock_threshold' =>
+                        isset($this->request->post['product_out_of_stock_threshold'])));
 
             $output['result_text'] = $this->language->get('text_settings_success_saved');
 
