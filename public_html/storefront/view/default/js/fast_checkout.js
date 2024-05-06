54de0d2b1b215b03e78741dd08a889f4d188d28a
abolabo <abolabo@gmail.com>
2024-05-06 13:28:54 +0300

    SF: fc stripe js-fix ACORN-1036

--- public_html/storefront/view/default/js/fast_checkout.js
+++ public_html/storefront/view/default/js/fast_checkout.js
@@ -204,14 +204,14 @@ $(document).on(
             if ($(this).hasClass('selected')) {
                 return;
             }
-            const payment_id = $(this).find('.card-body').attr('data-payment-id');
+            const payment_id = $(this).attr('data-payment-id');
 
             const paymentAvailable = $(this).attr('data-payment-available');
             if (payment_id === 'account_balance' || paymentAvailable === 'false') {
                 return;
             }
             let form = $('#PayFrm');
-            let url = fc_main_url +'&' + $(form[0].elements).not("textarea#comment").serialize();
+            let url = fc_main_url +'&' + $(form[0].elements).not("textarea#comment").serialize() + '&payment_method=' + payment_id;
             $('#payment_details').remove();
             $('form').unbind("submit");
             form.attr('action', url);
