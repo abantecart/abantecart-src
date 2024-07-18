a6544a73191cbb6c2c623693bdf92096ff3d8cfc
abolabo <abolabo@gmail.com>
2024-07-18 14:08:06 +0300

    sf: cancellation order fix ACORN-1205

--- public_html/storefront/controller/pages/account/order_details.php
+++ public_html/storefront/controller/pages/account/order_details.php
@@ -1,21 +1,21 @@
 <?php
 /*
- *   $Id$
+ * $Id$
  *
- *   AbanteCart, Ideal OpenSource Ecommerce Solution
- *   http://www.AbanteCart.com
+ * AbanteCart, Ideal OpenSource Ecommerce Solution
+ * http://www.AbanteCart.com
  *
- *   Copyright © 2011-2024 Belavier Commerce LLC
+ * Copyright © 2011-2024 Belavier Commerce LLC
  *
- *   This source file is subject to Open Software License (OSL 3.0)
- *   License details is bundled with this package in the file LICENSE.txt.
- *   It is also available at this URL:
- *   <http://www.opensource.org/licenses/OSL-3.0>
+ * This source file is subject to Open Software License (OSL 3.0)
+ * License details is bundled with this package in the file LICENSE.txt.
+ * It is also available at this URL:
+ * <http://www.opensource.org/licenses/OSL-3.0>
  *
- *  UPGRADE NOTE:
- *    Do not edit or add to this file if you wish to upgrade AbanteCart to newer
- *    versions in the future. If you wish to customize AbanteCart for your
- *    needs please refer to http://www.AbanteCart.com for more information.
+ * UPGRADE NOTE:
+ * Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+ * versions in the future. If you wish to customize AbanteCart for your
+ * needs please refer to http://www.AbanteCart.com for more information.
  */
 
 if (!defined('DIR_CORE')) {
@@ -25,6 +25,11 @@ if (!defined('DIR_CORE')) {
 class ControllerPagesAccountOrderDetails extends AController
 {
     public $error = [];
+    public function __construct($registry, $instance_id, $controller, $parent_controller = '')
+    {
+        parent::__construct($registry, $instance_id, $controller, $parent_controller);
+        $this->data['order_details_rt'] = 'account/order_details';
+    }
 
     public function main()
     {
@@ -32,7 +37,7 @@ class ControllerPagesAccountOrderDetails extends AController
         $this->extensions->hk_InitData($this, __FUNCTION__);
         $order_token = '';
         $this->data['guest'] = $guest = false;
-        $this->data['order_details_rt'] = 'account/order_details';
+
 
         /** @var ModelAccountOrder $mdlOrder */
         $mdlOrder = $this->loadModel('account/order');
