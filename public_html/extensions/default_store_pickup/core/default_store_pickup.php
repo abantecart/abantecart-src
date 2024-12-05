334b01bc8f510465ef66bcc9dca2704c58cb16c6
abolabo <abolabo@gmail.com>
2024-12-05 20:00:52 +0200

    extensions: pickup from store fix. https://github.com/abantecart/abantecart-src/issues/1715

--- public_html/extensions/default_store_pickup/core/default_store_pickup.php
+++ public_html/extensions/default_store_pickup/core/default_store_pickup.php
@@ -1,24 +1,23 @@
 <?php
-/** @noinspection PhpUndefinedClassInspection */
+/*
+ *   $Id$
+ *
+ *   AbanteCart, Ideal OpenSource Ecommerce Solution
+ *   http://www.AbanteCart.com
+ *
+ *   Copyright © 2011-2024 Belavier Commerce LLC
+ *
+ *   This source file is subject to Open Software License (OSL 3.0)
+ *   License details is bundled with this package in the file LICENSE.txt.
+ *   It is also available at this URL:
+ *   <http://www.opensource.org/licenses/OSL-3.0>
+ *
+ *  UPGRADE NOTE:
+ *    Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+ *    versions in the future. If you wish to customize AbanteCart for your
+ *    needs please refer to http://www.AbanteCart.com for more information.
+ */
 
-/*------------------------------------------------------------------------------
-  $Id$
-
-  AbanteCart, Ideal OpenSource Ecommerce Solution
-  http://www.AbanteCart.com
-
-  Copyright © 2011-2021 Belavier Commerce LLC
-
-  This source file is subject to Open Software License (OSL 3.0)
-  License details is bundled with this package in the file LICENSE.txt.
-  It is also available at this URL:
-  <http://www.opensource.org/licenses/OSL-3.0>
-
- UPGRADE NOTE:
-   Do not edit or add to this file if you wish to upgrade AbanteCart to newer
-   versions in the future. If you wish to customize AbanteCart for your
-   needs please refer to http://www.AbanteCart.com for more information.
-------------------------------------------------------------------------------*/
 if (!defined('DIR_CORE')) {
     header('Location: static_pages/');
 }
@@ -27,7 +26,10 @@ class ExtensionDefaultStorePickup extends Extension
 {
     /**
      * @return bool
+     * @throws AException
      */
+    const SHP_TXT_ID = 'default_store_pickup.default_store_pickup';
+
     protected function isEnabled()
     {
         return ($this->baseObject->config->get('default_store_pickup_status'));
@@ -35,105 +37,24 @@ class ExtensionDefaultStorePickup extends Extension
 
     protected function removeStoreAddressFromAddressBook()
     {
+        $id = 0;
         $that = $this->baseObject;
         /** @var ModelAccountAddress $mdl */
         $mdl = $that->loadModel('account/address');
         $allAddresses = $mdl->getAddresses();
-        $exists = false;
         foreach ($allAddresses as $address) {
-            if (
-                $address['postcode'] == $that->config->get('config_postcode')
+            if ($address['postcode'] == $that->config->get('config_postcode')
                 && $address['country_id'] == $that->config->get('config_country_id')
                 && $address['zone_id'] == $that->config->get('config_zone_id')
             ) {
                 $id = $address['address_id'];
-                $exists = true;
                 break;
             }
         }
-        if ($exists) {
-            $mdl->deleteAddress( $id );
+        if ($id) {
+            $mdl->deleteAddress($id);
             $that->session->data['shipping_address_id'] = $that->customer->getAddressId();
         }
-    }
-
-    public function onControllerPagesCheckoutShipping_ProcessData()
-    {
-        $that = $this->baseObject;
-        if (!$this->isEnabled()) {
-            return;
-        }
-        if ($that->request->post['shipping_method'] != 'default_store_pickup.default_store_pickup'
-            || !$that->cart->hasShipping()
-        ) {
-            $this->removeStoreAddressFromAddressBook();
-            return;
-        }
-
-        //save store address to customer address list
-        /** @var ModelAccountAddress $mdl */
-        $mdl = $that->loadModel('account/address');
-        $allAddresses = $mdl->getAddresses();
-        $exists = false;
-        foreach ($allAddresses as $address) {
-            if (
-                $address['postcode'] == $that->config->get('config_postcode')
-                && $address['country_id'] == $that->config->get('config_country_id')
-                && $address['zone_id'] == $that->config->get('config_zone_id')
-            ) {
-                $that->session->data['shipping_address_id'] = $address['address_id'];
-                $exists = true;
-                break;
-            }
-        }
-        if (!$exists) {
-            $that->session->data['shipping_address_id'] = $mdl->addAddress(
-                [
-                    'address_1'  => $that->config->get('config_address'),
-                    'country_id' => $that->config->get('config_country_id'),
-                    'zone_id'    => $that->config->get('config_zone_id'),
-                    'postcode'   => $that->config->get('config_postcode'),
-                    'firstname'  => $that->customer->getFirstName(),
-                    'lastname'   => $that->customer->getLastName(),
-                    'city'       => $that->config->get('config_city'),
-                ]
-            );
-        }
-
-        $that->tax->setZone(
-            $that->config->get('config_country_id'),
-            $that->config->get('config_zone_id')
-        );
-    }
-
-    public function onControllerPagesCheckoutGuestStep2_ProcessData()
-    {
-        $that = $this->baseObject;
-        if (!$this->isEnabled()) {
-            return;
-        }
-        if ($that->request->post['shipping_method'] != 'default_store_pickup.default_store_pickup'
-            || !$that->cart->hasShipping()
-        ) {
-            return;
-        }
-
-        $that->session->data['guest']['shipping']['firstname'] = $that->session->data['guest']['shipping']['firstname']
-            ? : $that->session->data['guest']['firstname'];
-
-        $that->session->data['guest']['shipping']['lastname'] = $that->session->data['guest']['shipping']['lastname']
-            ? : $that->session->data['guest']['lastname'];
-
-        $that->session->data['guest']['shipping']['address_1'] = $that->config->get('config_address');
-        $that->session->data['guest']['shipping']['postcode'] = $that->config->get('config_postcode');
-        $that->session->data['guest']['shipping']['city'] = $that->config->get('config_city');
-        $that->session->data['guest']['shipping']['country_id'] = $that->config->get('config_country_id');
-        $that->session->data['guest']['shipping']['zone_id'] = $that->config->get('config_zone_id');
-
-        $that->tax->setZone(
-            $that->config->get('config_country_id'),
-            $that->config->get('config_zone_id')
-        );
     }
 
     public function onControllerResponsesCheckoutPay_InitData()
@@ -142,9 +63,10 @@ class ExtensionDefaultStorePickup extends Extension
         if (!$this->isEnabled()) {
             return;
         }
-        if (($that->request->get['shipping_method'] != 'default_store_pickup.default_store_pickup'
-                && $that->session->data['fc']['shipping_method']['id'] != 'default_store_pickup.default_store_pickup')
+        if (($that->request->get['shipping_method'] != self::SHP_TXT_ID
+                && $that->session->data['fc']['shipping_method']['id'] != self::SHP_TXT_ID)
             || !$that->cart->hasShipping()
+            || $this->baseObject_method == 'success'
         ) {
             return;
         }
@@ -155,46 +77,42 @@ class ExtensionDefaultStorePickup extends Extension
             $allAddresses = $mdl->getAddresses();
             $exists = false;
             foreach ($allAddresses as $address) {
-                if (
-                    $address['postcode'] == $that->config->get('config_postcode')
+                if ($address['postcode'] == $that->config->get('config_postcode')
                     && $address['country_id'] == $that->config->get('config_country_id')
                     && $address['zone_id'] == $that->config->get('config_zone_id')
                 ) {
-                    $that->request->get['shipping_address_id'] =
-                    $that->session->data['fc']['shipping_address_id'] = $address['address_id'];
+                    $that->request->get['shipping_address_id']
+                        = $that->session->data['fc']['shipping_address_id']
+                        = $address['address_id'];
                     $exists = true;
                     break;
                 }
             }
             if (!$exists) {
-                $that->request->get['shipping_address_id'] =
-                $that->session->data['fc']['shipping_address_id'] =
-                    $mdl->addAddress(
-                        [
-                            'address_1'  => $that->config->get('config_address'),
-                            'country_id' => $that->config->get('config_country_id'),
-                            'zone_id'    => $that->config->get('config_zone_id'),
-                            'postcode'   => $that->config->get('config_postcode'),
-                            'firstname'  => $that->customer->getFirstName(),
-                            'lastname'   => $that->customer->getLastName(),
-                            'city'       => $that->config->get('config_city'),
-                        ]
-                    );
+                $that->request->get['shipping_address_id']
+                    = $that->session->data['fc']['shipping_address_id']
+                    = $mdl->addAddress(
+                    [
+                        'address_1'  => $that->config->get('config_address'),
+                        'country_id' => $that->config->get('config_country_id'),
+                        'zone_id'    => $that->config->get('config_zone_id'),
+                        'postcode'   => $that->config->get('config_postcode'),
+                        'firstname'  => $that->customer->getFirstName(),
+                        'lastname'   => $that->customer->getLastName(),
+                        'city'       => $that->config->get('config_city'),
+                    ]
+                );
             }
         } else {
-            $that->session->data['fc']['guest']['shipping']['firstname'] =
-                $that->session->data['fc']['guest']['shipping']['firstname']
-                    ? : $that->session->data['fc']['guest']['firstname'];
-
-            $that->session->data['fc']['guest']['shipping']['lastname'] =
-                $that->session->data['fc']['guest']['shipping']['lastname']
-                    ? : $that->session->data['fc']['guest']['lastname'];
-
-            $that->session->data['fc']['guest']['shipping']['address_1'] = $that->config->get('config_address');
-            $that->session->data['fc']['guest']['shipping']['postcode'] = $that->config->get('config_postcode');
-            $that->session->data['fc']['guest']['shipping']['city'] = $that->config->get('config_city');
-            $that->session->data['fc']['guest']['shipping']['country_id'] = $that->config->get('config_country_id');
-            $that->session->data['fc']['guest']['shipping']['zone_id'] = $that->config->get('config_zone_id');
+            $sGuest = $that->session->data['fc']['guest'];
+            $sGuest['shipping']['firstname'] = $sGuest['shipping']['firstname'] ?: $sGuest['firstname'];
+            $sGuest['shipping']['lastname'] = $sGuest['shipping']['lastname'] ?: $sGuest['lastname'];
+            $sGuest['shipping']['address_1'] = $that->config->get('config_address');
+            $sGuest['shipping']['postcode'] = $that->config->get('config_postcode');
+            $sGuest['shipping']['city'] = $that->config->get('config_city');
+            $sGuest['shipping']['country_id'] = $that->config->get('config_country_id');
+            $sGuest['shipping']['zone_id'] = $that->config->get('config_zone_id');
+            $that->session->data['fc']['guest'] = array_merge($that->session->data['fc']['guest'], $sGuest);
         }
 
         $that->tax->setZone(
@@ -205,7 +123,7 @@ class ExtensionDefaultStorePickup extends Extension
 
     public function onControllerPagesCheckoutFinalize_InitData()
     {
-        if(!$this->isEnabled()){
+        if (!$this->isEnabled()) {
             return;
         }
 
