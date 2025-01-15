d5bb246a82b186404fdbbdc1ee27b8621c6ca5be
abolabo <abolabo@gmail.com>
2025-01-15 18:38:19 +0200

    extensions: address field length fix + sdk upgrade. ACORN-1501

--- public_html/extensions/default_authorizenet/vendor/composer/installed.php
+++ public_html/extensions/default_authorizenet/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.4.0.x-dev',
-        'version' => '1.4.0.9999999-dev',
-        'reference' => 'efa706939653da8c6ca63794c0eb2f9ffba99ef6',
+        'pretty_version' => '1.4.1.x-dev',
+        'version' => '1.4.1.9999999-dev',
+        'reference' => 'ec8b2518b2a27878c31740b7ab3fae6323cccb5d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.4.0.x-dev',
-            'version' => '1.4.0.9999999-dev',
-            'reference' => 'efa706939653da8c6ca63794c0eb2f9ffba99ef6',
+            'pretty_version' => '1.4.1.x-dev',
+            'version' => '1.4.1.9999999-dev',
+            'reference' => 'ec8b2518b2a27878c31740b7ab3fae6323cccb5d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'authorizenet/authorizenet' => array(
-            'pretty_version' => '2.0.3',
-            'version' => '2.0.3.0',
-            'reference' => 'e1acf55c9cb22bef1852b1e494502973ade11cce',
+            'pretty_version' => '2.0.4',
+            'version' => '2.0.4.0',
+            'reference' => '8555cc245953dd0ac57f7ea424a5572eae4c7191',
             'type' => 'library',
             'install_path' => __DIR__ . '/../authorizenet/authorizenet',
             'aliases' => array(),
