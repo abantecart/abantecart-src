428ee775c9b13ea3b0bc41809c3c5a404b71756c
abolabo <abolabo@gmail.com>
2025-01-22 12:26:12 +0200

    1.4.2 initial commit

--- public_html/install/abantecart_database.sql
+++ public_html/install/abantecart_database.sql
@@ -12057,7 +12057,7 @@ VALUES  (20, NOW(),'1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
 VALUES  (21,'AbanteCart','1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
-VALUES  (22,'1.4.1','1');
+VALUES  (22,'1.4.2','1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
 VALUES  (23,'','1');
 INSERT INTO `ac_dataset_values` (`dataset_column_id`, `value_varchar`,`row_id`)
--- public_html/install/abantecart_upgrade.php
+++ public_html/install/abantecart_upgrade.php
@@ -1,5 +1,1 @@
 <?php
-
-// clone pages/account layout as new account layouts (8).
-//use cloneLayout
-//rename prior page and layout to Account Dashboard
