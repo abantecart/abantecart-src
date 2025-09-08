29133f5a7ae08841631cb3bcac4e42ff2efe6f88
abolabo <abolabo@gmail.com>
2025-09-08 10:12:34 +0300

    unittest fix

--- tests/phpunit/AbanteCartTestBootstrap.php
+++ tests/phpunit/AbanteCartTestBootstrap.php
@@ -40,7 +40,6 @@ class AbanteCartTest extends PHPUnit\Framework\TestCase
         define('ABC_TEST_ROOT_PATH', $dirname);
         define('ABC_TEST_HTTP_HOST', 'travis-ci.org');
         define('ABC_TEST_PHP_SELF', 'abantecart/abantecart-src/public_html/index.php');
-        ini_set('session.save_path', '/tmp');
 
         $_SERVER['HTTP_HOST'] = ABC_TEST_HTTP_HOST;
         $_SERVER['PHP_SELF'] = ABC_TEST_PHP_SELF;
