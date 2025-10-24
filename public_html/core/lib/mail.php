3a85712846b976adfe3169f688ab80847588c542
abolabo <abolabo@gmail.com>
2025-10-24 10:02:24 +0300

    Core: AMail class minor fixes

--- public_html/core/lib/mail.php
+++ public_html/core/lib/mail.php
@@ -8,14 +8,14 @@
  *   Copyright © 2011-2025 Belavier Commerce LLC
  *
  *   This source file is subject to Open Software License (OSL 3.0)
- *   License details is bundled with this package in the file LICENSE.txt.
+ *   License details are bundled with this package in the file LICENSE.txt.
  *   It is also available at this URL:
  *   <http://www.opensource.org/licenses/OSL-3.0>
  *
  *  UPGRADE NOTE:
  *    Do not edit or add to this file if you wish to upgrade AbanteCart to newer
  *    versions in the future. If you wish to customize AbanteCart for your
- *    needs please refer to http://www.AbanteCart.com for more information.
+ *    needs, please refer to http://www.AbanteCart.com for more information.
  */
 
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
@@ -55,28 +55,29 @@ class AMail
      *
      * @throws AException
      */
-    public function __construct($config = null)
+    public function __construct(AConfig $config = null)
     {
         $dsn = '';
         $registry = Registry::getInstance();
-        $config = is_object($config) ? $registry->get('config') : $config;
+        $config = $config ?? $registry?->get('config');
         $this->email = new Email();
 
         //set default configuration values
         $this->transporting = $config->get('config_mail_transporting');
         if ($this->transporting == 'smtp') {
             $host = $config->get('config_smtp_host');
-            $host = substr($host,0, 6) == 'ssl://' ? substr($host,6) : $host;
+            $host = substr($host, 0, 6) == 'ssl://' ? substr($host, 6) : $host;
             $dsn = 'smtp://'
                 . urlencode($config->get('config_smtp_username')) . ':' . urlencode($config->get('config_smtp_password'))
                 . '@' . urlencode($host) . ':' . $config->get('config_smtp_port');
             //try to set timeout silently
             try {
                 ini_set('default_socket_timeout', $config->get('config_smtp_timeout'));
-            } catch (Exception $e) {}
+            } catch (Exception) {
+            }
         } elseif ($this->transporting == 'mail') {
             $dsn = 'native://default';
-        } elseif ($this->transporting == 'dsn') {
+        } elseif ($this->transporting == 'dsn' && defined('MAILER') && is_array(MAILER)) {
             if (MAILER['dsn']) {
                 $dsn = MAILER['dsn'];
             } else {
@@ -85,13 +86,13 @@ class AMail
                     . '@' . urlencode(MAILER['host'] ?: 'default') . (MAILER['port'] ? ':' . MAILER['port'] : '');
             }
         }
-        if(!$dsn){
+        if (!$dsn) {
             $dsn = 'native://default';
         }
 
         $this->log = $registry->get('log');
         $this->messages = $registry->get('messages');
-        $this->storeId = $config->get('current_store_id') ?? $config->get('config_store_id') ?? 0;
+        $this->storeId = (int)($config->get('current_store_id') ?? $config->get('config_store_id'));
         $this->extensions = $registry->get('extensions');
 
         $transport = Symfony\Component\Mailer\Transport::fromDsn($dsn);
@@ -102,7 +103,7 @@ class AMail
     /**
      * @param string|array $to - email address
      */
-    public function setTo($to)
+    public function setTo(string|array $to = '')
     {
         if (is_array($to)) {
             foreach ($to as $t) {
@@ -116,7 +117,7 @@ class AMail
     /**
      * @param string $from - email address
      */
-    public function setFrom($from)
+    public function setFrom(string $from = '')
     {
         $this->email->from($from);
     }
@@ -125,7 +126,7 @@ class AMail
      * @param string $header
      * @param string $value
      */
-    public function addHeader($header, $value)
+    public function addHeader(string $header, string $value)
     {
         $this->email->getHeaders()->addTextHeader(trim($header, " "), trim($value));
     }
@@ -133,11 +134,11 @@ class AMail
     /**
      * @param string $name - sender's name
      */
-    public function setSender(string $name, $from = null)
+    public function setSender(string $name, string $from = null)
     {
         $from = $from ?? current($this->email->getFrom());
         $from = $from instanceof Address ? $from->getAddress() : (string)$from;
-        if($from) {
+        if ($from) {
             $this->email->sender(new Address($from, $name));
         }
     }
@@ -145,7 +146,7 @@ class AMail
     /**
      * @param string $reply_to - email address
      */
-    public function setReplyTo($reply_to)
+    public function setReplyTo(string $reply_to = '')
     {
         $this->email->replyTo(new Address($reply_to));
 
@@ -154,7 +155,7 @@ class AMail
     /**
      * @param string $subject
      */
-    public function setSubject($subject)
+    public function setSubject(string $subject = '')
     {
         $this->email->subject($subject);
     }
@@ -162,7 +163,7 @@ class AMail
     /**
      * @param string $text
      */
-    public function setText($text)
+    public function setText(string $text = '')
     {
         $this->email->text($text);
     }
@@ -170,57 +171,58 @@ class AMail
     /**
      * @param string $html
      */
-    public function setHtml($html)
+    public function setHtml(string $html = '')
     {
         $this->email->html($html);
     }
 
     /**
-     * @param string $text_id
+     * @param string $textId
      * @param array $placeholders
      * @param int $languageId
      *
      * @throws AException
      */
-    public function setTemplate($text_id, array $placeholders = [], $languageId = 0)
+    public function setTemplate(string $textId, array $placeholders = [], int $languageId = 0)
     {
-
-        $text_id = trim($text_id);
-        if (empty($text_id)) {
+        if (preformatTextID($textId) != $textId) {
+            $this->log->write('Email text id "' . $textId . '" must be in one word without spaces, underscores are allowed');
+            return;
+        }
+        if (empty($textId)) {
             $this->log->write('Email text id can\'t be empty');
             return;
         }
 
-        if (!preg_match("/(^[\w]+)$/i", $text_id)) {
-            $this->log->write('Email text id "' . $text_id . '" must be in one word without spaces, underscores are allowed');
-            return;
-        }
 
         $db = Registry::getInstance()->get('db');
         if (!$languageId) {
             /** @var ALanguageManager $language */
             $language = Registry::getInstance()->get('language');
-            $languageId = IS_ADMIN ? $language->getContentLanguageID() : $language->getLanguageID();
+            $languageId = IS_ADMIN ? $language?->getContentLanguageID() : $language?->getLanguageID();
+        }
+        if (!$languageId) {
+            throw new AException(__METHOD__ . ' Language ID is required for email template!');
         }
 
         $emailTemplate = $db->query(
             "SELECT * 
             FROM " . $db->table('email_templates') . " 
-            WHERE `text_id`='" . $text_id . "' 
+            WHERE `text_id`='" . $textId . "' 
                 AND `language_id` = " . (int)$languageId . "
-                AND `status` = 1 and `store_id` = " . (int)$this->storeId . " LIMIT 1"
+                AND `status` = 1 and `store_id` = " . (int)$this->storeId . " 
+            LIMIT 1"
         );
         if (empty($emailTemplate->rows)) {
-            $this->log->write('Email Template with text id "' . $text_id . '" and language_id = ' . $languageId . ' not found');
+            $this->log->write('Email Template with text id "' . $textId . '" and language_id = ' . $languageId . ' not found');
             return;
         }
 
         $this->emailTemplate = $emailTemplate->row;
-        $arAllowedPlaceholders = explode(',', $this->emailTemplate['allowed_placeholders']);
-
-        foreach ($arAllowedPlaceholders as &$placeholder) {
-            $placeholder = trim($placeholder);
-        }
+        $arAllowedPlaceholders = array_map(
+            'trim',
+            explode(',', $this->emailTemplate['allowed_placeholders'])
+        );
 
         foreach ($placeholders as $key => $val) {
             if (in_array($key, $arAllowedPlaceholders, true)) {
@@ -228,20 +230,26 @@ class AMail
             }
         }
 
-        $this->extensions->hk_ProcessData($this, 'setTemplate', [
-            'text_id' => $text_id,
-            'language_id' => $languageId,
-        ]);
+        $this->extensions->hk_ProcessData(
+            $this,
+            'setTemplate',
+            [
+                'text_id'     => $textId,
+                'language_id' => $languageId,
+            ]
+        );
 
         $subject = html_entity_decode($this->emailTemplate['subject'], ENT_QUOTES);
         $htmlBody = html_entity_decode($this->emailTemplate['html_body'], ENT_QUOTES);
         $textBody = $this->emailTemplate['text_body'];
 
-        // allow to pass html as text_variable (needed for logo as resource_html)
+        // allow passing HTML as text_variable (needed for logo as resource_html)
         //override default escaping by transparent custom
-        $mustache = new Mustache_Engine(['escape' => function ($value) {
-            return $value;
-        }]);
+        $mustache = new Mustache_Engine([
+            'escape' => function ($value) {
+                return $value;
+            }
+        ]);
         $subject = $mustache->render($subject, $this->placeholders);
         $htmlBody = $mustache->render($htmlBody, $this->placeholders);
         $textBody = $mustache->render($textBody, $this->placeholders);
@@ -272,15 +280,15 @@ class AMail
     }
 
     /**
-     * @param string $file - full path to file
+     * @param string $path - full path to file
      * @param string $filename
      */
-    public function addAttachment($file, $filename = '')
+    public function addAttachment(string $path, string $filename = '')
     {
         if (!$filename) {
-            $filename = md5(pathinfo($file, PATHINFO_FILENAME)) . '.' . pathinfo($file, PATHINFO_EXTENSION);
+            $filename = md5(pathinfo($path, PATHINFO_FILENAME)) . '.' . pathinfo($path, PATHINFO_EXTENSION);
         }
-        $this->email->attachFromPath($file, $filename);
+        $this->email->attachFromPath($path, $filename);
 
     }
 
@@ -288,7 +296,7 @@ class AMail
      * @return bool
      * @throws TransportExceptionInterface|AException
      */
-    public function send(?bool $silent = false)
+    public function send(bool $silent = false)
     {
 
         if (defined('IS_DEMO') && IS_DEMO) {
@@ -323,18 +331,16 @@ class AMail
             $error = 'Error: E-Mail message required!';
             $this->log->write($error);
             $this->error[] = $error;
-            $this->messages->saveError('Mailer error!', 'Can\'t send emails. Please see log for details and check your mail settings.');
+            $this->messages->saveError('Mailer error!', "Can't send emails. Please see log for details and check your mail settings.");
             return $silent;
         }
 
         try {
             $this->email->ensureValidity();
             $this->mailer->send($this->email);
-        }catch(Exception|Error $e){
-            $this->log->write(
-                __CLASS__.': '.$e->getMessage()."\n".$e->getTraceAsString()
-            );
-            $this->error[] = $e->getMessage()."\n".$e->getTraceAsString();
+        } catch (Exception|Error $e) {
+            $this->log->write(__CLASS__ . ': ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+            $this->error[] = $e->getMessage() . "\n" . $e->getTraceAsString();
         }
 
         if ($this->error) {
